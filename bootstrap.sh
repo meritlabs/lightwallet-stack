e0af9eb2a0d1e1706afe702f36958f859dc23e10
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-08-30 15:27:36 +0300

    fix npm install problems and bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 basedir=`echo $PWD/${1##*/}`
-cd ../merit-bitcoin-referrals/src
-./bitcoind -conf=${basedir}data-1/bitcoin.conf -datadir=${basedir}data-1/ & ./bitcoind -conf=${basedir}data-3/bitcoin.conf -datadir=${basedir}data-3/ && fg
+cd ../merit-bitcoin/src
+./bitcoind -conf=${basedir}data-and-logs/data-1/bitcoin.conf -datadir=${basedir}data-and-logs/data-1/ & ./bitcoind -conf=${basedir}data-and-logs/data-3/bitcoin.conf -datadir=${basedir}data-and-logs/data-3/ && fg
 # & ./bitcoind -conf=${basedir}data-2/bitcoin.conf -datadir=${basedir}data-2/ 
 
-alias bc1=./src/bitcoin-cli -conf=-conf=${basedir}data-1/bitcoin.conf 
-alias bc2=./src/bitcoin-cli -conf=-conf=${basedir}data-2/bitcoin.conf 
-alias bc3=./src/bitcoin-cli -conf=-conf=${basedir}data-3/bitcoin.conf 
+alias bc1=./src/bitcoin-cli -conf=-conf=${basedir}data-and-logs/data-1/bitcoin.conf 
+alias bc2=./src/bitcoin-cli -conf=-conf=${basedir}data-and-logs/data-2/bitcoin.conf 
+alias bc3=./src/bitcoin-cli -conf=-conf=${basedir}data-and-logs/data-3/bitcoin.conf 
 
