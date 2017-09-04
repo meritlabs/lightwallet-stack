6913ab9186366320187c10b8dbd1ea34d2a2452a
Adil Wali <adil@adil.io>
2017-09-04 16:29:59 -0700

    Updating bootstrap and clean bash script with proper dirs.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
-cd data-1 
+cd data-and-logs/data-1 
 ls | grep -v bitcoin.conf | xargs rm -rf
 cd ../data-2
 ls | grep -v bitcoin.conf | xargs rm -rf
 cd ../data-3
 ls | grep -v bitcoin.conf | xargs rm -rf
+cd ../../
