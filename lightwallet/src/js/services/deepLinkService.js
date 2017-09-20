e24176479aea7681f232ad7963b95af0798d90a8
Adil Wali <adil@merit.me>
2017-09-20 15:20:21 -0700

    Scaffolding an easySend onboarding, with deeplingk to webapp.

--- lightwallet/src/js/services/deepLinkService.js
+++ lightwallet/src/js/services/deepLinkService.js
@@ -1,7 +1,17 @@
 'use strict';
 
-angular.module('copayApp.services').factory('deepLinkService', function($ionicPlatform) {
+angular.module('copayApp.services').factory('deepLinkService', function($ionicPlatform, platformInfo) {
     var deeplink = {};
+
+    $ionicPlatform.ready(function(){
+        // Set branch debug to true in develpoment environments.  
+        //Branch.setDebug(true);
+    });
+
+    angular.element(document).ready(function () {
+        //Nothing yet
+    });
+
     // $ionicPlatform.ready(function() {
         
     //     $ionicPlatform.on('deviceReady', function() {
@@ -21,6 +31,20 @@ angular.module('copayApp.services').factory('deepLinkService', function($ionicPl
                 alert('Deep Link Data:' + JSON.stringify(data));
             }
         });
+    }
+
+    deeplink.isInvited = function() {
+        $routeParams.inviteCode ? true : false;
+    }
+
+    deeplink.redirectDeepLink = function() {
+        if ($routeParams.inviteCode && $routeParams.secret) {
+            $state.go('onboarding.fromEasySend', {
+                secret: $routeParams.secret,
+                inviteCode: $routeParams.inviteCode,
+                amount: $routeParams.amount
+            });
+        } 
     }
 
     return deeplink;
