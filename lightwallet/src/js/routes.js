fa7b56ec262c698742f2d5e30c4797a7e0a50885
Adil Wali <adil@merit.me>
2017-09-20 15:20:21 -0700

    Scaffolding an easySend onboarding, with deeplingk to webapp.

--- lightwallet/src/js/routes.js
+++ lightwallet/src/js/routes.js
@@ -739,6 +739,7 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
           }
         }
       })
+
       /*
        *
        * Onboarding
@@ -836,6 +837,18 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
         },
       })
 
+      // DEEPLINK INTO ONBOARDING FLOW
+      .state('onboarding.fromEasySend', {
+        templateUrl: 'views/onboarding/welcome-easysend.html',
+        controller: 'welcomeController',
+        params: {
+          inviteCode: null,
+          amount: null,
+          secret: null
+        }
+      })
+      
+      
       /*
        *
        * Feedback
@@ -1186,7 +1199,7 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
         }
       });
   })
-  .run(function($rootScope, $state, $location, $log, $timeout, startupService, ionicToast, fingerprintService, $ionicHistory, $ionicPlatform, $window, appConfigService, lodash, platformInfo, profileService, uxLanguage, gettextCatalog, openURLService, storageService, scannerService, configService, emailService, /* plugins START HERE => */ coinbaseService, glideraService, amazonService, bitpayCardService, applicationService, mercadoLibreService, deepLinkService) {
+  .run(function($rootScope, $state, $location, $log, $timeout, startupService, ionicToast, fingerprintService, $ionicHistory, $ionicPlatform, $window, appConfigService, lodash, platformInfo, profileService, uxLanguage, gettextCatalog, openURLService, storageService, scannerService, configService, emailService, $routeParams, /* plugins START HERE => */ coinbaseService, glideraService, amazonService, bitpayCardService, applicationService, mercadoLibreService, deepLinkService) {
 
     uxLanguage.init();
 
