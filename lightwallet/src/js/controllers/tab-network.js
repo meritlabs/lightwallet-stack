62b24331439c2fc833607c46f5af14f9196d8930
tony.pizzicato <tony@merit.me>
2017-09-22 17:28:21 +0300

    network tab

---
+++ lightwallet/src/js/controllers/tab-network.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular
+  .module('copayApp.controllers')
+  .controller('tabNetworkController', function(
+    $rootScope,
+    $scope,
+    $log,
+    $state,
+    $ionicHistory,
+    gettextCatalog,
+    lodash,
+    profileService,
+    walletService
+  ) {
+    $scope.$on('$ionicView.beforeEnter', function(event, data) {
+      $scope.wallets = profileService.getWallets();
+      $scope.singleWallet = $scope.wallets.length == 1;
+
+      if (!$scope.wallets[0]) return;
+
+      // select first wallet if no wallet selected previously
+      var selectedWallet = checkSelectedWallet($scope.wallet, $scope.wallets);
+      $scope.onWalletSelect(selectedWallet);
+    });
+
+    var checkSelectedWallet = function(wallet, wallets) {
+      if (!wallet) {
+        return wallets[0];
+      }
+
+      var w = lodash.find(wallets, wallet.id);
+
+      return w ? wallet : wallets[0];
+    };
+
+    $scope.onWalletSelect = function(wallet) {
+      $scope.wallet = wallet;
+    };
+
+    $scope.showWalletSelector = function() {
+      if ($scope.singleWallet) return;
+      $scope.walletSelectorTitle = gettextCatalog.getString('Select a wallet');
+      $scope.showWallets = true;
+    };
+
+    $scope.canGoBack = function() {
+      return $state.params.passthroughMode;
+    };
+
+    function goBack() {
+      $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+      });
+      $ionicHistory.backView().go();
+    }
+
+    $scope.goBack = goBack;
+  });
