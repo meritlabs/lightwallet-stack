e650e7fd65faca5c8482ad3bce691f99689b27e8
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-10-03 14:31:58 +0300

    fix transaction info method in meritd.js

--- lightwallet/angular-bitcore-wallet-client/index.js
+++ lightwallet/angular-bitcore-wallet-client/index.js
@@ -35,7 +35,7 @@ bwcModule.provider("bwcService", function() {
       //note opts use `bwsurl` all lowercase;
       //setting a backup-backup bwsUrl because this module won't have access to the configService
       var bwc = new Client({
-        baseUrl: opts.bwsurl || 'http://192.168.1.7:3232/bws/api',
+        baseUrl: opts.bwsurl || 'http://127.0.0.1:3232/bws/api',
         verbose: opts.verbose,
         timeout: 100000,
         transports: ['polling'],
