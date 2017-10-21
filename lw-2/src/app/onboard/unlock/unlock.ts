3f39b44c565252a51ae9204df4abdaf25ea401d5
Adil Wali <adil@adil.io>
2017-10-21 09:32:30 -0700

    Adding createAndUnlockMethod to unlock view.

--- lw-2/src/app/onboard/unlock/unlock.ts
+++ lw-2/src/app/onboard/unlock/unlock.ts
@@ -28,12 +28,20 @@ export class UnlockComponent {
 
   unlock() {
     //unlock actions
-    this.unlockState = 'success';
-    this.navCtrl.push('TransactComponent');
+    this.createAndUnlockWallet().then(() => {
+      this.unlockState = 'success';
+      this.navCtrl.push('TransactComponent');
+    });
   }
 
-  createAndUnlockWallet() {
-    this.walletService.createDefaultWallet();
+  createAndUnlockWallet(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.walletService.createDefaultWallet().then((wallet: any) => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
   }
 
 }
