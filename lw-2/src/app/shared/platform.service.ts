3fb1875c754e49a77722b00a27db02680bce80a0
Adil Wali <adil@adil.io>
2017-10-21 16:14:52 -0700

    Adding a Platform service.

---
+++ lw-2/src/app/shared/platform.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { Logger } from '@nsalaun/ng-logger';
+
+@Injectable()
+export class PlatformService {
+  isAndroid: boolean;
+  isIOS: boolean;
+  isWP: boolean;
+  isSafari: boolean;
+  isCordova: boolean;
+  isNW: boolean;
+  ua: string;
+  isMobile: boolean;
+  isDevel: boolean;
+
+  constructor(private platform: Platform, private log: Logger) {
+    let chrome: any;
+    var ua = navigator ? navigator.userAgent : null;
+
+    if (!ua) {
+      console.log('Could not determine navigator. Using fixed string');
+      ua = 'dummy user-agent';
+    }
+
+    // Fixes IOS WebKit UA
+    ua = ua.replace(/\(\d+\)$/, '');
+
+    this.isAndroid = platform.is('android');
+    this.isIOS = platform.is('ios');
+    this.isWP = platform.is('windows') && platform.is('mobile');
+    this.ua = ua;
+    this.isCordova = platform.is('cordova');
+    this.isNW = this.isNodeWebkit();
+    this.isMobile = platform.is('mobile');
+    this.isDevel = !this.isMobile && !this.isNW;
+  }
+
+  getBrowserName(): string {
+    let chrome: any;
+    let userAgent = window.navigator.userAgent;
+    let browsers = { chrome: /chrome/i, safari: /safari/i, firefox: /firefox/i, ie: /internet explorer/i };
+
+    for (let key in browsers) {
+      if (browsers[key].test(userAgent)) {
+        return key;
+      }
+    };
+
+    return 'unknown';
+  }
+
+  isNodeWebkit(): boolean {
+    let isNode = (typeof process !== "undefined" && typeof require !== "undefined");
+    if (isNode) {
+      try {
+        return (typeof require('nw.gui') !== "undefined");
+      } catch (e) {
+        return false;
+      }
+    }
+  }
+}
