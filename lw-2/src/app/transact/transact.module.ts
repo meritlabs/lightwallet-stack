2d09eb5b100f6e5c024d72b1c18e7a1b1ed4257e
Adil Wali <adil@adil.io>
2017-10-21 07:19:11 -0700

    Working toward more of a folders-by-features structure.
    No more separate providers directory.

--- lw-2/src/app/transact/transact.module.ts
+++ lw-2/src/app/transact/transact.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TransactPage } from './transact';
 
+/*
+  Transact is the most thoughtful name we can think of to define all of 
+  'primary' actions associated with merit (receiving, sending, primarily).
+*/
 @NgModule({
   declarations: [
     TransactPage,
