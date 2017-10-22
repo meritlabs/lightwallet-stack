3d99e5f600ae966f71a9d29795bff4ecf35247b0
Adil Wali <adil@merit.me>
2017-10-21 18:03:00 -0700

    Adding an App-Settings service for dynamic app settings.

---
+++ lw-2/src/app/shared/app-settings.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Logger } from '@nsalaun/ng-logger';
+import 'rxjs/add/operator/map';
+
+import { LanguageService } from '../shared/language.service';
+import { ConfigService } from '../shared/config.service';
+import { TouchIdService } from '../shared//touchid.service';
+
+interface App {
+  packageName: string;
+  packageDescription: string;
+  packageNameId: string;
+  themeColor: string;
+  userVisibleName: string;
+  purposeLine: string;
+  bundleName: string;
+  appUri: string;
+  name: string;
+  nameNoSpace: string;
+  nameCase: string;
+  nameCaseNoSpace: string;
+  gitHubRepoName: string;
+  gitHubRepoUrl: string;
+  gitHubRepoBugs: string;
+  disclaimerUrl: string;
+  url: string;
+  appDescription: string;
+  winAppName: string;
+  WindowsStoreIdentityName: string;
+  WindowsStoreDisplayName: string;
+  windowsAppId: string;
+  pushSenderId: string;
+  description: string;
+  version: string;
+  androidVersion: string;
+  commitHash: string;
+  _extraCSS: string;
+  _enabledExtensions: object;
+}
+
+@Injectable()
+export class AppService {
+  public info: App;
+  private jsonPath: string = 'assets/appConfig.json';
+
+  constructor(
+    public http: Http,
+    private logger: Logger,
+    private language: LanguageService,
+    private config: ConfigService,
+    private touchid: TouchIdService
+  ) {
+    this.logger.info('AppService initialized.');
+  }
+
+  public load() {
+    return new Promise((resolve, reject) => {
+      this.config.load().then(() => {
+        this.language.load();
+        this.touchid.init();
+        this.getInfo().subscribe((info) => {
+          this.info = info;
+          resolve();
+        });
+      }).catch((err) => {
+        this.logger.error(err);
+        reject();
+      });
+    });
+  }
+
+  private getInfo() {
+    return this.http.get(this.jsonPath)
+      .map((res: Response) => res.json());
+  }
+}
