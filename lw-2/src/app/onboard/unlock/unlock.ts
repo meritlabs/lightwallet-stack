820b7e707571adff059968c3e75be51fabcee826
Adil Wali <adil@merit.me>
2017-10-26 14:25:06 -0700

    Quick Comment.

--- lw-2/src/app/onboard/unlock/unlock.ts
+++ lw-2/src/app/onboard/unlock/unlock.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WalletService } from 'merit/wallets/wallet.service';
 
-
+// Unlock view for wallet
 @IonicPage({
   defaultHistory: ['OnboardingView']
 })
