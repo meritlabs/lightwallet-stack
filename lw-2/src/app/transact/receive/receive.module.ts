dba25f42b0cb7a8fcaedc3441c5c3b4d380d54f5
Adil Wali <adil@merit.me>
2017-10-26 05:36:55 -0700

    Make all imports relative to the app-root for sanity and success!

--- lw-2/src/app/transact/receive/receive.module.ts
+++ lw-2/src/app/transact/receive/receive.module.ts
@@ -3,7 +3,7 @@ import { IonicPageModule } from 'ionic-angular';
 import { ReceiveView } from './receive';
 import { MomentModule } from 'angular2-moment';
 import { QRCodeModule } from 'angular2-qrcode';
-import {ProfileService} from "../../../providers/profile-service";
+import { ProfileService } from "@app/core/profile.service";
 
 @NgModule({
   declarations: [
