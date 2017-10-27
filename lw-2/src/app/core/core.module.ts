7b92dce55dddabab2cc6dbda2dca3cc21439acab
Adil Wali <adil@merit.me>
2017-10-26 18:03:15 -0700

    Properly exposes providers in coreModule.

--- lw-2/src/app/core/core.module.ts
+++ lw-2/src/app/core/core.module.ts
@@ -4,6 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Logger } from "./logger";
+import { BwcService } from 'merit/core/bwc.service';
+import { PersistenceService } from 'merit/core/persistence.service';
+import { BwcError } from 'merit/core/bwc-error.model';
+import { PlatformService } from 'merit/core/platform.service';
+import { ProfileService } from 'merit/core/profile.service';
+import { LanguageService } from 'merit/shared/language.service';
+
 
 
 import { MomentModule } from 'angular2-moment';
@@ -21,6 +28,12 @@ import { MomentModule } from 'angular2-moment';
         StatusBar,
         SplashScreen,
         Logger,
+        BwcService,
+        PersistenceService,
+        BwcError,
+        PlatformService,
+        ProfileService,
+        LanguageService
     ]
 })
 
--- lw-2/src/app/onboard/unlock/unlock.module.ts
+++ lw-2/src/app/onboard/unlock/unlock.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UnlockView } from 'merit/onboard/unlock/unlock';
+import { WalletService } from 'merit/wallets/wallet.service';
 
 @NgModule({
   declarations: [
     UnlockView,
   ],
+  providers: [
+    WalletService
+  ],
   imports: [
     IonicPageModule.forChild(UnlockView),
   ],
 })
-export class UnlockComponentModule {}
+export class UnlockViewModule {}
