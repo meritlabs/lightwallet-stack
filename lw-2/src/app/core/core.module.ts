a776f61ed98829919281a68c3570e4c69057845a
Adil Wali <adil@merit.me>
2017-10-26 18:56:42 -0700

    HttpModule in Core.

--- lw-2/src/app/core/core.module.ts
+++ lw-2/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule, Http } from '@angular/http';
 
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -10,16 +11,28 @@ import { BwcError } from 'merit/core/bwc-error.model';
 import { PlatformService } from 'merit/core/platform.service';
 import { ProfileService } from 'merit/core/profile.service';
 import { LanguageService } from 'merit/shared/language.service';
+import { TxFormatService } from 'merit/transact/tx-format.service';
+import { AppService } from 'merit/core/app-settings.service';
+import { ConfigService } from 'merit/shared/config.service';
+import { TransactModule } from 'merit/transact/transact.module';
+
+
 
 
 
 import { MomentModule } from 'angular2-moment';
 
-// The core module exists to make commonly used singleton services available 
-// for use in other modules.  
+/* 
+  The core module exists to make commonly used singleton services available 
+  for use in other modules.  
+  The goal, over time, is to make this module leaner and tighter as we continue
+  to evolve the application.
+*/
 @NgModule({
     imports: [
         CommonModule,
+        TransactModule, // Ideally, we can remove this dependency.
+        HttpModule        
     ],
     exports: [],
     declarations: [],
@@ -33,7 +46,10 @@ import { MomentModule } from 'angular2-moment';
         BwcError,
         PlatformService,
         ProfileService,
-        LanguageService
+        LanguageService,
+        TxFormatService,
+        AppService,
+        ConfigService
     ]
 })
 
