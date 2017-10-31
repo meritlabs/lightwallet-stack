c97c117f5c2602c4ac22e75bbdf836f90944696e
Adil Wali <adil@merit.me>
2017-10-31 10:09:36 -0700

    WIP with bluebird.

--- lw-2/src/app/wallets/wallets.ts
+++ lw-2/src/app/wallets/wallets.ts
@@ -3,8 +3,13 @@ import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { Wallet } from "./wallet.model";
 import { ProfileService } from "merit/core/profile.service";
 import { WalletService } from "merit/wallets/wallet.service";
+import { Logger } from "merit/core/logger";
 import * as _ from "lodash";
+import { Promise } from 'bluebird';
 
+/* 
+  Using bluebird promises!
+*/ 
 @IonicPage()
 @Component({
   selector: 'view-wallets',
@@ -26,7 +31,8 @@ export class WalletsView {
     private navCtrl:NavController,
     private app:App,
     private profileService:ProfileService,
-    private walletService: WalletService
+    private walletService: WalletService,
+    private logger: Logger
   ) {
     //this.navCtrl = app.getRootNavs()[0];
   }
@@ -36,16 +42,17 @@ export class WalletsView {
   }
 
   ionViewDidLoad() {
-    console.log("Updating all the wallets.");
+    this.logger.warn("Updating all the wallets.");
     
     // TODO: Show loader?
-    this.updateAllWallets().then((updatedWallets) => {
-      console.log("Updated all the wallets.");
-      this.wallets = updatedWallets;
-    }).catch((err) => {
-      console.log("Could not update wallets");
-      // TODO: Throw a toast?  
-    }); 
+    this.updateAllWallets();
+    // .then((updatedWallets) => {
+    //   this.logger.warn("Updated all the wallets.");
+    //   this.wallets = updatedWallets;
+    // }).catch((err) => {
+    //   this.logger.warn("Could not update wallets");
+    //   // TODO: Throw a toast?  
+    // }); 
   }
 
   openWallet(wallet) {
@@ -64,21 +71,51 @@ export class WalletsView {
     this.navCtrl.push('ImportView');
   }
 
-  private updateAllWallets(): Promise<Array<Wallet>> {
-    return new Promise((resolve, reject) => {
+  private updateAllWallets(): void {
+    //return new Promise((resolve, reject) => {
       let wallets = this.profileService.getWallets();
-      console.log("Just got wallets");
-      console.log(wallets);
-      _.each(wallets, (wallet) => {
+      //var updatedWallets:Array<Wallet> = []; 
+
+      Promise.map(wallets, (wallet) => {
         this.walletService.getStatus(wallet).then((status) => {
           wallet.status = status;
-        }).catch((err) => {
-          console.log("Could not update all wallets!");
-          reject(err);
         });
+        return wallet;
+      }, {concurrency: 3}).then((upstatuses) => {
+        console.log("Let's update the wallets with: ");
+        console.log(upstatuses);
+        this.wallets = upstatuses;
+      }, (updatedWallets) => {
+        console.log("I think we got an error: ");
+        console.log(updatedWallets);
+      }).catch((err) => {
+        console.log("Bamboozled!!");
+        console.log(err);
       });
-      resolve(wallets);
-    })
+
+      // this.logger.warn("Just got wallets");
+      // this.logger.warn(wallets);
+      // _.forOwn(wallets, (wallet, walletId) => {
+      //   this.logger.warn("Learn you some JS");
+      //   this.logger.warn(wallet);
+      //   this.logger.warn(walletId);
+      //   this.walletService.getStatus(wallet).then((status) => {
+      //     wallet.status = status || "donkey";
+      //     updatedWallets.push(wallet);
+      //     this.logger.warn("Dirty status: ");
+      //     this.logger.warn(status);
+      //   }).catch((err) => {
+      //     this.logger.warn("Could not update all wallets!");
+      //     reject(err);
+      //   }).then((status) => {
+      //     this.logger.warn("Dirty status: ");
+      //     this.logger.warn(status);
+      //     });
+      // });
+      // this.logger.warn("Wallets AfterStatus: ");
+      // this.logger.warn(updatedWallets);
+      // resolve(updatedWallets);
+    //})
   }
 
 
