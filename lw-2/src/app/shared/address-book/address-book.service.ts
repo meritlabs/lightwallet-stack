4fb0623bf2a73bbe8da79ad0a43400c8ab24029a
Adil Wali <adil@merit.me>
2017-11-01 12:24:39 -0700

    WIP on addressbook.

---
+++ lw-2/src/app/shared/address-book/address-book.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Logger } from 'merit/core/logger';
+import { Promise } from 'bluebird';
+import * as _ from 'lodash';
+
+
+@Injectable()
+export class AddressBookService {
+  
+}
