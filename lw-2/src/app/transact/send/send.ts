c0cabbd19a781582992e2cdf6e625866da9ab762
Adil Wali <adil@merit.me>
2017-11-01 11:13:32 -0700

    Send view, including proper contact searching.

--- lw-2/src/app/transact/send/send.ts
+++ lw-2/src/app/transact/send/send.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Promise } from 'bluebird';
 
+import { Wallet } from 'merit/wallets/wallet.model';
+import { WalletService } from 'merit/wallets/wallet.service';
+import * as _ from 'lodash';
 
 @IonicPage()
 @Component({
@@ -8,11 +12,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'send.html',
 })
 export class SendView {
-
+  private walletsToTransfer: Array<any>; // Eventually array of wallets
+  private showTransferCard: boolean;
+  private wallets: Array<Wallet>;
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private walletService: WalletService
   ) {
 
   }
@@ -20,5 +27,44 @@ export class SendView {
   ionViewDidLoad() {
     //do something here
   }
+
+  private updateWalletList(): Promise<any> {
+    let walletList:Array<any> = [];
+    return new Promise((resolve, reject) => {
+      _.each(this.wallets, (w) => {
+        walletList.push({
+          color: w.color,
+          name: w.name, 
+          recipientType: 'wallet',
+          getAddress: (cb) => {
+            this.walletService.getAddress(w, false, cb);
+          }
+        });
+      });
+      return resolve(walletList);
+    });
+  }
+
+  private addressBookToContactList(ab): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let cl = _.map(ab, function(v, k) {
+        var item = {
+          name: _.isObject(v) ? v.name : v,
+          address: k,
+          email: _.isObject(v) ? v.email : null,
+          phoneNumber: _.isObject(v) ? v.phoneNumber : null,
+          recipientType: 'contact',
+          sendMethod: 'address',
+          getAddress: function(cb) {
+            return cb(null, k);
+          },
+        };
+        item.searchTerm = item.name + item.email + item.phoneNumber;
+        return item;
+      });
+      resolve(cl);
+    });
+  };
+
 
 }
