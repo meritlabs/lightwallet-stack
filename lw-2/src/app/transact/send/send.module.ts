56468e6ad0034250912b6257288676faa06b9f25
Adil Wali <adil@merit.me>
2017-11-02 13:27:50 -0700

    Importing gravatar component.

--- lw-2/src/app/transact/send/send.module.ts
+++ lw-2/src/app/transact/send/send.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SendView } from 'merit/transact/send/send';
+import { GravatarComponent } from 'merit/shared/gravatar.component';
 
 // This module manaages the sending of money.
 @NgModule({
@@ -9,6 +10,7 @@ import { SendView } from 'merit/transact/send/send';
   ],
   imports: [
     IonicPageModule.forChild(SendView),
+    GravatarComponent
   ],
   exports: [
   ]
