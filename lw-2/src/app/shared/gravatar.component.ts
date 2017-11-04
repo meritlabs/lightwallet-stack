993db7d29b1e13d4cd743bd2c8f59eeaab374dc3
Adil Wali <adil@merit.me>
2017-11-04 14:25:28 +0800

    Gravatar module complete.

--- lw-2/src/app/shared/gravatar.component.ts
+++ lw-2/src/app/shared/gravatar.component.ts
@@ -1,22 +1,22 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {Md5} from 'ts-md5/dist/md5';
 
+// Used to display friendly images of people who use gravatar.
 @Component({ 
-  selector: '[gravatar]', 
+  selector: 'gravatar', 
   inputs: ['name', 'height', 'width', 'email'],
   template: `
-  <img class="gravatar" alt="{{ name }}" height="{{ height }}"  width="{{ width }}" src="https://secure.gravatar.com/avatar/{{ emailHash }}.jpg?s={{ width }}&d=mm">
+  <img class="gravatar" [alt]="name" [height]="height"  [width]="width" src="https://secure.gravatar.com/avatar/{{ emailHash }}.jpg?s={{ width }}&d=mm">
 `
  })
 export class GravatarComponent {
-    name: string;
-    height: string; 
-    width: string;
-    email: string;
-    emailHash: string;
+    public name: string;
+    public height: string; 
+    public width: string;
+    public email: string;
+    public emailHash: string;
 
     constructor(
-      el: ElementRef
     ) {
       this.emailHash = Md5.hashStr(this.email.toLowerCase() || '').toString();
     }
