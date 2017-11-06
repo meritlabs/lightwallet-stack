a0150fbfb73654547469e1e1e55747d7760728de
Adil Wali <adil@merit.me>
2017-11-06 12:56:37 +0300

    Baseline send confirmation view.

--- lw-2/src/app/transact/send/confirm/send-confirm.ts
+++ lw-2/src/app/transact/send/confirm/send-confirm.ts
@@ -12,8 +12,21 @@ import { NavController, NavParams, IonicPage } from 'ionic-angular';
 })
 export class SendConfirmView {
 
+  // Core Params
   public address: string;
   public amount: string;
+  public description: string;
+  public sendMax: boolean;
+  public feeLevel: any;
+  public allowSpendUnconfirmed: boolean = true; // TODO: Consider removing entirely.
+
+  // Vanity Params -- Not on the blockchain; but we use convenience and usability.
+  public recipientType: any; // TODO: Define type
+  public toName: string;
+  public toEmail: string;
+  public toPhoneNumber: string;
+  public toColor: string;
+  public txp: any; // Transaction proposal
 
   constructor(
     public navCtrl: NavController, 
