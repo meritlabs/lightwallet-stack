a58fdb75efe57aca0307891f2cec199c9373280b
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-08 16:41:58 +0300

    connect create in service and in last view

--- lw-2/src/app/vaults/create-vault/create-vault.service.ts
+++ lw-2/src/app/vaults/create-vault/create-vault.service.ts
@@ -2,4 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CreateVaultService {
+
+    createVault(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            console.log('CreateVaultService#createVault');
+            resolve();
+        });
+    }
+       
 }
