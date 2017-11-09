24ed07627f2dcb4df23cbe6891ae09dfc4b35805
Adil Wali <adil@merit.me>
2017-11-09 06:50:27 +0300

    Let's make the WalletClient a singleton!

--- lw-2/src/app/core/bwc.service.ts
+++ lw-2/src/app/core/bwc.service.ts
@@ -1,37 +1,40 @@
 import { Injectable } from '@angular/core';
 
-//import * as BWC from  './../../lib/bwc';
-import { Client as BWC } from './../../lib/bwc/bwc-ts';
+import { Client } from './../../../../packages/merit-wallet-client';
 
 @Injectable()
 export class BwcService {
-  public buildTx = BWC.buildTx;
-  public parseSecret = BWC.parseSecret;
-  public Client = BWC;
-  constructor() {
+  private BWC: any;
+  public buildTx: any; // = BWC.buildTx;
+  public parseSecret: any; // = BWC.parseSecret;
+  constructor(opts: any = {}) {
+    this.BWC = Client.getInstance(opts);
+    this.buildTx = this.BWC.buildTx;
+    this.parseSecret = this.BWC.parseSecret;
     console.log('Hello BwcService Service');
   }
+  
   public getBitcore() {
-    return BWC.Bitcore;
+    return this.BWC.Bitcore;
   }
 
   public getErrors() {
-    return BWC.errors;
+    return this.BWC.errors;
   }
 
   public getSJCL() {
-    return BWC.sjcl;
+    return this.BWC.sjcl;
   }
 
   public getUtils() {
-    return BWC.Utils;
+    return this.BWC.Utils;
   }
 
   getClient(walletData, opts) {
     opts = opts || {};
 
     //note opts use `bwsurl` all lowercase;
-    let bwc = new BWC({
+    let bwc = Client.getInstance({
       baseUrl: opts.bwsurl || 'http://localhost:3232/bws/api',
       verbose: opts.verbose,
       timeout: 100000,
