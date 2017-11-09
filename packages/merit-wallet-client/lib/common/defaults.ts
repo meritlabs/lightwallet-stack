bb59e8a08705b9e6294f0a27a0ce97144d5b1f55
Adil Wali <adil@merit.me>
2017-11-09 21:05:35 +0300

    Tightening the importing and exporting of modules.

--- packages/merit-wallet-client/lib/common/defaults.ts
+++ packages/merit-wallet-client/lib/common/defaults.ts
@@ -1,9 +1,8 @@
 export module Defaults {
 
-  const DEFAULT_FEE_PER_KB:number = 10000;
-  const MIN_FEE_PER_KB:number = 0;
-  const MAX_FEE_PER_KB:number = 1000000;
-  const MAX_TX_FEE:number = 1 * 1e8;
-
-
+  export const DEFAULT_FEE_PER_KB:number = 10000;
+  export const MIN_FEE_PER_KB:number = 0;
+  export const MAX_FEE_PER_KB:number = 1000000;
+  export const MAX_TX_FEE:number = 1 * 1e8;
+  
 };
