15fdf76ce8891a1e3bf17ac571829b0eccebfabb
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-10 15:04:53 +0300

    Properly setting up consts inside Typescript chain.

--- packages/merit-wallet-client/lib/common/constants.ts
+++ packages/merit-wallet-client/lib/common/constants.ts
@@ -1,24 +1,25 @@
-export module Constants {
+export const Constants = {
 
-  export const SCRIPT_TYPES = {
+  SCRIPT_TYPES: {
     P2SH: 'P2SH',
     P2PKH: 'P2PKH',
-  };
-  export const DERIVATION_STRATEGIES = {
+  },
+  
+  DERIVATION_STRATEGIES: {
     BIP44: 'BIP44',
     BIP45: 'BIP45',
     BIP48: 'BIP48',
-  };
+  },
 
-  export const PATHS = {
+  PATHS: {
     REQUEST_KEY: "m/1'/0",
     TXPROPOSAL_KEY: "m/1'/1",
     REQUEST_KEY_AUTH: "m/2", // relative to BASE
-  };
+  },
 
-  export const BIP45_SHARED_INDEX = 0x80000000 - 1;
+  BIP45_SHARED_INDEX: 0x80000000 - 1,
 
-  export const UNITS = {
+  UNITS: {
     mrt: {
       toMicros: 100000000,
       full: {
@@ -41,5 +42,5 @@ export module Constants {
         minDecimals: 0,
       }
     },
-  };
+  },
 };
--- packages/merit-wallet-client/lib/common/index.ts
+++ packages/merit-wallet-client/lib/common/index.ts
@@ -1,7 +1,11 @@
+import { Constants as _Constants } from './constants';
+import { Defaults as _Defaults } from './defaults';
+import { Utils as _Utils } from './utils';
+
+// TODO: Investigate a way to import/expert with one line in modules.
+// Adil mentioned that there was a way to do.
 export module Common {
-
-export let Constants = require('./constants');
-export let Defaults = require('./defaults');
-export let Utils = require('./utils');
-
+    export const Constants = _Constants;
+    export const Defaults = _Defaults;
+    export const Utils = _Utils;
 }
