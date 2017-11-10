4e60fb559792bb65cf0406da7acf70335abbc6d3
Adil Wali <adil@merit.me>
2017-11-10 17:54:08 +0300

    MeritWalletClient instead of Client for clarity.

--- packages/merit-wallet-client/index.ts
+++ packages/merit-wallet-client/index.ts
@@ -2,16 +2,16 @@ import { MeritClient } from './lib/index';
 let BwcError = require('./lib/errors');
 
 // Singleton to ensure that conserve and handle memory most effectively.
-export class Client extends MeritClient {
-  private static _instance: Client;
+export class MeritWalletClient extends MeritClient {
+  private static _instance: MeritWalletClient;
   public errors;
   private constructor(opts:any = {}) {
     super({opts});
     this.errors = BwcError;
   }
 
-  public static getInstance(opts:any = {}): Client {
-    return (this._instance || new Client(opts));
+  public static getInstance(opts:any = {}): MeritWalletClient {
+    return (this._instance || new MeritWalletClient(opts));
   }
 }
 
