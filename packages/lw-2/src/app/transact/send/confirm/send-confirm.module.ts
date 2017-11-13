064eca0ac3e4202bfa4098a8e43bebee90bdc22d
Adil Wali <adil@merit.me>
2017-11-13 14:59:12 +0500

    Using -modal instead of .modal.

--- packages/lw-2/src/app/transact/send/confirm/send-confirm.module.ts
+++ packages/lw-2/src/app/transact/send/confirm/send-confirm.module.ts
@@ -6,7 +6,7 @@ import { ProfileService } from 'merit/core/profile.service';
 import { WalletService } from 'merit/wallets/wallet.service';
 import { WalletsModule } from "merit/wallets/wallets.module";
 import { NotificationService } from 'merit/shared/notification.service';
-import { FeeLevelModal } from 'merit/shared/fee/fee-level';
+import { FeeLevelModal } from 'merit/shared/fee/fee-level-modal';
 import { FeeService } from 'merit/shared/fee/fee.service';
 
 /*
