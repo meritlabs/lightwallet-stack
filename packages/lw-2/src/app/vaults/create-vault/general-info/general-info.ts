b66664620939b63f6bd53c731a59538780d33ce4
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-18 16:41:15 +0300

    choose from wallets and vaults for the whitelist

--- packages/lw-2/src/app/vaults/create-vault/general-info/general-info.ts
+++ packages/lw-2/src/app/vaults/create-vault/general-info/general-info.ts
@@ -5,6 +5,7 @@ import { IonicPage, NavController } from 'ionic-angular';
 import { CreateVaultService } from "merit/vaults/create-vault/create-vault.service";
 import { WalletService } from "merit/wallets/wallet.service";
 import { ProfileService } from "merit/core/profile.service";
+import { VaultsService } from 'merit/vaults/vaults.service';
 
 @IonicPage({
   defaultHistory: ['ProfileView']
@@ -24,6 +25,7 @@ export class CreateVaultGeneralInfoView {
     private createVaultService: CreateVaultService, 
     private profileService: ProfileService,
     private walletService: WalletService,
+    private vaultsService: VaultsService,
   ){}
 
   checkNextAvailable() {
@@ -38,14 +40,19 @@ export class CreateVaultGeneralInfoView {
     // fetch users wallets
     this.updateAllWallets().then((wallets) => {
       const walletDTOs = _.map(wallets, (w) => {
-        return { 'id': w.id, 'name': w.name, 'pubKey': w.credentials.xPubKey };
+        const name = w.name || w._id;
+        return { 'id': w.id, 'name': name, 'pubKey': w.credentials.xPubKey };
       });
       this.whitelistCandidates = this.whitelistCandidates.concat(walletDTOs);
     });
 
     // fetch users vaults
     this.updateAllWVaults().then((vaults) => {
-      this.whitelistCandidates = this.whitelistCandidates.concat(vaults);
+      const walletDTOs = _.map(vaults, (w) => {
+        const name = w.name || w._id;
+        return { 'id': w.id, 'name': name, 'pubKey': w.xPubKey };
+      });
+      this.whitelistCandidates = this.whitelistCandidates.concat(walletDTOs);
     });
   }
 
@@ -60,13 +67,18 @@ export class CreateVaultGeneralInfoView {
         wallet.status = await this.walletService.getStatus(wallet);
         return wallet; 
       }));
-    });
+    })
     return wallets;
   }
 
   private updateAllWVaults(): Promise<Array<any>> {
-    return new Promise((resolve, reject) => {
-      return resolve([]);
+    return this.profileService.getWallets().then((ws) => {
+      if (_.isEmpty(ws)) {
+        Promise.resolve(null); //ToDo: add proper error handling;
+      }
+      return _.head(ws);
+    }).then((walletClient) => {
+      return this.vaultsService.getVaults(walletClient);
     });
   }
 }
