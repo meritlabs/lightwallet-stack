d2de0d2c74f32ac41f2261ed844c126619af4ee7
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-21 23:48:51 +0300

    [WIP] display wallet images to distinguish vaults and wallets

--- packages/lw-2/src/app/vaults/vault-details/vault-details.ts
+++ packages/lw-2/src/app/vaults/vault-details/vault-details.ts
@@ -1,9 +1,17 @@
+import * as _ from "lodash";
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { VaultsService } from 'merit/vaults/vaults.service';
 import { BwcService } from 'merit/core/bwc.service';
 import { ProfileService } from 'merit/core/profile.service';
 import { Logger } from 'merit/core/logger';
+
+import { CreateVaultService } from "merit/vaults/create-vault/create-vault.service";
+import { WalletService } from "merit/wallets/wallet.service";
+import { ProfileService } from "merit/core/profile.service";
+import { VaultsService } from 'merit/vaults/vaults.service';
+import { BwcService } from 'merit/core/bwc.service';
 
 
 
@@ -20,21 +28,24 @@ export class VaultDetailsView {
 
   private bitcore:any;
   public vault: any;
-  public whitelist: Array<any>;
+  public whitelist: Array<any> = [];
+  private bitcore: any = null;
 
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public vaultsService: VaultsService,
-    public bwcService: BwcService,
-    public profileService: ProfileService,
-    private logger:Logger) {
+    private logger:Logger,
+    private profileService: ProfileService,
+    private walletService: WalletService,
+    private vaultsService: VaultsService,
+    private bwc: BwcService,
+  ) {
     // We can assume that the wallet data has already been fetched and 
     // passed in from the wallets (list) view.  This enables us to keep
     // things fast and smooth.  We can refresh as needed.
     this.vault = this.navParams.get('vault');
-    this.bitcore = bwcService.getBitcore();
+    this.bitcore = this.bwc.getBitcore();
     this.whitelist = this.vault.whitelist;
     console.log("Inside the vault-details view.");
     console.log(this.vault);
@@ -50,7 +61,35 @@ export class VaultDetailsView {
   ionViewDidLoad() {
     console.log("Vault-Detail View Did Load.");
     console.log(this.vault);
-    //do something here
+
+    Promise.all([
+      this.getAllWallets().then((wallets) => {
+        return _.map(wallets, (w) => {
+          const name = w.name || w._id;
+          return { 'id': w.id, 'name': name, 'pubKey': w.credentials.xPubKey, 'type': 'wallet' };
+        });
+
+
+        
+      }), 
+      // fetch users vaults
+      this.getAllWVaults().then((vaults) => {
+        return _.map(vaults, (v) => {
+          const name = v.name || v._id;
+          const key = new this.bitcore.Address(v.address).toString();
+          return { 'id': v._id, 'name': name, 'pubKey': key, 'type': 'vault' }; 
+        });
+      }),
+    ]).then((arr: Array<Array<any>>) => {
+      const whitelistCandidates = _.flatten(arr);
+      _.each(this.vault.whitelist, (wl) => {
+        const found = _.find(whitelistCandidates, { pubKey: wl });
+        if (found) {
+          this.whitelist.push(found);
+        }
+      });
+    });
+
     this.profileService.getHeadWalletClient().then((walletClient) => {
       this.vaultsService.getVaultCoins(walletClient, this.vault).then((coins) => {
 
@@ -69,6 +108,27 @@ export class VaultDetailsView {
         console.log(tx);
         console.log(tx.serialize());
       });
+    });
+  }
+
+  private getAllWallets(): Promise<Array<any>> {
+    const wallets = this.profileService.getWallets().then((ws) => {
+      return Promise.all(_.map(ws, async (wallet:any) => {
+        wallet.status = await this.walletService.getStatus(wallet);
+        return wallet; 
+      }));
+    })
+    return wallets;
+  }
+
+  private getAllWVaults(): Promise<Array<any>> {
+    return this.profileService.getWallets().then((ws) => {
+      if (_.isEmpty(ws)) {
+        Promise.resolve(null); //ToDo: add proper error handling;
+      }
+      return _.head(ws);
+    }).then((walletClient) => {
+      return this.vaultsService.getVaults(walletClient);
     });
   }
 }
