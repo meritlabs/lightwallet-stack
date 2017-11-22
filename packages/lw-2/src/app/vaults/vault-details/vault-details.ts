00537d23b9cf69f79a6e193ee09ce111b644367b
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-22 16:24:54 +0300

    fix types

--- packages/lw-2/src/app/vaults/vault-details/vault-details.ts
+++ packages/lw-2/src/app/vaults/vault-details/vault-details.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import * as Promise from 'bluebird'; 
 
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
