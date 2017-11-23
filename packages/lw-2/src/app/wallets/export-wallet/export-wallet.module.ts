c860bbce1c364be392d1fbc11368f70447b61863
fedor <f.avetisov@gmail.com>
2017-11-23 13:40:24 +0300

    added edit-wallet views

---
+++ packages/lw-2/src/app/wallets/export-wallet/export-wallet.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ExportWalletView } from './export-wallet';
+
+@NgModule({
+  declarations: [
+    ExportWalletView,
+  ],
+  imports: [
+    IonicPageModule.forChild(ExportWalletView),
+  ],
+})
+export class $$moduleName$$Module {}
---
+++ packages/lw-2/src/app/wallets/set-wallet-password/set-wallet-password.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SetWalletPasswordView } from './set-wallet-password';
+
+@NgModule({
+  declarations: [
+    SetWalletPasswordView,
+  ],
+  imports: [
+    IonicPageModule.forChild(SetWalletPasswordView),
+  ],
+})
+export class $$moduleName$$Module {}
