e3b15e55711fbc7c5cf19c55f03d6f8e19b951db
fedor <f.avetisov@gmail.com>
2017-11-23 15:25:02 +0300

    added export-wallet view

--- packages/lw-2/src/app/wallets/export-wallet/export-wallet.module.ts
+++ packages/lw-2/src/app/wallets/export-wallet/export-wallet.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ExportWalletView } from './export-wallet';
+import { QRCodeModule } from 'angular2-qrcode';
+
 
 @NgModule({
   declarations: [
     ExportWalletView,
   ],
   imports: [
+    QRCodeModule, 
     IonicPageModule.forChild(ExportWalletView),
   ],
 })
