14685d066cc62834c9c46413da80e5badb2ca6c2
Josh Ferrerra <josh@merit.me>
2017-11-27 14:59:03 -0800

    fix broken imports for easysend service

--- packages/lw-2/src/app/transact/send/easy-send/easy-send.service.ts
+++ packages/lw-2/src/app/transact/send/easy-send/easy-send.service.ts
@@ -2,7 +2,6 @@ import * as Promise from 'bluebird';
 import { Injectable } from '@angular/core';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { MeritWalletClient } from 'src/lib/merit-wallet-client';
-import { BwcError } from 'src/lib/merit-wallet-client/lib/errors';
 import { EasySend } from 'merit/transact/send/easy-send/easy-send.model';
 
 @Injectable()
@@ -15,7 +14,7 @@ export class EasySendService {
     // TODO: get a passphrase from the user
     let opts = {
       network: wallet.network,
-      passphrase: 'donkey'
+      passphrase: ''
     };
 
     return wallet.buildEasySendScript(opts).then((easySend) => {
@@ -35,13 +34,13 @@ export class EasySendService {
         return Promise.resolve(easySend);
       });
     }).catch((err) => {
-      return Promise.reject(new BwcError('error building easysend script' + err));
+      return Promise.reject(new Error('error building easysend script' + err));
     });
   }
 
   public sendSMS(phoneNumber: string, url: string): Promise<any> {
     return Promise.resolve(this.socialSharing.shareViaSMS(url, phoneNumber)).catch((err) => {
-      return Promise.reject(new BwcError('error sending sms: ' + err));
+      return Promise.reject(new Error('error sending sms: ' + err));
     });
   }
 
@@ -49,7 +48,7 @@ export class EasySendService {
     return Promise.resolve().then(this.socialSharing.canShareViaEmail).then(() => {
       return this.socialSharing.shareViaEmail(url, 'Someone sent you some Merit', [emailAddress]);
     }).catch((err) => {
-      return Promise.reject(new BwcError('error sending email: ' + err));
+      return Promise.reject(new Error('error sending email: ' + err));
     })
   }
 }
