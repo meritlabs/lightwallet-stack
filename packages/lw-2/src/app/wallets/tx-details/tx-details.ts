adc8cd26e6db80bfde4aa6e571ba397d004a0de3
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-11-30 17:54:05 +0300

    display amounts and fiat amount on tx details screen for vaults
    also fixes image paths for tx actions

--- packages/lw-2/src/app/wallets/tx-details/tx-details.ts
+++ packages/lw-2/src/app/wallets/tx-details/tx-details.ts
@@ -7,6 +7,9 @@ import { WalletService } from "merit/wallets/wallet.service";
 import { ProfileService } from "merit/core/profile.service";
 import { Logger } from "merit/core/logger";
 import { VaultsService } from "merit/vaults/vaults.service";
+import { BwcService } from 'merit/core/bwc.service';
+import { FiatAmount } from 'merit/shared/fiat-amount.model';
+import { RateService } from 'merit/transact/rate.service';
 
 const CONFIRMATION_THRESHOLD = 6;
 
@@ -22,6 +25,9 @@ export class TxDetailsView {
   public wallet: any;
   public tx: any;
   public confirmations: string;
+  public vault: any;
+  private bitcore: any;
+  public amountStr: string;
   
   constructor(
     private navParams: NavParams,
@@ -30,8 +36,13 @@ export class TxDetailsView {
     private vaultService: VaultsService,
     private profileService: ProfileService,
     private logger:Logger,
+    private bws: BwcService,
+    private rateService: RateService,
   ) {
     this.wallet = this.walletService.getWallet(this.navParams.get('walletId'));
+    this.vault = this.navParams.get('vault');
+    this.bitcore = this.bws.getBitcore();
+    
     this.tx = {};
     this.confirmations = null;
   }
@@ -42,19 +53,25 @@ export class TxDetailsView {
 
     if (vault) {
       console.log('it`s vault TX');
-      let finish = (list: any) => {
+      const finish = (list: any) => {
         console.log('searching list', list, txId);
         let tx = _.find(list, {
           txid: txId
         });
 
         if (!tx) throw new Error('Could not get transaction');
-        return tx;
+        return this.processTx(tx);
       };
 
       this.vaultService.getVaultTxHistory(this.wallet, vault).then((txs) => {
         const tx = finish(txs);
         this.tx = tx;
+        if (this.tx.action == 'sent') this.title = 'Sent Funds';
+        if (this.tx.action == 'received') this.title = 'Received Funds';
+        if (this.tx.action == 'moved') this.title = 'Moved Funds';
+
+        if (this.tx.safeConfirmed) this.confirmations = this.tx.safeConfirmed;
+        else if (this.tx.confirmations > CONFIRMATION_THRESHOLD)  this.confirmations = `${CONFIRMATION_THRESHOLD}+`;
       });
     } else {
       this.walletService.getTx(this.wallet, txId).then((tx) => {
@@ -78,6 +95,25 @@ export class TxDetailsView {
 
   private viewOnBlockchain() {
     this.logger.warn("Viewing on blockchain is not yet implemented and up for discussion.")
+  }
+
+  private processTx(tx: any): any {
+    console.log('this', this);
+    const thisAddr = new this.bitcore.Address(this.vault.address).toString();
+    const summ = _.reduce(tx.outputs, (acc: number, output: any) => {
+      if (output.address != thisAddr) {
+        return acc;
+      }
+      return acc + output.amount;
+    }, 0);
+    console.log('output summ', summ);
+    tx.amount = summ;
+    const amountStr = this.txFormatService.formatAmountStr(summ);
+    tx.amountStr = amountStr;
+    this.amountStr = amountStr;
+    tx.altAmount = this.rateService.toFiat(tx.amount, this.wallet.cachedStatus.alternativeIsoCode);
+    tx.altAmountStr = new FiatAmount(tx.altAmount);
+    return tx;
   }
 
 }
