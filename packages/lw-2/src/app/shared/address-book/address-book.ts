42b8e6203f972f02d2d92e3432cfb8a39b82f37a
Josh Ferrerra <josh@merit.me>
2017-12-04 16:34:26 -0800

    update send view to use united MeritContact

--- packages/lw-2/src/app/shared/address-book/address-book.ts
+++ packages/lw-2/src/app/shared/address-book/address-book.ts
@@ -3,10 +3,8 @@ import * as Promise from 'bluebird';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AddressBookService } from 'merit/shared/address-book/address-book.service';
-//import { AddressBook, MeritContact } from 'merit/shared/address-book/contact/contact.model';
 import { Logger } from 'merit/core/logger';
 import { Contact } from '@ionic-native/contacts';
-//import {emptyMeritContact} from "./contact/contact.model";
 import { MeritContact } from "merit/shared/address-book/merit-contact.model";
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -22,9 +20,9 @@ export class AddressBookView {
 
   loading:boolean;
 
-  contacts:Array<MeritContact> = [];
-  filteredContacts:Array<MeritContact> = [];
-  renderingContacts:Array<MeritContact> = [];
+  private contacts:Array<MeritContact> = [];
+  private filteredContacts:Array<MeritContact> = [];
+  private renderingContacts:Array<MeritContact> = [];
 
   searchQuery:string = '';
 
@@ -43,81 +41,29 @@ export class AddressBookView {
   ionViewWillEnter() {
     this.loading = true;
     this.updateContacts().then(() => {
-      this.filterContacts();
       this.loading = false;
-    })
-  }
-
-  private updateContacts() {
-    return new Promise((resolve, reject) => {
-
-      this.contactsOffset = 0;
-
-      this.addressBookService.getAddressbook('testnet').then((addressBook) => {
-
-        this.addressBookService.getAllDeviceContacts().then((deviceContacts) => {
-
-          let contacts = deviceContacts.map((contact) => new MeritContact(contact));
-          contacts = contacts.concat( _.map(addressBook, value => value) );
-
-          contacts.sort((a,b) => {
-            if ((a.meritAddresses.length && b.meritAddresses.length) || (a.meritAddresses.length && b.meritAddresses.length)) {
-              return a.name.formatted > b.name.formatted ? 1 : -1;
-            } else {
-              return a.meritAddresses.length ? -1 : 1;
-            }
-          });
-
-          this.contacts = contacts;
-          this.filterContacts();
-          resolve();
-        });
-
-      });
-
     });
   }
 
-  private filterContacts() {
+  private updateContacts(): Promise<void> {
     this.contactsOffset = 0;
 
-    if (!this.searchQuery) {
-      this.filteredContacts = this.contacts;
-      return this.renderingContacts = this.filteredContacts.splice(this.contactsOffset, this.contactsLimit);
-    }
-
-
-    let exp = new RegExp(this.searchQuery, 'ig');
-    this.filteredContacts = this.contacts.filter((contact) => {
-
-      let hasMatches = false;
-
-      if (contact.name.formatted.match(exp)) hasMatches = true;
-
-      contact.emails.forEach((email) => {
-        if (email.value.match(exp)) hasMatches =  true;
-      });
-
-      contact.phoneNumbers.forEach((phone) => {
-        if (phone.value.match(exp)) hasMatches = true;
-      });
-
-      contact.meritAddresses.forEach((address) => {
-        if (address.address.match(exp)) hasMatches = true;
-      });
-
-      return hasMatches;
-
+    return this.addressBookService.getAllMeritContacts().then((contacts) => {
+      this.contacts = contacts;
+      this.filterContacts();
     });
+  }
 
+  private filterContacts(): void{
+    this.contactsOffset = 0;
+    this.filteredContacts = this.addressBookService.searchContacts(this.contacts, this.searchQuery);
     this.renderingContacts = this.filteredContacts.splice(0, this.contactsLimit);
-
   }
 
   renderMoreContacts(infiniteScroll) {
     console.log('RENDERING MORE CONTACTS');
     this.contactsOffset += this.contactsLimit;
-    this.renderingContacts = this.renderingContacts.concat(this.filterContacts().splice(this.contactsOffset, this.contactsOffset+this.contactsLimit));
+    this.renderingContacts = this.renderingContacts.concat(this.filteredContacts.splice(this.contactsOffset, this.contactsOffset+this.contactsLimit));
     infiniteScroll.complete();
   }
 
@@ -128,7 +74,6 @@ export class AddressBookView {
 
   doRefresh(refresher) {
     this.updateContacts().then(() => {
-      this.filterContacts();
       refresher.complete()
     });
   }
