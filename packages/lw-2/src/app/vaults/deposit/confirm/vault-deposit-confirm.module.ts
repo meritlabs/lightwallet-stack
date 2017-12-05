6114dc446b9c067910f0b74691addc239da7bf5e
Eugene Sypachev (@Axblade) <eugene@merit.me>
2017-12-05 20:44:52 +0300

    summary screen works now

--- packages/lw-2/src/app/vaults/deposit/confirm/vault-deposit-confirm.module.ts
+++ packages/lw-2/src/app/vaults/deposit/confirm/vault-deposit-confirm.module.ts
@@ -7,6 +7,14 @@ import { SharedModule } from 'merit/shared/shared.module';
 import { VaultsService } from 'merit/vaults/vaults.service';
 import { MomentModule } from 'angular2-moment';
 
+import { ProfileService } from 'merit/core/profile.service';
+import { WalletService } from 'merit/wallets/wallet.service';
+import { WalletsModule } from "merit/wallets/wallets.module";
+import { NotificationService } from 'merit/shared/notification.service';
+import { FeeLevelModal } from 'merit/shared/fee/fee-level-modal';
+import { FeeService } from 'merit/shared/fee/fee.service';
+import { EasySendService } from 'merit/transact/send/easy-send/easy-send.service';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /*
   ToDo: Work to get this lazy-loadable as possible. 
@@ -17,8 +25,15 @@ import { MomentModule } from 'angular2-moment';
   ],
   imports: [
     IonicPageModule.forChild(VaultDepositConfirmView),
+    SharedModule,
   ],
   providers: [
+    WalletService,
+    NotificationService,
+    EasySendService,
+    SocialSharing,
+    FeeService,
+    FeeLevelModal,
   ]
 })
 export class VaultDepositConfirmModule {}
