69a7c6c3e8125266fd18e02fb7c4f01ae2f7fb6e
Josh Ferrerra <josh@merit.me>
2017-12-06 09:57:49 -0800

    WIP getting merit address to store on device contact

--- packages/lw-2/src/app/shared/address-book/merit-contact.service.ts
+++ packages/lw-2/src/app/shared/address-book/merit-contact.service.ts
@@ -1,8 +1,11 @@
 import * as _ from 'lodash';
+import * as Promise from 'bluebird';
 import { Injectable } from '@angular/core';
-import { Contacts, Contact as DeviceContact, IContactProperties } from '@ionic-native/contacts';
+import { Contacts, Contact as DeviceContact, IContactProperties, IContactField } from '@ionic-native/contacts';
 import { MeritContact } from 'merit/shared/address-book/merit-contact.model';
 import { AddressBookService } from 'merit/shared/address-book/address-book.service';
+import { BwcService } from 'merit/core/bwc.service';
+import { Logger } from 'merit/core/logger';
 
 /**
  * Creates MeritContact model and injects native contact in it
@@ -10,15 +13,19 @@ import { AddressBookService } from 'merit/shared/address-book/address-book.servi
 
 @Injectable()
 export class MeritContactService {
+  private bitcore;
 
   constructor(
-    private addressBookService:AddressBookService
+    private addressBookService:AddressBookService,
+    private bwcService: BwcService,
+    private logger: Logger
   ) {
+    this.bitcore = bwcService.getBitcore();
   }
 
-  private updateModel(contact) {
+  private updateModel(contact: MeritContact) {
 
-    contact.nativeModel.name = { 'formatted': contact.name.formatted, 'givenName': contact.name.formatted };
+    contact.nativeModel.name = contact.name;
     contact.nativeModel.emails = contact.emails;
     contact.nativeModel.phoneNumbers = contact.phoneNumbers;
     contact.nativeModel.urls = contact.urls;
@@ -30,8 +37,9 @@ export class MeritContactService {
     }
 
     if (contact.storeOnDevice) {
+      this.updateURLs(contact);
       this.updateModel(contact);
-      return contact.nativeModel.save();
+      return Promise.resolve(contact.nativeModel.save());
     } else {
       let address = contact.meritAddresses[0].address;
       let network = contact.meritAddresses[0].network;
@@ -50,13 +58,37 @@ export class MeritContactService {
     let network = contact.meritAddresses[0].network;
     return this.addressBookService.remove(address, network).then(() => {
       if (contact.storeOnDevice) {
+        this.updateURLs(contact);
         this.updateModel(contact);
-        return contact.nativeModel.save();
+        this.logger.info('attempting to save contact to device');
+        this.logger.info(contact.nativeModel);
+        return Promise.resolve(contact.nativeModel.save());
       } else {
         return this.addressBookService.add(contact, address, network);
       }
     });
   }
 
+  private updateURLs(contact:MeritContact) {
+    if(_.isEmpty(contact.meritAddresses)) return contact;
+    let address = _.head(contact.meritAddresses).address;
+    let network = _.head(contact.meritAddresses).network || this.bitcore.Address.fromString(address).network;
+    let urlObj = {
+      type: 'other',
+      value: `merit:${address}:${network}`
+    }
+    for(let i = 0; i < contact.urls.length; i++) {
+      if(contact.urls[i].value.indexOf('merit:') == 0) {
+        this.logger.info('replacing url with')
+        this.logger.info(urlObj)
+        contact.urls[i] = urlObj;
+        return contact;
+      }
+    }
+    this.logger.info('inserting url with')
+    this.logger.info(urlObj)
+    contact.urls.push(urlObj);
+    return contact;
+  }
 
 }
