ab704d8e45e119630a63428ef880d841341f8484
Adil Wali <adil@merit.me>
2017-12-08 13:30:13 -0800

    Consistent handling of address params for insight-API.

--- packages/insight-api/lib/wallet.js
+++ packages/insight-api/lib/wallet.js
@@ -11,17 +11,14 @@ function WalletController(node) {
 WalletController.prototype.getANV = function(req, res) {
   var self = this;
   // If there are no keys passed in, the ANV is zero.
-  if (_.isEmpty(req.body.keys)) return res.jsonp(0);
+  if (_.isEmpty(req.body.addresses)) return res.jsonp(0);
 
-  var keys = req.body.keys.join(" ");;
+  var addresses = req.body.addresses;
 
-  self.node.services.meritd.getANV(keys, function(err, result) {
+  self.node.services.meritd.getANV(addresses, function(err, result) {
     if(err) {
       return self.common.handleErrors(err, res);
     }
-
-    console.log("Insight API responds to getANV with: ", result);
-    
     res.jsonp(result);
   });
 };
