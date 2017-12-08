9a52352c2ea2590688416f9dbafc827743618652
Adil Wali <adil@merit.me>
2017-12-07 19:52:08 -0800

    Using ngZone to effectively update the view when ANV and rewards are calculated.

--- packages/lw-2/src/app/community/network/network.ts
+++ packages/lw-2/src/app/community/network/network.ts
@@ -9,9 +9,10 @@ import { MeritToastController } from "merit/core/toast.controller";
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { WalletService } from "merit/wallets/wallet.service";
 import { TxFormatService } from "merit/transact/tx-format.service";
-import * as Promise from 'bluebird'; 
+import * as Promise from 'bluebird';
 import { MeritWalletClient } from 'src/lib/merit-wallet-client';
 import { Logger } from 'merit/core/logger';
+import { NgZone } from '@angular/core/src/zone/ng_zone';
 
 
 
@@ -24,18 +25,19 @@ import { Logger } from 'merit/core/logger';
 })
 export class NetworkView {
 
-  public wallets:MeritWalletClient[];
+  public wallets: MeritWalletClient[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private profileService:ProfileService,
-    private clipboard:Clipboard,
-    private toastCtrl:MeritToastController,
+    private profileService: ProfileService,
+    private clipboard: Clipboard,
+    private toastCtrl: MeritToastController,
     private socialSharing: SocialSharing,
-    private walletService:WalletService,
-    private txFormatService:TxFormatService,
-    private logger: Logger
+    private walletService: WalletService,
+    private txFormatService: TxFormatService,
+    private logger: Logger,
+    private zone: NgZone
   ) {
 
   }
@@ -43,23 +45,35 @@ export class NetworkView {
   ionViewDidLoad() {
     //do something here
     this.profileService.getWallets().then((wallets: MeritWalletClient[]) => {
-      
+
       return Promise.map(wallets, (wallet: MeritWalletClient) => {
         return this.walletService.getANV(wallet).then((anv) => {
           return wallet.totalNetworkValue = this.txFormatService.parseAmount(anv, 'MRT').amountUnitStr;
         }).then(() => {
           return this.walletService.getRewards(wallet).then((data) => {
-            wallet.miningRewards = this.txFormatService.parseAmount(data.mining, 'MRT').amountUnitStr;
-            wallet.ambassadorRewards = this.txFormatService.parseAmount(data.ambassador, 'MRT').amountUnitStr;
+            this.logger.warn("Got Rewards in network view with: ", data);
+            // If we cannot properly fetch data, let's return wallets as-is.  
+            if (data && data.mining) {
+              wallet.miningRewards = this.txFormatService.parseAmount(data.mining, 'MRT').amountUnitStr;
+            }
+            if (data && data.ambassador) {
+
+              wallet.ambassadorRewards = this.txFormatService.parseAmount(data.ambassador, 'MRT').amountUnitStr;
+            }
             return wallet;
           });
         });
       }).then((processedWallets: MeritWalletClient[]) => {
-        this.wallets = processedWallets;
+        this.logger.info("What are the processed wallets?: ", processedWallets);
+        // We must explicitly run this in the angular zone because it's a change on 
+        // the MWC class.
+        this.zone.run(() => {
+          this.wallets = processedWallets;
+        });
       });
     });
 
-    
+
 
   }
 
@@ -74,11 +88,11 @@ export class NetworkView {
   }
 
   shareCode(code) {
-    this.socialSharing.share('My invite code to use Merit: '+code);
+    this.socialSharing.share('My invite code to use Merit: ' + code);
   }
 
   shareAddress(address) {
-    this.socialSharing.share('merit'+address);
+    this.socialSharing.share('merit' + address);
   }
 
 
