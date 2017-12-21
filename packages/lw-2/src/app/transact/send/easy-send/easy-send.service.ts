3c1a505cea5ffed7048dfe4cb9f9760ac63cbb10
Josh Ferrerra <josh@merit.me>
2017-12-21 10:35:20 -0800

    add storage methods to easySend service

--- packages/lw-2/src/app/transact/send/easy-send/easy-send.service.ts
+++ packages/lw-2/src/app/transact/send/easy-send/easy-send.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@angular/core';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { MeritWalletClient } from 'src/lib/merit-wallet-client';
 import { EasySend } from 'merit/transact/send/easy-send/easy-send.model';
+import { PersistenceService } from 'merit/core/persistence.service';
 
 @Injectable()
 export class EasySendService {
   constructor(
+    private persistenceService: PersistenceService,
     private socialSharing: SocialSharing
   ) {}
   public createEasySendScriptHash(wallet: MeritWalletClient): Promise<EasySend> {
@@ -56,10 +58,38 @@ export class EasySendService {
 
   public sendEmail(emailAddress: string, amountMrt:string, url: string): Promise<any> {
     return Promise.resolve(this.socialSharing.canShareViaEmail()).then(() => {
-      const message:string = `I just sent you ${amountMrt} Merit!  Merit is a new digital currency, and if you don't have a Merit Wallet yet, you can easily make one to claim the money. \n \n Here is the link to claim the Merit: \n \n ${url}`
+      const message:string =
+        `I just sent you ${amountMrt} Merit! ` +
+        `Merit is a new digital currency, and if you don't have a Merit Wallet yet, ` +
+        `you can easily make one to claim the money. \n \n` +
+        `Here is the link to claim the Merit: \n \n ${url}`
       return this.socialSharing.shareViaEmail(message, `Here is ${amountMrt} Merit!`, [emailAddress]);
     }).catch((err) => {
       return Promise.reject(new Error('error sending email: ' + err));
     })
+  }
+
+  public updatePendingEasySends(wallet: MeritWalletClient): Promise<void> {
+    return this.persistenceService.getPendingEasySends(wallet.id)
+      .filter((easySend: EasySend) => {
+        return wallet.validateEasyScript(easySend.script.toAddress().toString()).then((txn) => {
+          return txn.result.found && !txn.result.spent;
+        })
+      })
+      .then((pendingEasySends: EasySend[]) => {
+        return this.persistenceService.setPendingEasySends(wallet.id, pendingEasySends);
+      });
+  }
+
+  private storeEasySend(walletId: string, easySend: EasySend): Promise<void> {
+    return this.persistenceService.getPendingEasySends(walletId)
+      .then((history: EasySend[]) => {
+        history = history || [];
+        history.push(easySend);
+        return history;
+      })
+      .then((newHistory: EasySend[]) => {
+        return this.persistenceService.setPendingEasySends(walletId, newHistory);
+      });
   }
 }
