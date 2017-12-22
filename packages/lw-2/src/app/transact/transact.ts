51ddc2aa1d611dc09fbe7db5012b206e1bd04ea8
fedor <f.avetisov@gmail.com>
2017-12-22 18:02:37 +0300

    added authentication check in transact view

--- packages/lw-2/src/app/transact/transact.ts
+++ packages/lw-2/src/app/transact/transact.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Tabs } from 'ionic-angular';
 import { Logger } from 'merit/core/logger';
+import { ProfileService } from 'merit/core/profile.service';
 
 // Transact is the proposed name of the umbrella for the primary actions 
 // That exist through the tabs on the bottom of the screen. 
@@ -25,9 +26,15 @@ export class TransactView {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private logger: Logger
+    private logger: Logger,
+    private profileService: ProfileService
   ) {
     this.logger.info("Hello TRANSACT VIEW!");
+  }
+
+  public ionViewCanEnter() {
+    let profile = this.profileService.profile;
+    return (profile &&  profile.credentials && profile.credentials.length > 0);
   }
 
 
