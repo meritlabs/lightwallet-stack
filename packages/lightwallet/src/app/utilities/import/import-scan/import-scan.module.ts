2d8570e5262a575708ea37f7edeef398391652c4
Ibby Hadeed <ibby93@gmail.com>
2018-01-09 07:29:39 -0500

    Change the way we scan barcodes to avoid NavCtrl issues

--- packages/lightwallet/src/app/utilities/import/import-scan/import-scan.module.ts
+++ packages/lightwallet/src/app/utilities/import/import-scan/import-scan.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ImportScanView } from 'merit/utilities/import/import-scan/import-scan';
 
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-
-
 @NgModule({
   declarations: [
     ImportScanView,
-  ],
-  providers: [
-    BarcodeScanner
   ],
   imports: [
     IonicPageModule.forChild(ImportScanView),
--- packages/lightwallet/src/app/utilities/import/import.module.ts
+++ packages/lightwallet/src/app/utilities/import/import.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { IonicPageModule } from 'ionic-angular';
+import { AddressScannerService } from 'merit/utilities/import/address-scanner.service';
 import { ImportView } from 'merit/utilities/import/import';
 import { DerivationPathService } from "merit/utilities/mnemonic/derivation-path.service";
 import { WalletService } from "merit/wallets/wallet.service";
@@ -15,7 +17,9 @@ import { MnemonicService } from "merit/utilities/mnemonic/mnemonic.service";
   providers: [
     DerivationPathService,
     WalletService,
-    MnemonicService
+    MnemonicService,
+    BarcodeScanner,
+    AddressScannerService
   ]
 })
 export class ImportViewModule {}
