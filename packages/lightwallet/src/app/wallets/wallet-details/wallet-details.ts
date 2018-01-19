eb27d49a673751a67b74e769cfc59f4b1d1a26ee
Ibby Hadeed <ibby93@gmail.com>
2018-01-19 03:26:27 -0500

    Add history page

--- packages/lightwallet/src/app/wallets/wallet-details/wallet-details.ts
+++ packages/lightwallet/src/app/wallets/wallet-details/wallet-details.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import * as _ from 'lodash';
 import { Logger } from 'merit/core/logger';
 import { WalletService } from 'merit/wallets/wallet.service';
 import { MeritWalletClient } from '../../../lib/merit-wallet-client/index';
+import { formatWalletHistory } from '../../../utils/transactions';
 
 
 @IonicPage({
@@ -57,47 +57,12 @@ export class WalletDetailsView {
 
   private getWalletHistory(force: boolean = false): void {
     this.walletService.getTxHistory(this.wallet, { force: force }).then((walletHistory) => {
-      this.wallet.completeHistory = this.formatWalletHistory(walletHistory);
+      this.wallet.completeHistory = formatWalletHistory(walletHistory);
     }).catch((err) => {
       this.logger.info(err);
     });
   }
 
-  private formatWalletHistory(wh: any[]): any[] {
-    if (!_.isEmpty(wh)) {
-      return _.map(wh, (h: any) => {
-        if (!_.isNil(h) && !_.isNil(h.action)) {
-          const pendingString = h.isPendingEasySend ? '(pending) ' : '';
-          switch (h.action) {
-            case 'sent':
-              if (h.confirmations == 0) {
-                h.actionStr = 'Sending Payment...';
-              } else {
-                h.actionStr = 'Payment Sent';
-              }
-              break;
-            case 'received':
-              if (h.confirmations == 0) {
-                h.actionStr = 'Receiving Payment...';
-              } else {
-                h.actionStr = 'Payment Received';
-              }
-              break;
-            case 'moved':
-              h.actionStr = 'Moved Merit';
-              break;
-            default:
-              h.actionStr = 'Recent Transaction';
-              break
-          }
-          h.actionStr = pendingString + h.actionStr;
-        }
-        return h;
-      });
-    } else {
-      return [];
-    }
-  }
 
   // add up to the total balance in status.
   private txHistoryInSyncWithStatus(): boolean {
