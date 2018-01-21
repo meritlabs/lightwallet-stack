18211290f6982d70647362cce07e86913652bcab
Ibby Hadeed <ibby93@gmail.com>
2018-01-21 03:28:18 -0500

    Update history page

--- packages/lightwallet/src/pages/history/history.ts
+++ packages/lightwallet/src/pages/history/history.ts
@@ -17,14 +17,28 @@ export class HistoryView {
   constructor(private walletService: WalletService,
               private profileService: ProfileService) {}
 
+  async ngOnInit() {
+    await this.loadData();
+  }
+
+  async refresh(refresher: any) {
+    try {
+      await this.loadData();
+    } catch (e) {}
+
+    refresher.complete();
+  }
+
   async loadData() {
     const wallets = await this.profileService.getWallets();
-    const transactions = wallets.map((wallet: MeritWalletClient) =>
+    const transactions = await Promise.all(wallets.map((wallet: MeritWalletClient) =>
       this.walletService.getTxHistory(wallet)
-    );
+    ));
 
     // TODO sort by transaction date
-    this.transactions = sortBy([].concat.call(this, transactions), '');
+    this.transactions = sortBy(Array.prototype.concat.apply([], transactions), '');
+
+    console.log('Transactions are ', this.transactions);
   }
 
 }
