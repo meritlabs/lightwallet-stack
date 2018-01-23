e120fc23cff4f9298ef50733dbce3c38ce514be0
Ibby Hadeed <ibby93@gmail.com>
2018-01-23 17:48:49 -0500

    Add temporary functionality for deposit + send buttons

--- packages/lightwallet/src/app/transact/receive/receive.ts
+++ packages/lightwallet/src/app/transact/receive/receive.ts
@@ -42,9 +42,6 @@ export class ReceiveView {
   error: string;
   mainAddressGapReached: boolean;
 
-  hasUnlockedWallets:boolean;
-  loading:boolean;
-
   constructor(private navCtrl: NavController,
               private navParams: NavParams,
               private modalCtrl: ModalController,
