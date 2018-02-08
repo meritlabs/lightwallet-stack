a6429a0f52cb03afc5e381cb76a89e7b4080160f
Ibby Hadeed <ibby93@gmail.com>
2018-02-08 04:03:34 -0500

    Show message when there are no active/confirmed requests
    Helps new users know where to find the information in the future

--- packages/lightwallet/src/pages/unlock-requests/unlock-requests.module.ts
+++ packages/lightwallet/src/pages/unlock-requests/unlock-requests.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UnlockRequestsView } from './unlock-requests';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
     UnlockRequestsView,
   ],
   imports: [
-    IonicPageModule.forChild(UnlockRequestsView),
+    IonicPageModule.forChild(UnlockRequestsView)
   ],
 })
 export class UnlockRequestsModule {}
