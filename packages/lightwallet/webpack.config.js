5d0fb2c7c52da40a79618a641948e528223bd0d5
fedor <f.avetisov@gmail.com>
2018-02-15 01:24:51 +0300

    added environment config

--- packages/lightwallet/webpack.config.js
+++ packages/lightwallet/webpack.config.js
@@ -1,13 +1,45 @@
-// The below config override allows Ionic Serve to follow the the alias of 'merit/' --> ./src/app
-// This enables us to do all imports relative to the appRoot.
+var chalk = require("chalk");
+var fs = require('fs');
 var path = require('path');
 var tsconfig = require('./tsconfig.json');
-var webpackConfig = require('@ionic/app-scripts/config/webpack.config');
+var webpackConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-webpackConfig.dev.resolve = webpackConfig.prod.resolve = {
+var env = process.env.IONIC_ENV;
+console.log('environment setting is: ', env);
+
+webpackConfig.prod.resolve = {
+    alias: {
+        "@app/env": path.resolve(environmentPath('prod')),
+        'merit': path.resolve(tsconfig.compilerOptions.baseUrl, './src/app')
+    },
     extensions: ['.ts', '.js', '.json'],
-    modules: [path.resolve('node_modules')],
-    alias: {'merit': path.resolve(tsconfig.compilerOptions.baseUrl, './src/app')}
+    modules: [path.resolve('node_modules')]
 };
 
-module.exports = webpackConfig;
+webpackConfig.dev.resolve = {
+    alias: {
+        "@app/env": path.resolve(environmentPath('dev')),
+        'merit': path.resolve(tsconfig.compilerOptions.baseUrl, './src/app')
+    },
+    extensions: ['.ts', '.js', '.json'],
+    modules: [path.resolve('node_modules')]
+};
+
+if (env !== 'prod' && env !== 'dev') {
+    // Default to dev config
+    webpackConfig[env] = webpackConfig.dev;
+}
+
+function environmentPath(env) {
+    var filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+    //var filePath = './src/environments/environment.ts';
+    if (!fs.existsSync(filePath)) {
+        console.log(chalk.red('\n' + filePath + ' does not exist!'));
+    } else {
+        return filePath;
+    }
+}
+
+module.exports = function () {
+    return webpackConfig;
+};
