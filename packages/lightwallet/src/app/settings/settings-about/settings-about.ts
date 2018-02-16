fb0c096723512f9265e5cb8701bc4c1895f34a21
Ibby Hadeed <ibby93@gmail.com>
2018-02-16 03:12:23 -0500

    Set commit hash in build time + related fixes

--- packages/lightwallet/src/app/settings/settings-about/settings-about.ts
+++ packages/lightwallet/src/app/settings/settings-about/settings-about.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage } from 'ionic-angular';
 import { AppService } from 'merit/core/app-settings.service';
+declare const WEBPACK_CONFIG: any;
 
 @IonicPage()
 @Component({
@@ -10,55 +11,37 @@ import { AppService } from 'merit/core/app-settings.service';
 })
 export class SettingsAboutView {
 
-  public version: string;
-  public commitHash: string;
-  public repoUrl: string;
+  version: string;
+  commitHash: string = typeof WEBPACK_CONFIG !== 'undefined'? WEBPACK_CONFIG.COMMIT_HASH : '1234';
+  repoUrl: string;
 
-  constructor(private navCtrl: NavController,
-              private navParams: NavParams,
-              private  alertCtrl: AlertController,
+  constructor(private  alertCtrl: AlertController,
               private inAppBrowser: InAppBrowser,
-              private appSettingsService: AppService) {
-
-  }
+              private appSettingsService: AppService) {}
 
   async ionViewDidLoad() {
-
-    let info = await this.appSettingsService.getInfo();
-
     this.version = this.appSettingsService.info.version;
-    this.commitHash = this.appSettingsService.info.commitHash;
     this.repoUrl = this.appSettingsService.info.gitHubRepoUrl;
-
-  }
-
-  toSessionLog() {
-    this.navCtrl.push('SessionLogView');
-  }
-
-  toTermsOfUse() {
-    this.navCtrl.push('TermsOfUseView');
   }
 
   toGithub() {
-
-    let confirm = this.alertCtrl.create({
+    this.alertCtrl.create({
       title: 'External link',
       message: 'You can see the latest developments and contribute to this open source app by visiting our project on GitHub',
       buttons: [
         {
-          text: 'Cancel', role: 'cancel', handler: () => {
-          }
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
         },
         {
-          text: 'Open GitHub', handler: () => {
-            this.inAppBrowser.create(this.repoUrl);
+          text: 'Open GitHub',
+          handler: () => {
+            this.inAppBrowser.create(this.repoUrl + '/commit/' + this.commitHash, '_system');
           }
         }
       ]
-    });
-
-    confirm.present();
+    }).present();
   }
 
 }
