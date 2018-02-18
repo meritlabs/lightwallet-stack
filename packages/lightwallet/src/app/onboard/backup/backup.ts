14ad6adcf1c7d04e3bdf7db1462aad87230ad21f
Ibby Hadeed <ibby93@gmail.com>
2018-02-17 19:54:06 -0500

    Prevent going back after wallet has been created

--- packages/lightwallet/src/app/onboard/backup/backup.ts
+++ packages/lightwallet/src/app/onboard/backup/backup.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
-import * as _ from 'lodash';
 
-
-@IonicPage({
-  defaultHistory: ['OnboardingView']
-})
+@IonicPage()
 @Component({
   selector: 'view-backup',
   templateUrl: 'backup.html',
@@ -15,25 +11,16 @@ export class BackupView {
   public mnemonic: string;
 
   constructor(public alertController: AlertController,
-              public navCtrl: NavController,
-              public navParams: NavParams,) {
-  }
-
-  ionViewDidLoad(): void {
+              private navCtrl: NavController,
+              private navParams: NavParams) {
     this.mnemonic = this.navParams.get('mnemonic');
   }
 
-  public toVerify(): void {
-    this.navCtrl.push('VerifyBackupView', {
-      mnemonic: this.mnemonic
-    })
-  }
-
-  public toTransactView(): void {
+  async toTransactView() {
     // Now that we are unlocked, we no longer need these other views in the stack,
     // so we shall destroy them.
-    this.navCtrl.setRoot('TransactView');
-    this.navCtrl.popToRoot();
+    await this.navCtrl.setRoot('TransactView');
+    return this.navCtrl.popToRoot();
   }
 
   public skipBackupPrompt(): void {
@@ -41,7 +28,7 @@ export class BackupView {
       title: 'No backup, no Merit',
       message: 'If you lose your wallet without a backup, it is lost for good!',
       buttons: [
-        { text: 'Cancel', role: 'cancel', handler: _.noop },
+        { text: 'Cancel', role: 'cancel' },
         { text: 'Ok', handler: this.toTransactView.bind(this) }
       ]
     }).present();
