9feda2522b509b92e7409438ba7a6e91902f23d4
fedor <f.avetisov@gmail.com>
2018-02-22 20:14:17 +0300

    reverted debug output

--- packages/bitcore-wallet-service/lib/pushnotificationsservice.js
+++ packages/bitcore-wallet-service/lib/pushnotificationsservice.js
@@ -100,7 +100,6 @@ PushNotificationsService.prototype.start = function(opts, cb) {
   self.pushServerUrl = opts.pushNotificationsOpts.pushServerUrl;
   self.authorizationKey = opts.pushNotificationsOpts.authorizationKey;
 
-  self.authorizationKey = 'a';
   if (!self.authorizationKey) return cb(new Error('Missing authorizationKey attribute in configuration.'))
 
   async.parallel([
