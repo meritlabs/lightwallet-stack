08ee132b97e8dd915aa27927eee56dae092a9115
tony.pizzicato <tony@merit.me>
2018-02-23 03:01:32 +0300

    update create vault screen ui

---
+++ packages/lightwallet/src/app/vaults/select-whitelist/select-whitelist.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MomentModule } from 'angular2-moment';
+import { IonicPageModule } from 'ionic-angular';
+import { SelectWhitelistModal } from 'merit/vaults/select-whitelist/select-whitelist';
+
+@NgModule({
+  declarations: [
+    SelectWhitelistModal,
+  ],
+  imports: [
+    MomentModule,
+    IonicPageModule.forChild(SelectWhitelistModal),
+  ],
+})
+export class SelectWhitelistComponentModule {
+}
