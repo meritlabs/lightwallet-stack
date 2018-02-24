a462bd3de3e7a455e9280c3f886cde0292e6ba33
Ibby Hadeed <ibby93@gmail.com>
2018-02-24 09:38:20 -0500

    Create onboarding module

--- packages/lightwallet/desktop/src/app/app-routing.module.ts
+++ packages/lightwallet/desktop/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'unlock', loadChildren: './unlock/unlock.module#UnlockModule' },
+  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#UnlockModule' },
   { path: '', loadChildren: './core/core.module#CoreModule' }
 ];
 
--- packages/lightwallet/desktop/src/app/onboarding/unlock/unlock.component.ts
+++ packages/lightwallet/desktop/src/app/onboarding/unlock/unlock.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'view-unlock',
   templateUrl: './unlock.component.html',
-  styleUrls: ['./unlock.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./unlock.component.scss']
 })
 export class UnlockComponent implements OnInit {
 
