67aa73e463f3d3750df499cc7025fc58207f9ce5
Ibby Hadeed <ibby93@gmail.com>
2018-02-24 05:10:29 -0500

    Add community page

---
+++ packages/lightwallet-desktop/src/app/core/community/community.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-community',
+  templateUrl: './community.component.html',
+  styleUrls: ['./community.component.scss']
+})
+export class CommunityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
