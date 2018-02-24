07604b2147c1b37abe4383c587f01956694b15a8
Ibby Hadeed <ibby93@gmail.com>
2018-02-24 10:07:43 -0500

    Fix reference to onboarding module

--- packages/lightwallet/desktop/src/app/app-routing.module.ts
+++ packages/lightwallet/desktop/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#UnlockModule' },
+  { path: 'onboarding', loadChildren: './onboarding/onboarding.module#OnboardingModule' },
   { path: '', loadChildren: './core/core.module#CoreModule' }
 ];
 
