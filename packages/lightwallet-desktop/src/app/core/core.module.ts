e97959a99a1a85c6f9676d6b7b64153caef83ba1
Ibby Hadeed <ibby93@gmail.com>
2018-02-24 08:35:55 -0500

    Add ngrx platform

--- packages/lightwallet-desktop/src/app/core/core.module.ts
+++ packages/lightwallet-desktop/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { WalletsComponent } from './wallets/wallets.component';
@@ -7,17 +8,24 @@ import { HistoryComponent } from './history/history.component';
 import { ReceiveComponent } from './receive/receive.component';
 import { SendComponent } from './send/send.component';
 import { CoreComponent } from './core.component';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CommunityComponent } from './community/community.component';
 import { NotificationsComponent } from './toolbar/notifications/notifications.component';
+import { contactsReducer } from '../../reducers/contacts';
+import { walletsReducer } from '../../reducers/wallets';
 
 export function getPages() {
   return [
     WalletsComponent, HistoryComponent, ReceiveComponent, SendComponent, DashboardComponent, CommunityComponent
   ];
 }
+
+export const reducers = {
+  contacts: contactsReducer,
+  wallets: walletsReducer
+};
 
 @NgModule({
   entryComponents: [
@@ -27,7 +35,9 @@ export function getPages() {
   imports: [
     CommonModule,
     CoreRoutingModule,
-    MatButtonModule
+    StoreModule.forRoot(reducers),
+    MatButtonModule,
+    MatMenuModule
   ],
   declarations: [
     CoreComponent,
