8f606c8eb9cc65a9d7d46d69571b511a1fb6e0d8
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-01 00:18:49 +0200

    Swith to sass

--- packages/lightwallet/desktop/src/app/core/receive/receive.component.ts
+++ packages/lightwallet/desktop/src/app/core/receive/receive.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'view-receive',
   templateUrl: './receive.component.html',
-  styleUrls: ['./receive.component.scss'],
+  styleUrls: ['./receive.component.sass'],
   encapsulation: ViewEncapsulation.None
 })
 export class ReceiveComponent implements OnInit {
