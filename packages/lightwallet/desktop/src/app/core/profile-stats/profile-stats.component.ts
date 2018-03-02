c757557a32fa26a5c4a20e436da58ccd26d38ec4
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-02 18:55:36 +0200

    Inject profile-stats component to the network component

--- packages/lightwallet/desktop/src/app/core/profile-stats/profile-stats.component.ts
+++ packages/lightwallet/desktop/src/app/core/profile-stats/profile-stats.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-stats',
   templateUrl: './profile-stats.component.html',
-  styleUrls: ['./profile-stats.component.scss']
+  styleUrls: ['./profile-stats.component.sass']
 })
 export class ProfileStatsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
