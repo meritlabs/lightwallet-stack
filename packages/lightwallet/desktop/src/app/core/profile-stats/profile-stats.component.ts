a13b8797f91216f62ca54db3da736ef9452b14aa
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-02 18:51:19 +0200

    Init Profilen stats component

---
+++ packages/lightwallet/desktop/src/app/core/profile-stats/profile-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-stats',
+  templateUrl: './profile-stats.component.html',
+  styleUrls: ['./profile-stats.component.scss']
+})
+export class ProfileStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
