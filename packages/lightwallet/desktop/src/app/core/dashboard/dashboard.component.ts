b0b09772258b431a2482358dc51f03a2cf413b8a
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-03 21:44:44 +0200

    Init dashboard route

--- packages/lightwallet/desktop/src/app/core/dashboard/dashboard.component.ts
+++ packages/lightwallet/desktop/src/app/core/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'view-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  styleUrls: ['./dashboard.component.sass'],
   encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
