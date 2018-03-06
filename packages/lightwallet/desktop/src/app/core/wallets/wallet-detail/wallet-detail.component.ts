2647103041b5ce7f6840428f65abce9a093ce043
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-06 20:56:39 +0200

    Switch to sass

--- packages/lightwallet/desktop/src/app/core/wallets/wallet-detail/wallet-detail.component.ts
+++ packages/lightwallet/desktop/src/app/core/wallets/wallet-detail/wallet-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'view-wallet-detail',
   templateUrl: './wallet-detail.component.html',
-  styleUrls: ['./wallet-detail.component.scss']
+  styleUrls: ['./wallet-detail.component.sass']
 })
 export class WalletDetailComponent implements OnInit {
 
