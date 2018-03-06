aefda38f37b3757e076c8549e8474723786dbb89
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-07 00:18:05 +0200

    Init wallet preferences route

--- packages/lightwallet/desktop/src/app/core/wallets/wallet-settings/wallet-settings.component.ts
+++ packages/lightwallet/desktop/src/app/core/wallets/wallet-settings/wallet-settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'view-wallet-settings',
   templateUrl: './wallet-settings.component.html',
-  styleUrls: ['./wallet-settings.component.scss']
+  styleUrls: ['./wallet-settings.component.sass']
 })
 export class WalletSettingsComponent implements OnInit {
 
