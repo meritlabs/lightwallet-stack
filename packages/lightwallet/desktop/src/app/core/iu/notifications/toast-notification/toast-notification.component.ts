fa815ff5c728bdaeb03b666f2c0cf517646f1603
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-03-06 16:05:19 +0300

    fix desktop wallet build

--- packages/lightwallet/desktop/src/app/core/iu/notifications/toast-notification/toast-notification.component.ts
+++ packages/lightwallet/desktop/src/app/core/iu/notifications/toast-notification/toast-notification.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+type INotification = {
+  status: string,
+  title: string,
+  text: string,
+};
+
 @Component({
   selector: 'app-toast-notification',
   templateUrl: './toast-notification.component.html',
@@ -9,7 +15,7 @@ export class ToastNotificationComponent implements OnInit {
   constructor() {}
   ngOnInit() {}
   @Input() show: boolean = false;
-  @Input() message: Object = {
+  @Input() message: INotification = {
     "status": null,
     "title": null,
     "text": null
