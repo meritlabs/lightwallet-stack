f690545b17be83d9186d17211631721bc3d168e5
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-07 23:30:16 +0200

    Port mobile colors to desktop

--- packages/lightwallet/desktop/src/app/core/wallets/wallet-settings/wallet-settings.component.ts
+++ packages/lightwallet/desktop/src/app/core/wallets/wallet-settings/wallet-settings.component.ts
@@ -9,22 +9,86 @@ export class WalletSettingsComponent implements OnInit {
   wallet: any = {
     name: 'Wallet name',
     color: {
-      name: 'Green',
-      rgba: '87,182,121, .6'
+      name: 'Sunglo',
+      color: 'rgba(87,182,121, .6)'
     }
   };
   avalibleColors: any = [
     {
-      name: 'Green',
-      rgba: '87,182,121, .6'
+      name: 'Sunglo',
+      color: '#E57373'
     },
     {
-      name: 'Blue',
-      rgba: '0,0,255, .5'
+      name: 'Carissma',
+      color: '#E985A7'
     },
     {
-      name: 'Red',
-      rgba: '255,0,0, .5'
+      name: 'Light Wisteria',
+      color: '#ca85d6'
+    },
+    {
+      name: 'Lilac Bush',
+      color: '#A185D4'
+    },
+    {
+      name: 'Moody Blue',
+      color: '#7987d1'
+    },
+    {
+      name: 'Havelock Blue',
+      color: '#64aae3'
+    },
+    {
+      name: 'Picton Blue',
+      color: '#53b9e8'
+    },
+    {
+      name: 'Viking',
+      color: '#4ccdde'
+    },
+    {
+      name: 'Ocean Green',
+      color: '#48ae6c'
+    },
+    {
+      name: 'Puerto Rico',
+      color: '#44baad'
+    },
+    {
+      name: 'Wild Willow',
+      color: '#99c666'
+    },
+    {
+      name: 'Turmeric',
+      color: '#bcc84c'
+    },
+    {
+      name: 'Buttercup',
+      color: '#f5a623'
+    },
+    {
+      name: 'Supernova',
+      color: '#ffc30e'
+    },
+    {
+      name: 'Yellow Orange',
+      color: '#ffaf37'
+    },
+    {
+      name: 'Portage',
+      color: '#8997eb'
+    },
+    {
+      name: 'Gray',
+      color: '#808080'
+    },
+    {
+      name: 'Shuttle Gray',
+      color: '#5f6c82'
+    },
+    {
+      name: 'Tuna',
+      color: '#383d43'
     }
   ];
   constructor() { }
