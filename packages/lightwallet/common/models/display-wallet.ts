c743c7581ce4a2cabbf2066f5f352c8cbb22fadd
Adil Wali <adil@merit.me>
2018-03-07 19:15:03 -0800

    Reducing the data needed to display community page.

--- packages/lightwallet/common/models/display-wallet.ts
+++ packages/lightwallet/common/models/display-wallet.ts
@@ -3,12 +3,15 @@ import { DEFAULT_WALLET_COLOR } from '../utils/constants';
 import { MeritWalletClient } from '@merit/common/merit-wallet-client';
 import { WalletService } from '@merit/common/services/wallet.service';
 import { SendService } from '@merit/common/services/send.service';
+import { FiatAmount } from '@merit/common/models/fiat-amount';
+import { TxFormatService } from '@merit/common/services/tx-format.service';
 
 export interface IDisplayWalletOptions {
   skipStatus?: boolean;
   skipRewards?: boolean;
   skipAnv?: boolean;
   skipAlias?: boolean;
+  skipShareCode?: boolean;
 }
 
 export function ClientProperty(target: DisplayWallet, key: keyof MeritWalletClient) {
@@ -33,12 +36,14 @@ export class DisplayWallet {
 
   referrerAddress: string;
   alias: string;
+  shareCode: string;
   // We will only have one icon type to start.
   iconUrl: string = "/assets/v1/icons/ui/wallets/wallet-ico-grey.svg";
 
   totalBalanceStr?: string;
   totalBalanceMicros?: number;
   cachedBalanceUpdatedOn: string; // only available if we're using cached balance
+  totalBalanceFiat?: string;
 
   totalNetworkValueMicro: number;
   totalNetworkValueMerit: string;
@@ -57,6 +62,7 @@ export class DisplayWallet {
 
   constructor(public client: MeritWalletClient,
               private walletService: WalletService,
+              private txFormatService: TxFormatService,
               private sendService?: SendService) {
     this.referrerAddress = this.walletService.getRootAddress(this.client).toString();
 
@@ -72,6 +78,16 @@ export class DisplayWallet {
     }
   }
 
+  // Alias if you have one; otherwise address. 
+  async updateShareCode() {
+    const { alias } = await this.sendService.getAddressInfo(this.referrerAddress);
+    if (alias) {
+      this.shareCode = alias;
+    } else {
+      this.shareCode = this.referrerAddress;
+    }
+  }
+
   async updateAnv() {
     this.totalNetworkValueMicro = await this.walletService.getANV(this.client);
   }
@@ -83,6 +99,8 @@ export class DisplayWallet {
     if (this.status.totalBalanceStr) {
       this.totalBalanceStr = this.client.status.totalBalanceStr;
       this.totalBalanceMicros = this.client.status.totalBalanceMicros;
+      const usdAmount = await this.txFormatService.formatToUSD(this.totalBalanceMicros);
+      this.totalBalanceFiat = new FiatAmount(+usdAmount).amountStr;
     } else {
       this.totalBalanceStr = this.client.cachedBalance;
       this.cachedBalanceUpdatedOn = this.client.cachedBalanceUpdatedOn;
@@ -99,8 +117,8 @@ export class DisplayWallet {
   }
 }
 
-export async function createDisplayWallet(wallet: MeritWalletClient, walletService: WalletService, sendService?: SendService, options: IDisplayWalletOptions = {}): Promise<DisplayWallet> {
-  const displayWallet = new DisplayWallet(wallet, walletService, sendService);
+export async function createDisplayWallet(wallet: MeritWalletClient, walletService: WalletService, txFormatService: TxFormatService, sendService?: SendService, options: IDisplayWalletOptions = {}): Promise<DisplayWallet> {
+  const displayWallet = new DisplayWallet(wallet, walletService, txFormatService, sendService);
 
   if (!options.skipAlias)
     await displayWallet.updateAlias();
@@ -113,6 +131,9 @@ export async function createDisplayWallet(wallet: MeritWalletClient, walletServi
 
   if (!options.skipRewards)
     await displayWallet.updateRewards();
+ 
+  if (!options.skipShareCode)
+    await displayWallet.updateShareCode();
 
   return displayWallet;
 }
--- packages/lightwallet/desktop/src/app/core/network/network.component.ts
+++ packages/lightwallet/desktop/src/app/core/network/network.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayWallet } from '@merit/common/models/display-wallet';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { getWalletsLoading, getWallets, IAppState } from '@merit/common/reducers';
+import { TxFormatService } from '@merit/common/services/tx-format.service';
+import { FiatAmount } from '@merit/common/models/fiat-amount';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-network',
@@ -6,8 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./network.component.sass']
 })
 export class NetworkComponent implements OnInit {
+  wallets$: Observable<Array<DisplayWallet & any>> = this.store.select(getWallets)
+  .map((dWallets: DisplayWallet[]) => dWallets.map((dWallet: DisplayWallet) => {
+    if (!_.isNil(dWallet.totalNetworkValueMicro)) {
+      dWallet.totalNetworkValueMerit = this.txFormatService.parseAmount(dWallet.totalNetworkValueMicro, 'micros').amountUnitStr;
+      dWallet.totalNetworkValueFiat = new FiatAmount(+this.txFormatService.formatToUSD(dWallet.totalNetworkValueMicro)).amountStr;
+    }
 
-  constructor() { }
+    if (!_.isNil(dWallet.miningRewardsMicro)) {
+      dWallet.miningRewardsMerit = this.txFormatService.parseAmount(dWallet.miningRewardsMicro, 'micros').amountUnitStr;
+      dWallet.miningRewardsFiat = new FiatAmount(+this.txFormatService.formatToUSD(dWallet.miningRewardsMicro)).amountStr;
+    }
+
+    if (!_.isNil(dWallet.ambassadorRewardsMicro)) {
+      dWallet.ambassadorRewardsMerit = this.txFormatService.parseAmount(dWallet.ambassadorRewardsMicro, 'micros').amountUnitStr;
+      dWallet.ambassadorRewardsFiat = new FiatAmount(+this.txFormatService.formatToUSD(dWallet.ambassadorRewardsMicro)).amountStr;
+    }
+    return dWallet;
+    
+  }));
+  walletsLoading$: Observable<boolean> = this.store.select(getWalletsLoading);
+
+  constructor(
+    private store: Store<IAppState>,
+    private txFormatService: TxFormatService    
+  ) { }
 
   ngOnInit() {
   }
