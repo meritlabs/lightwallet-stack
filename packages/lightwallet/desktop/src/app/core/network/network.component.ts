c6d9eec0d1357544e2cd88602b3390bf404f6ee7
Ibby Hadeed <ibby93@gmail.com>
2018-03-09 15:53:10 -0500

    Fix reducer paths

--- packages/lightwallet/desktop/src/app/core/network/network.component.ts
+++ packages/lightwallet/desktop/src/app/core/network/network.component.ts
@@ -1,32 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
 import { Observable } from 'rxjs/Observable';
-import { from } from 'rxjs/observable/from';
 import { Store } from '@ngrx/store';
-//import { Rx } from 'rxjs/rx';
-import { reduce, groupBy } from 'rxjs/operators';
-import { getWalletsLoading, getWallets, IAppState } from '@merit/common/reducers';
-import { TxFormatService } from '@merit/common/services/tx-format.service';
-import { FiatAmount } from '@merit/common/models/fiat-amount';
-import * as _ from 'lodash';
+import {getWallets, getWalletsLoading} from "@merit/common/reducers/wallets.reducer";
+import {IRootAppState} from "@merit/common/reducers";
 
 @Component({
   selector: 'app-network',
   templateUrl: './network.component.html',
   styleUrls: ['./network.component.sass']
 })
-export class NetworkComponent implements OnInit {
+export class NetworkComponent {
   wallets$: Observable<Array<DisplayWallet>> = this.store.select(getWallets);
   walletsLoading$: Observable<boolean> = this.store.select(getWalletsLoading);
 
 
   constructor(
-    private store: Store<IAppState>,
-  ) { 
-    
-  }
-
-  ngOnInit() {
-  }
+    private store: Store<IRootAppState>,
+  ) {}
 
 }
