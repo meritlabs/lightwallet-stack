19b9a5622c92beabeb58469eed7c19aa9ee9559f
Ibby Hadeed <ibby93@gmail.com>
2018-03-11 10:39:51 -0400

    Move toast notification component

---
+++ packages/lightwallet/desktop/src/app/components/toast-notification/toast-notification.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+export interface INotificationMessage {
+  status: string;
+  title: string;
+  text: string;
+}
+
+@Component({
+  selector: 'app-toast-notification',
+  templateUrl: './toast-notification.component.html',
+  styleUrls: ['./toast-notification.component.sass']
+})
+export class ToastNotificationComponent {
+  @Input() show: boolean;
+  @Input() message: INotificationMessage = {
+    status: null,
+    title: null,
+    text: null
+  };
+}
