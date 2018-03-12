489e16383d3cfa065a51337215b5259a9e7b5f18
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-12 20:43:36 +0300

    Add route for restoring wallet from the DAT file

---
+++ packages/lightwallet/desktop/src/app/core/wallets/import-wallet/import-with-file/import-with-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-import-with-file',
+  templateUrl: './import-with-file.component.html',
+  styleUrls: ['./import-with-file.component.scss']
+})
+export class ImportWithFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
