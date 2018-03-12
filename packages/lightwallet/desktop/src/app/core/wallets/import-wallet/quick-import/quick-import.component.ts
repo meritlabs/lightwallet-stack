66aaab740d5a920e783e0b9259356574b3fd4ca6
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-12 16:23:55 +0300

    Add subrouts for wallet importing

---
+++ packages/lightwallet/desktop/src/app/core/wallets/import-wallet/quick-import/quick-import.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-import',
+  templateUrl: './quick-import.component.html',
+  styleUrls: ['./quick-import.component.scss']
+})
+export class QuickImportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
