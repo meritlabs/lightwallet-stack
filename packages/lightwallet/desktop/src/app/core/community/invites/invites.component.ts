6465816457d557123ce1db09e444e194304650f0
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-13 15:25:11 +0300

    Init invites component

---
+++ packages/lightwallet/desktop/src/app/core/community/invites/invites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invites',
+  templateUrl: './invites.component.html',
+  styleUrls: ['./invites.component.sass']
+})
+export class InvitesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
