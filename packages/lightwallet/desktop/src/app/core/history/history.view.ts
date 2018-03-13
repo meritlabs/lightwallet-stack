fec1df0755a40493b8cb589335cb5b43a227c5a7
Ibby Hadeed <ibby93@gmail.com>
2018-03-13 02:51:40 -0400

    History component -> history view

--- packages/lightwallet/desktop/src/app/core/history/history.view.ts
+++ packages/lightwallet/desktop/src/app/core/history/history.view.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'view-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.sass']
+  templateUrl: './history.view.html',
+  styleUrls: ['./history.view.sass']
 })
 export class HistoryComponent {
 }
