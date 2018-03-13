a4037ddf624cc333e4afc0a8404d10842a5a09fb
Ibby Hadeed <ibby93@gmail.com>
2018-03-13 02:55:26 -0400

    Define components in shared components module

--- packages/lightwallet/desktop/src/app/components/components.module.ts
+++ packages/lightwallet/desktop/src/app/components/components.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { ErrorMessageComponent } from './error-message/error-message.component';
-import { MeritIconComponent } from './merit-icon/merit-icon.component';
+import { LoadingSpinnerComponent } from '@merit/desktop/app/components/loading-spinner/loading-spinner.component';
+import { ErrorMessageComponent } from '@merit/desktop/app/components/error-message/error-message.component';
+import { MeritIconComponent } from '@merit/desktop/app/components/merit-icon/merit-icon.component';
+import { HistoryItemComponent } from '@merit/desktop/app/components/history-item/history-item.component';
+import { HistoryListComponent } from '@merit/desktop/app/components/history-list/history-list.component';
+import { ToastNotificationComponent } from '@merit/desktop/app/components/toast-notification/toast-notification.component';
+
+export function getComponents() {
+  return [
+    LoadingSpinnerComponent,
+    ErrorMessageComponent,
+    MeritIconComponent,
+    HistoryItemComponent,
+    HistoryListComponent,
+    ToastNotificationComponent
+  ];
+}
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LoadingSpinnerComponent, ErrorMessageComponent, MeritIconComponent],
-  exports: [LoadingSpinnerComponent, ErrorMessageComponent, MeritIconComponent]
+  imports: [CommonModule],
+  declarations: getComponents(),
+  exports: getComponents()
 })
 export class ComponentsModule { }
