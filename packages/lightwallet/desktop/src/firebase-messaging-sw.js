1feca2659080ce79d2a4111669df270400fbe481
Ibby Hadeed <ibby93@gmail.com>
2018-03-15 07:42:51 -0400

    Add firebase SW

---
+++ packages/lightwallet/desktop/src/firebase-messaging-sw.js
@@ -0,0 +1,7 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+
+firebase.initializeApp({
+  'messagingSenderId': '1091326413792'
+});
