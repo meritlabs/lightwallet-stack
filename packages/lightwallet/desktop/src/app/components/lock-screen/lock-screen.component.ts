24f60c5f559739377efcfc6e12e990f56a9223b6
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-15 22:40:44 +0200

    Init lock screen component

---
+++ packages/lightwallet/desktop/src/app/components/lock-screen/lock-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lock-screen',
+  templateUrl: './lock-screen.component.html',
+  styleUrls: ['./lock-screen.component.sass']
+})
+export class LockScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
