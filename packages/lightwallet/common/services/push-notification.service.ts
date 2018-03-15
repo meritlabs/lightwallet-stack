fd59c7fff690143ee4ad8fb5f79319fafd75493c
Ibby Hadeed <ibby93@gmail.com>
2018-03-15 14:38:29 -0400

    Save changes to push notification services

--- packages/lightwallet/common/services/push-notification.service.ts
+++ packages/lightwallet/common/services/push-notification.service.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoggerService } from '@merit/common/services/logger.service';
 import { MeritWalletClient } from '@merit/common/merit-wallet-client';
+import { DisplayWallet } from '@merit/common/models/display-wallet';
 
 
 @Injectable()
 export class PushNotificationService {
   protected token;
-  protected retriesRemaining: number = 3;
 
   protected get pushNotificationsEnabled(): boolean {
     return false;
@@ -21,18 +21,65 @@ export class PushNotificationService {
               protected logger: LoggerService) {}
 
   // abstract
-  async requestPermission(){}
+  protected async requestPermission(){}
 
   // abstract
-  async getToken() {}
+  protected async getToken() {}
 
-  subscribe(walletClient: MeritWalletClient) {
+  subscribeToEvents() {}
+
+  // abstract
+  protected getWallets(): Promise<MeritWalletClient[]> { return; }
+
+  async enable() {
+    const wallets = await this.getWallets();
+    this.logger.warn('Got Wallets: ', wallets);
+    wallets.forEach((walletClient: MeritWalletClient) => {
+      this.logger.warn('Subscribing to push with: ', walletClient);
+      this.subscribe(walletClient);
+      // We should be handling real-time updates to the application through either data push or
+      // through long-polling, but not both.
+      // this.pollingNotificationService.disablePolling(walletClient);
+      // TODO(ibby): make sure this is implemented elsewhere
+    });
+  }
+
+  async disable() {
+    const wallets = await this.getWallets();
+    wallets.forEach((wallet: MeritWalletClient) => {
+      this.unsubscribe(wallet);
+      // We should be handling real-time updates to the application through either data push or
+      // through long-polling, but not both.
+      // this.pollingNotificationService.enablePolling(walletClient);
+      // TODO(ibby): Handle this elsewhere if possible
+    });
+    this.token = void 0;
+  }
+
+  async subscribe(walletClient: MeritWalletClient) {
+    console.log('Subscribing to push notifications of wallet ', walletClient, this.pushNotificationsEnabled, this.token);
     if (this.pushNotificationsEnabled && this.token) {
-
+      try {
+        await walletClient.pushNotificationsSubscribe({ token: this.token })
+      } catch(err) {
+        if (err) {
+          this.logger.error(walletClient.name + ': Subscription Push Notifications error. ', JSON.stringify(err));
+        } else {
+          this.logger.info(walletClient.name + ': Subscription Push Notifications success.');
+        }
+      }
     }
   }
 
-  unsubscribe(walletClient: MeritWalletClient) {
-
+  async unsubscribe(walletClient: MeritWalletClient) {
+    try {
+      await walletClient.pushNotificationsUnsubscribe(this.token)
+    } catch(err) {
+      if (err) {
+        this.logger.error(walletClient.name + ': Subscription Push Notifications error. ', JSON.stringify(err));
+      } else {
+        this.logger.info(walletClient.name + ': Subscription Push Notifications success.');
+      }
+    }
   }
 }
