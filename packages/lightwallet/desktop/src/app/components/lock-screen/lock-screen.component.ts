3d8436aa63a046428960e0367ade87085e540da5
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-16 23:58:41 +0200

    Add params for route lock component

--- packages/lightwallet/desktop/src/app/components/lock-screen/lock-screen.component.ts
+++ packages/lightwallet/desktop/src/app/components/lock-screen/lock-screen.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lock-screen',
   templateUrl: './lock-screen.component.html',
   styleUrls: ['./lock-screen.component.sass']
 })
-export class LockScreenComponent {}
+export class LockScreenComponent {
+  @Input() fullScreen: boolean = false;
+}
