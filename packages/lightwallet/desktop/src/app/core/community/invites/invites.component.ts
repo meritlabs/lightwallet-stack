aee160e8d02a3a063f700171e6398d7e0f6d7ed4
Ibby Hadeed <ibby93@gmail.com>
2018-03-16 16:28:46 -0400

    Move invites to the top of the page

--- packages/lightwallet/desktop/src/app/core/community/invites/invites.component.ts
+++ packages/lightwallet/desktop/src/app/core/community/invites/invites.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-invites',
   templateUrl: './invites.component.html',
   styleUrls: ['./invites.component.sass']
 })
-export class InvitesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class InvitesComponent {
 }
