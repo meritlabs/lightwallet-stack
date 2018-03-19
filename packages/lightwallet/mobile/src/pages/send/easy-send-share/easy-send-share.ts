c620868de0ca91d99075a7af13504b2bfc5a4b23
Ibby Hadeed <ibby93@gmail.com>
2018-03-19 12:42:26 -0400

    Take the user to wallets page after they're done

--- packages/lightwallet/mobile/src/pages/send/easy-send-share/easy-send-share.ts
+++ packages/lightwallet/mobile/src/pages/send/easy-send-share/easy-send-share.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Tabs } from 'ionic-angular';
 import { MeritToastController, ToastConfig } from '@merit/common/services/toast.controller.service';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { PlatformService } from '@merit/common/services/platform.service';
@@ -18,25 +18,29 @@ export class EasySendShareView {
     private navParams: NavParams,
     private socialSharing: SocialSharing,
     private toastCtrl: MeritToastController,
-    private platformService: PlatformService
+    private platformService: PlatformService,
+    private tabs: Tabs
   ) {
     this.txData = this.navParams.get('txData');
   }
 
 
-  copyToClipboard(url) {
+  copyToClipboard() {
     this.toastCtrl.create({
       message: 'Copied to clipboard',
       cssClass: ToastConfig.CLASS_SUCCESS
     }).present();
   }
 
-  share(url) {
+  share(url: string) {
     if (SocialSharing.installed()) return this.socialSharing.share(url);
   }
 
-  toWallets() {
-    this.navCtrl.popToRoot();
+  async toWallets() {
+    try {
+      await this.navCtrl.popToRoot();
+      return this.tabs.select(0);
+    } catch (e) {}
   }
 
   showShareButton() {
