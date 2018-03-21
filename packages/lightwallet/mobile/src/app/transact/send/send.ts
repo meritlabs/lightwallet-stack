a68d5b8ab0cf6ea7878d403a162872751a3f885f
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-21 23:13:55 +0200

    Add raw aligment for slider inside elements

--- packages/lightwallet/mobile/src/app/transact/send/send.ts
+++ packages/lightwallet/mobile/src/app/transact/send/send.ts
@@ -56,6 +56,7 @@ export class SendView {
 
     let pagesVisited = await this.persistenceService.getPagesVisited();
     this.showSlider = (pagesVisited.indexOf('send') == -1);
+    this.showSlider = true;
   }
 
   async hideSlider() {
