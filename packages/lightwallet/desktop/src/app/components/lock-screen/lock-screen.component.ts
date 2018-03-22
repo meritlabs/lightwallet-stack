ccba0ba50f6bcc8cf76b06d98fe19c080cec61ad
Ibby Hadeed <ibby@zyra.ca>
2018-03-22 09:41:17 -0400

    Edit lock screen text

--- packages/lightwallet/desktop/src/app/components/lock-screen/lock-screen.component.ts
+++ packages/lightwallet/desktop/src/app/components/lock-screen/lock-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lock-screen',
@@ -6,5 +6,5 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./lock-screen.component.sass']
 })
 export class LockScreenComponent {
-  @Input() fullScreen: boolean = false;
+  @Input() fullScreen: boolean;
 }
