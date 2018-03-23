47002951306089d19a72230b3a1e0c0df2af7adb
Vladimir Gutorov <vlgutv@gmail.com>
2018-03-24 00:36:40 +0200

    Generate small loading spenner

--- packages/lightwallet/desktop/src/app/components/shared-components.module.ts
+++ packages/lightwallet/desktop/src/app/components/shared-components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from '@merit/desktop/app/components/loading-spinner/loading-spinner.component';
+import { LoadingSpinnerSmallComponent } from '@merit/desktop/app/components/loading-spinner-small/loading-spinner-small.component';
 import { ErrorMessageComponent } from '@merit/desktop/app/components/error-message/error-message.component';
 import { MeritIconComponent } from '@merit/desktop/app/components/merit-icon/merit-icon.component';
 import { ToastNotificationComponent } from '@merit/desktop/app/components/toast-notification/toast-notification.component';
@@ -12,7 +13,8 @@ export function getComponents() {
     ErrorMessageComponent,
     MeritIconComponent,
     ToastNotificationComponent,
-    WalletIconComponent
+    WalletIconComponent,
+    LoadingSpinnerSmallComponent
   ];
 }
 
