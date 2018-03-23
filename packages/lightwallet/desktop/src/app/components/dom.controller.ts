71c9116982b14efaf6f84f0b589b631618c45ab1
Ibby Hadeed <ibby@zyra.ca>
2018-03-23 00:32:20 -0400

    Create DOMController class

---
+++ packages/lightwallet/desktop/src/app/components/dom.controller.ts
@@ -0,0 +1,36 @@
+import {
+  ApplicationRef,
+  ComponentFactoryResolver,
+  EmbeddedViewRef,
+  Injectable,
+  Injector,
+  Renderer2,
+  Type
+} from '@angular/core';
+
+export interface IDynamicComponent {
+  destroy: Function;
+  dismiss(): void;
+  setConfig(config: any): void;
+}
+
+@Injectable()
+export class DOMController {
+  constructor(private appRef: ApplicationRef,
+              private cfr: ComponentFactoryResolver,
+              private injector: Injector,
+              private rnd: Renderer2) {}
+
+  create<T extends IDynamicComponent>(component: Type<T>, config: any, parentElement?: HTMLElement) {
+    const componentRef = this.cfr.resolveComponentFactory(component).create(this.injector);
+    this.appRef.attachView(componentRef.hostView);
+    const domElement = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+    this.rnd.appendChild(parentElement || document.body, domElement);
+    const instance: T = componentRef.instance;
+
+    instance.setConfig(config);
+    instance.destroy = () => this.appRef.detachView(componentRef.hostView);
+
+    return instance;
+  }
+}
--- packages/lightwallet/desktop/src/app/components/toast-notification/toast-notification.component.ts
+++ packages/lightwallet/desktop/src/app/components/toast-notification/toast-notification.component.ts
@@ -1,4 +1,5 @@
-import { Component, ComponentRef, Input, ViewContainerRef } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { DynamicComponent, IDynamicComponent } from '@merit/desktop/app/components/dom.controller';
 
 export interface INotificationMessage {
   status?: string;
@@ -15,17 +16,27 @@ export interface INotificationMessage {
     '[hidden]': '!show'
   }
 })
-export class ToastNotificationComponent {
+export class ToastNotificationComponent implements IDynamicComponent, OnInit, OnDestroy {
   @Input() show: boolean;
   @Input() message: INotificationMessage;
   @Input() duration: number = 3000;
 
   destroy: Function;
+  private timeout: any;
 
   ngOnInit() {
-    setTimeout(() => {
+    this.timeout = setTimeout(() => {
       this.dismiss();
     }, this.duration);
+  }
+
+  ngOnDestroy() {
+    this.timeout = void 0;
+  }
+
+  setConfig(config: any) {
+    this.message = config.message;
+    this.show = config.show;
   }
 
   dismiss() {
