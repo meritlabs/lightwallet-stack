0515ae33b25fd4472507c58c6d7120cecf289e4f
fedor <f.avetisov@gmail.com>
2018-03-24 11:42:27 +0300

    optimized network view load

--- packages/lightwallet/mobile/src/app/community/network/network.module.ts
+++ packages/lightwallet/mobile/src/app/community/network/network.module.ts
@@ -3,6 +3,7 @@ import { MomentModule } from 'angular2-moment/moment.module';
 import { IonicPageModule } from 'ionic-angular';
 import { NetworkView } from '@merit/mobile/app/community/network/network';
 import { ClipModule } from 'ng2-clip'
+import { CommonPipesModule } from '@merit/common/common-pipes.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { ClipModule } from 'ng2-clip'
   imports: [
     MomentModule,
     ClipModule,
+    CommonPipesModule,
     IonicPageModule.forChild(NetworkView),
   ]
 })
