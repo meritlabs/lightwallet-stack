8bbfea96c02cace2cbfbbc94602efc50778ba146
Ibby Hadeed <ibby@zyra.ca>
2018-03-24 20:29:50 -0400

    Remove demos

--- packages/lightwallet/desktop/src/app/core/wallets/wallets.view.ts
+++ packages/lightwallet/desktop/src/app/core/wallets/wallets.view.ts
@@ -2,8 +2,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
 import { IRootAppState } from '@merit/common/reducers';
 import { selectWallets, selectWalletsLoading } from '@merit/common/reducers/wallets.reducer';
-import { PasswordPromptController } from '@merit/desktop/app/components/password-prompt/password-prompt.controller';
-import { ToastControllerService } from '@merit/desktop/app/components/toast-notification/toast-controller.service';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
@@ -17,13 +15,5 @@ export class WalletsView {
   wallets$: Observable<DisplayWallet[]> = this.store.select(selectWallets);
   walletsLoading$: Observable<boolean> = this.store.select(selectWalletsLoading);
 
-  constructor(private store: Store<IRootAppState>,
-              private toastCtrl: ToastControllerService,
-              private passwordPromptCtrl: PasswordPromptController) {
-    this.toastCtrl.create({ title: 'Hello', text: 'Hello world', status: 'success' })
-    const passwordPrompt = this.passwordPromptCtrl.create();
-    passwordPrompt.onDismiss((password: string) => {
-      console.log('Password prompt dismissed', password);
-    });
-  }
+  constructor(private store: Store<IRootAppState>) {}
 }
