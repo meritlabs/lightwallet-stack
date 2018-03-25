e03620551398c1d1dd08ed4908e4248e75b82b06
Ibby Hadeed <ibby@zyra.ca>
2018-03-24 21:29:47 -0400

    use common environments file

---
+++ packages/lightwallet/common/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const ENV = {
+  production: false,
+  network: 'testnet',
+  mwsUrl: 'http://192.168.2.128:3232/bws/api',
+  rateUrl: 'https://192.168.2.128:3232/bws/api/v1/rates',
+  feeLevel: 'superEconomy'
+};
