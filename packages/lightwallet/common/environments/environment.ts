59b5c05d323d5370259f79ddb944f4b526f8a257
Ibby Hadeed <ibby93@gmail.com>
2018-03-24 21:31:24 -0400

    Delete environment.ts

--- packages/lightwallet/common/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const ENV = {
-  production: true,
-  network: 'testnet',
-  mwsUrl: 'http://192.168.2.128:3232/bws/api',
-  rateUrl: 'https://mws.merit.me/bws/api/v1/rates',
-  feeLevel: 'superEconomy'
-};
