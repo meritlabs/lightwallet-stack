00135661d4c06cf48811bb2073fe53fe53589a6c
Ibby Hadeed <ibby@zyra.ca>
2018-03-28 20:01:41 -0400

    Fix dashboard + onboarding guards

--- packages/lightwallet/desktop/src/app/guards/dashboard.guard.ts
+++ packages/lightwallet/desktop/src/app/guards/dashboard.guard.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { IRootAppState } from '@merit/common/reducers';
+import { IAppState, selectAppState } from '@merit/common/reducers/app.reducer';
+import { Store } from '@ngrx/store';
 import { isEmpty } from 'lodash';
 import { Observable } from 'rxjs/Observable';
 import { ProfileService } from '@merit/common/services/profile.service';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DashboardGuard implements CanActivate {
   constructor(private profileService: ProfileService,
-              private router: Router) {}
+              private router: Router,
+              private store: Store<IRootAppState>) {}
 
   canActivate(next: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (!isEmpty(this.profileService.getProfile()))
-      return true;
-
-    this.router.navigateByUrl('/onboarding/unlock');
+    return this.store.select(selectAppState)
+      .pipe(
+        map((state: IAppState) => {
+          if (state.credentialsLength > 0) {
+            return true;
+          } else {
+            this.router.navigateByUrl('/onboarding/unlock');
+            return false;
+          }
+        })
+      );
   }
 }
--- packages/lightwallet/desktop/src/app/guards/onboarding.guard.ts
+++ packages/lightwallet/desktop/src/app/guards/onboarding.guard.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { IRootAppState } from '@merit/common/reducers';
+import { IAppState, selectAppState } from '@merit/common/reducers/app.reducer';
 import { ProfileService } from '@merit/common/services/profile.service';
-import { isEmpty } from 'lodash';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class OnboardingGuard implements CanActivate {
   constructor(private profileService: ProfileService,
-              private router: Router) {}
+              private router: Router,
+              private store: Store<IRootAppState>) {}
 
   canActivate(next: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (isEmpty(this.profileService.getProfile()))
-      return true;
-
-    this.router.navigateByUrl('/dashboard');
+    return this.store.select(selectAppState)
+      .pipe(
+        map((state: IAppState) => {
+          if (state.credentialsLength === 0) {
+            return true;
+          } else {
+            this.router.navigateByUrl('/dashboard');
+            return false;
+          }
+        })
+      );
   }
 }
