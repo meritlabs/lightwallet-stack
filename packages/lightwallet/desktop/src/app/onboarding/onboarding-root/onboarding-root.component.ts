71a28bb62b10f4c1a58e3c0470b6d259cc1db4ae
Ibby Hadeed <ibby@zyra.ca>
2018-03-30 06:32:59 -0400

    Wire up onboarding

---
+++ packages/lightwallet/desktop/src/app/onboarding/onboarding-root/onboarding-root.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-onboarding-root',
+  templateUrl: './onboarding-root.component.html',
+  styleUrls: ['./onboarding-root.component.sass']
+})
+export class OnboardingRootComponent {}
