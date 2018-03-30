b1319f69368d47934c7ff80e1b948a6fc6d5c185
Ibby Hadeed <ibby@zyra.ca>
2018-03-30 06:54:09 -0400

    Center import wallet

--- packages/lightwallet/desktop/src/app/onboarding/onboarding-root/onboarding-root.component.ts
+++ packages/lightwallet/desktop/src/app/onboarding/onboarding-root/onboarding-root.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'view-onboarding-root',
   templateUrl: './onboarding-root.component.html',
-  styleUrls: ['./onboarding-root.component.sass']
+  styleUrls: ['./onboarding-root.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OnboardingRootComponent {}
