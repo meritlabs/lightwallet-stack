8f103c3b0177275cf48c9751ed54d6519a6af15d
Ibby Hadeed <ibby@zyra.ca>
2018-03-31 06:05:32 -0400

    Unlock requests work

---
+++ packages/lightwallet/desktop/src/app/components/confirm-dialog/confirm-dialog-controller.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {
+  ConfirmDialogComponent,
+  IConfirmDialogButton
+} from '@merit/desktop/app/components/confirm-dialog/confirm-dialog.component';
+import { DOMController } from '@merit/desktop/app/components/dom.controller';
+
+@Injectable()
+export class ConfirmDialogControllerService {
+  constructor(private domCtrl: DOMController) {}
+
+  create(title: string, message: string, buttons: IConfirmDialogButton[]) {
+    return this.domCtrl.create(ConfirmDialogComponent, { title, message, buttons });
+  }
+}
--- packages/lightwallet/desktop/src/app/core/community/invites/invite-requests/invite-requests.view.ts
+++ packages/lightwallet/desktop/src/app/core/community/invites/invite-requests/invite-requests.view.ts
@@ -1,8 +1,55 @@
 import { Component } from '@angular/core';
+import { IRootAppState } from '@merit/common/reducers';
+import { RefreshOneWalletTransactions, UpdateOneWalletTransactions } from '@merit/common/reducers/transactions.reducer';
+import { selectInviteRequests, UpdateInviteRequestsAction } from '@merit/common/reducers/wallets.reducer';
+import { LoggerService } from '@merit/common/services/logger.service';
+import { IUnlockRequest, UnlockRequestService } from '@merit/common/services/unlock-request.service';
+import { ConfirmDialogControllerService } from '@merit/desktop/app/components/confirm-dialog/confirm-dialog-controller.service';
+import { ToastControllerService } from '@merit/desktop/app/components/toast-notification/toast-controller.service';
+import { Store } from '@ngrx/store';
+import { ToastController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'view-invite-requests',
   templateUrl: './invite-requests.view.html',
   styleUrls: ['./invite-requests.view.scss']
 })
-export class InviteRequestsView {}
+export class InviteRequestsView {
+  inviteRequests$: Observable<IUnlockRequest[]> = this.store.select(selectInviteRequests);
+
+  constructor(private store: Store<IRootAppState>,
+              private confirmDialogCtrl: ConfirmDialogControllerService,
+              private logger: LoggerService,
+              private toastCtrl: ToastControllerService) {}
+
+  approveRequest(request: IUnlockRequest) {
+    const dialog = this.confirmDialogCtrl.create('Confirm action', 'Are you sure you would like to send an invite to this address?', [
+      {
+        text: 'Yes',
+        value: 'yes'
+      },
+      {
+        text: 'No',
+        value: 'no'
+      }
+    ]);
+
+    dialog.onDismiss(async (value: string) => {
+      if (value === 'yes') {
+        try {
+          await request.walletClient.sendInvite(request.address);
+          this.store.dispatch(new RefreshOneWalletTransactions(request.walletClient.id));
+
+          this.toastCtrl.success('The invite request has been confirmed.');
+          const remainingRequests = (await this.inviteRequests$.pipe(take(1)).toPromise()).filter((r: IUnlockRequest) => r.address !== request.address);
+          this.store.dispatch(new UpdateInviteRequestsAction(remainingRequests));
+        } catch (err) {
+          this.logger.error('Error sending invite', err);
+          this.toastCtrl.error('An error occurred while sending an invite.');
+        }
+      }
+    });
+  }
+}
