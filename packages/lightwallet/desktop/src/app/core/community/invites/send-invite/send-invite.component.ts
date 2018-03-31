47b5cf8ed589adf875b0463aea9355859d6647ae
Ibby Hadeed <ibby@zyra.ca>
2018-03-30 20:53:52 -0400

    Display correct number of invites + invite requests

--- packages/lightwallet/desktop/src/app/core/community/invites/send-invite/send-invite.component.ts
+++ packages/lightwallet/desktop/src/app/core/community/invites/send-invite/send-invite.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IDisplayTransaction } from '@merit/common/models/transaction';
+import { IRootAppState } from '@merit/common/reducers';
+import { selectSentInvites, selectTransactionsLoading } from '@merit/common/reducers/transactions.reducer';
+import { selectInvites } from '@merit/common/reducers/wallets.reducer';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-send-invite',
   templateUrl: './send-invite.component.html',
   styleUrls: ['./send-invite.component.scss']
 })
-export class SendInviteComponent implements OnInit {
+export class SendInviteComponent {
+  historyLoading$: Observable<boolean> = this.store.select(selectTransactionsLoading);
+  history$: Observable<IDisplayTransaction[]> = this.store.select(selectSentInvites);
+  availableInvites$: Observable<number> = this.store.select(selectInvites);
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private store: Store<IRootAppState>) {}
 }
