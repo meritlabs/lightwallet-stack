4636f4fc5486613c04b320e27914298f1e5ae62d
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-04-04 20:24:12 +0300

    get rid of console logs

--- packages/lightwallet/mobile/src/app/settings/notifications/notifications.ts
+++ packages/lightwallet/mobile/src/app/settings/notifications/notifications.ts
@@ -33,8 +33,6 @@ export class NotificationsView {
               private platformService: PlatformService,
               private pushService: PushNotificationsService,
               private emailService: EmailNotificationsService) {
-
-    console.log(configService, this.configService);
     this.emailForm = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, <any>(new EmailValidator().isValid(configService, emailService))])]
     });
