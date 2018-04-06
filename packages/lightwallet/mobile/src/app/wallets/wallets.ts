8d20064549df6e5ca3dc0e783fde9dd3d04cbcd8
Ibby Hadeed <ibby@zyra.ca>
2018-04-06 14:22:20 -0400

    Handle passwords for EasyReceive

--- packages/lightwallet/mobile/src/app/wallets/wallets.ts
+++ packages/lightwallet/mobile/src/app/wallets/wallets.ts
@@ -130,8 +130,8 @@ export class WalletsView {
    * @returns {Promise<void>}
    * @memberof WalletsView
    */
-  private async processEasyReceipt(receipt: EasyReceipt, isRetry: boolean): Promise<void> {
-    const data = await this.easyReceiveService.validateEasyReceiptOnBlockchain(receipt, '');
+  private async processEasyReceipt(receipt: EasyReceipt, isRetry: boolean, password: string = ''): Promise<void> {
+    const data = await this.easyReceiveService.validateEasyReceiptOnBlockchain(receipt, password);
     let txs = data.txs;
 
     if (!txs) return await this.easyReceiveService.deletePendingReceipt(receipt);
@@ -199,7 +199,7 @@ export class WalletsView {
               this.showPasswordEasyReceivePrompt(receipt, true); //the only way we can validate password input by the
                                                                  // moment
             } else {
-              this.processEasyReceipt(receipt, true);
+              this.processEasyReceipt(receipt, true, data.password);
             }
           }
         }
