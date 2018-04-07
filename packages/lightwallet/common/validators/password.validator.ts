857103208962b7a0b6e216e928da795a7cbfe8ec
Ibby Hadeed <ibby@zyra.ca>
2018-04-07 00:45:22 -0400

    Add easy receive password validator

--- packages/lightwallet/common/validators/password.validator.ts
+++ packages/lightwallet/common/validators/password.validator.ts
@@ -1,5 +1,7 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, ValidatorFn } from '@angular/forms';
 import { MeritWalletClient } from '@merit/common/merit-wallet-client';
+import { EasyReceipt } from '@merit/common/models/easy-receipt';
+import { EasyReceiveService } from '@merit/common/services/easy-receive.service';
 
 export class PasswordValidator {
   static MatchPassword(control: AbstractControl) {
@@ -14,6 +16,17 @@ export class PasswordValidator {
   }
 
   static VerifyWalletPassword(wallet: MeritWalletClient): ValidatorFn {
-    return (control: AbstractControl) => wallet.checkPassword(control.value)? null : { IncorrectPassword: true };
+    return (control: AbstractControl) => wallet.checkPassword(control.value) ? null : { IncorrectPassword: true };
+  }
+
+  static ValidateEasyReceivePassword(receipt: EasyReceipt, easyReceiveService: EasyReceiveService): AsyncValidatorFn {
+    return async (control: AbstractControl) => {
+      const { txs } = await easyReceiveService.validateEasyReceiptOnBlockchain(receipt, control.value);
+      if (!txs || !txs.length) {
+        return {
+          IncorrectPassword: true
+        };
+      }
+    };
   }
 }
