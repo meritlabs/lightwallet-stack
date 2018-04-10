7ffcccb83e0b00679b3b18460277d353358d2958
Adil Wali <adil@merit.me>
2018-04-10 14:11:11 -0700

    Adding unescape to the mobile app onboard experience.

--- packages/lightwallet/mobile/src/app/onboard/unlock/unlock.module.ts
+++ packages/lightwallet/mobile/src/app/onboard/unlock/unlock.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UnlockView } from '@merit/mobile/app/onboard/unlock/unlock';
+import { CommonPipesModule } from '@merit/common/common-pipes.module';
 
 @NgModule({
   declarations: [
     UnlockView,
   ],
   imports: [
-    IonicPageModule.forChild(UnlockView)
+    IonicPageModule.forChild(UnlockView),
+    CommonPipesModule
   ],
 })
 export class UnlockViewModule {
