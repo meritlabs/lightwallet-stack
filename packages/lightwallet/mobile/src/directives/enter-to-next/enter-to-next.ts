cca82c28398c7550a8fd5ab24a480117df50de06
Ibby Hadeed <ibby@zyra.ca>
2018-04-11 11:13:09 -0400

    Focus next input on enter

---
+++ packages/lightwallet/mobile/src/directives/enter-to-next/enter-to-next.ts
@@ -0,0 +1,39 @@
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+@Directive({
+  selector: '[enter-to-next]'
+})
+export class EnterToNextDirective implements AfterViewInit {
+  constructor(private plt: Platform,
+              private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    const inputNodes: NodeListOf<any> = this.el.nativeElement.querySelectorAll('input,textarea');
+    const inputs = [];
+    let i = 0;
+
+    for (i; i < inputNodes.length; i++)
+      inputs.push(inputNodes[i]);
+
+    (this.el.nativeElement as HTMLElement).onkeyup = (event: KeyboardEvent) => {
+      const { keyCode, target } = event;
+      if (keyCode === 13) {
+        let index;
+        const input = inputs.find((i, ii) => {
+          if (target == i) {
+            index = ii;
+            return true;
+          }
+        });
+
+        if (input) {
+          if (index !== inputs.length - 1)
+            inputs[index + 1].focus();
+          else
+            input.blur();
+        }
+      }
+    };
+  }
+}
