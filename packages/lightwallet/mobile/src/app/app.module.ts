3695fb331f45fad3ed777aa69ba54781d286fa37
Ibby Hadeed <ibby@zyra.ca>
2018-04-10 22:33:45 -0400

    Use correct push notifications class for mobile

--- packages/lightwallet/mobile/src/app/app.module.ts
+++ packages/lightwallet/mobile/src/app/app.module.ts
@@ -3,51 +3,50 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth';
+import { AppVersion } from '@ionic-native/app-version';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Clipboard } from '@ionic-native/clipboard';
 import { Contacts } from '@ionic-native/contacts';
+import { Diagnostic } from '@ionic-native/diagnostic';
 import { FCM } from '@ionic-native/fcm';
 import { File } from '@ionic-native/file';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Keyboard } from '@ionic-native/keyboard';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TouchID } from '@ionic-native/touch-id';
 import { IonicStorageModule } from '@ionic/storage';
+import { CommonProvidersModule } from '@merit/common/common-providers.module';
+import { AddressService } from '@merit/common/services/address.service';
+import { AppSettingsService } from '@merit/common/services/app-settings.service';
+import { ContactsService } from '@merit/common/services/contacts.service';
+import { EmailNotificationsService } from '@merit/common/services/email-notification.service';
+import { MobilePushNotificationsService } from '@merit/common/services/mobile-push-notifications-service';
+import { PollingNotificationsService } from '@merit/common/services/polling-notification.service';
+import { PopupService } from '@merit/common/services/popup.service';
+import { PushNotificationsService } from '@merit/common/services/push-notification.service';
+import { MeritToastController } from '@merit/common/services/toast.controller.service';
+import { VaultsService } from '@merit/common/services/vaults.service';
+import { MeritLightWallet } from '@merit/mobile/app/app.component';
+import { DeepLinkService } from '@merit/mobile/app/core/deep-link.service';
+import { PopupService as MobilePopupService } from '@merit/mobile/app/core/popup.service';
+import { AddressScannerService } from '@merit/mobile/app/utilities/import/address-scanner.service';
+import { TouchIdService } from '@merit/mobile/services/touch-id.service';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MomentModule } from 'angular2-moment';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { MeritLightWallet } from '@merit/mobile/app/app.component';
-import { DeepLinkService } from '@merit/mobile/app/core/deep-link.service';
-import { EmailNotificationsService } from '@merit/common/services/email-notification.service';
-import { PollingNotificationsService } from '@merit/common/services/polling-notification.service';
-import { PushNotificationsService } from '@merit/common/services/push-notification.service';
-import { PopupService } from '@merit/common/services/popup.service';
-import { PopupService as MobilePopupService } from '@merit/mobile/app/core/popup.service';
-import { AddressScannerService } from '@merit/mobile/app/utilities/import/address-scanner.service';
-import { VaultsService } from '@merit/common/services/vaults.service';
-import { ContactsService } from '@merit/common/services/contacts.service';
-import { Diagnostic } from '@ionic-native/diagnostic';
-import { Keyboard } from '@ionic-native/keyboard';
-import { AppVersion } from '@ionic-native/app-version';
-import { TouchIdService } from '@merit/mobile/services/touch-id.service';
-import { AppSettingsService } from '@merit/common/services/app-settings.service';
-import { CommonProvidersModule } from '@merit/common/common-providers.module';
-import { MeritToastController } from '@merit/common/services/toast.controller.service';
-import { AddressService } from '@merit/common/services/address.service';
-import { StoreModule } from '@ngrx/store';
 
 export function getProviders() {
   return [
     { provide: PopupService, useClass: MobilePopupService },
+    { provide: PushNotificationsService, useClass: MobilePushNotificationsService },
     ContactsService,
     AddressScannerService,
     DeepLinkService,
     EmailNotificationsService,
     MeritToastController,
-    PollingNotificationsService,
-    PushNotificationsService,
     AddressService,
     TouchIdService,
     VaultsService
@@ -90,7 +89,6 @@ export function loadConfigs(appService) {
     MomentModule,
     CommonModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
     IonicModule.forRoot(MeritLightWallet, {
       preloadModules: true,
       tabsHideOnSubPages: true,
