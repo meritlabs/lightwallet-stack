7192c155f240927e1b79b8e4a9ae1e44fe1e70cf
Adil Wali <adil@merit.me>
2018-04-11 16:12:39 -0700

    Default production config.

---
+++ packages/lightwallet/common/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const ENV = {
+  production: true,
+  network: 'livenet',
+  mwsUrl: 'https://mws.merit.me/bws/api',
+  rateUrl: 'https://mws.merit.me/bws/api/v1/rates',
+  feeLevel: 'superEconomy'
+};
