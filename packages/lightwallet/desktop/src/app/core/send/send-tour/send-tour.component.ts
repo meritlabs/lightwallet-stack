a021920005fa9c5af37c71c15bfda20c7c0779dd
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-11 23:47:30 +0300

    Init send tour component

---
+++ packages/lightwallet/desktop/src/app/core/send/send-tour/send-tour.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'send-tour',
+  templateUrl: './send-tour.component.html',
+  styleUrls: ['./send-tour.component.sass']
+})
+export class SendTourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
