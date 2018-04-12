2c44789fac17b79a6551c0a26cc86cc40aa05ef8
Ibby Hadeed <ibby@zyra.ca>
2018-04-12 16:29:58 -0400

    Save easy sends

--- packages/lightwallet/common/services/persistence2.service.ts
+++ packages/lightwallet/common/services/persistence2.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { EasySend } from '@merit/common/models/easy-send';
 import { INotification } from '@merit/common/reducers/notifications.reducer';
 
 export enum StorageKey {
   WalletPreferencesPrefix = 'merit_wallet_preferences_',
   NotificationSettings = 'merit_notification_settings',
-  Notifications = 'merit_notifications'
+  Notifications = 'merit_notifications',
+  EasySends = 'merit_easysends'
 }
 
+/**
+ * New storage service with the goal of minimizing & cleaning up the previous service.
+ */
 @Injectable()
 export class PersistenceService2 {
   constructor(private storage: Storage) {}
@@ -34,5 +39,19 @@ export class PersistenceService2 {
 
   getNotifications(): Promise<INotification[]> {
     return this.storage.get(StorageKey.Notifications);
+  }
+
+  async addEasySend(easySend: EasySend) {
+    const easySends = (await this.getEasySends()) || [];
+    easySends.push(easySend);
+    return this.setEasySends(easySends);
+  }
+
+  setEasySends(easySends: EasySend[]) {
+    return this.storage.set(StorageKey.EasySends, easySends);
+  }
+
+  getEasySends(): Promise<EasySend[]> {
+    return this.storage.get(StorageKey.EasySends);
   }
 }
