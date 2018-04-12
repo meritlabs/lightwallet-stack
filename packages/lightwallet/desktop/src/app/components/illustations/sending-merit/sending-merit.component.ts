a2e8285ca6d96df4429c9c6df256230dbad78d73
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-13 00:27:22 +0300

    Recreate illustrations components

---
+++ packages/lightwallet/desktop/src/app/components/illustations/sending-merit/sending-merit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'illustations-sending-merit',
+  templateUrl: './sending-merit.component.html',
+  styleUrls: ['./sending-merit.component.sass']
+})
+export class IllustationsSendingMeritComponent {}
