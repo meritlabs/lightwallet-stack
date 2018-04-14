64221ab972b0974a48a34f5aae1e1ab1846a7c02
Ibby Hadeed <ibby@zyra.ca>
2018-04-14 08:26:32 -0400

    Override hardware back button action on easy send page

--- packages/lightwallet/mobile/src/pages/send/easy-send-share/easy-send-share.ts
+++ packages/lightwallet/mobile/src/pages/send/easy-send-share/easy-send-share.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Tabs, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, Tabs, ViewController } from 'ionic-angular';
 import { MeritToastController, ToastConfig } from '@merit/common/services/toast.controller.service';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { PlatformService } from '@merit/common/services/platform.service';
@@ -15,29 +15,44 @@ export class EasySendShareView {
 
   showShareButton: boolean;
 
+  private backButtonAction: Function;
+
   constructor(
     private navCtrl: NavController,
     private navParams: NavParams,
     private socialSharing: SocialSharing,
     private toastCtrl: MeritToastController,
-    private platformService: PlatformService,
     private tabs: Tabs,
-    private viewCtrl: ViewController
+    private viewCtrl: ViewController,
+    private platform: Platform
   ) {
     this.txData = this.navParams.get('txData');
-    this.showShareButton = this.platformService.isCordova && SocialSharing.installed();
+    this.showShareButton = this.platform.is('cordova') && SocialSharing.installed();
   }
 
   ionViewWillEnter() {
+    // hide back button in case it's showing
     this.viewCtrl.showBackButton(false);
+
+    // override backbutton action
+    this.backButtonAction = this.platform.registerBackButtonAction(() => {
+      this.toWallets();
+    }, 100);
   }
 
   ionViewDidEnter() {
+    // disable swipe to go back
     this.navCtrl.swipeBackEnabled = false;
   }
 
   ionViewWillLeave() {
+    // allow swipe to go back on iOS
     this.navCtrl.swipeBackEnabled = true;
+
+    // clear back button handler
+    if (typeof this.backButtonAction == 'function') {
+      this.backButtonAction();
+    }
   }
 
   copyToClipboard() {
