ee830ca204dfadc2494e91e4ecb9c03b17fe95b4
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-15 12:18:18 +0300

    Init application startUp component

---
+++ packages/lightwallet/desktop/src/app/onboarding/app-start-up/app-start-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-start-up',
+  templateUrl: './app-start-up.component.html',
+  styleUrls: ['./app-start-up.component.sass']
+})
+export class AppStartUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
