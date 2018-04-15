065974bfdb0ffd01ca4cd329b7a04c65f3ab4a47
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-15 16:54:54 +0300

    Add component w/ create wallet

---
+++ packages/lightwallet/desktop/src/app/onboarding/app-start-up/invited-user/invited-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invited-user',
+  templateUrl: './invited-user.component.html',
+  styleUrls: ['./invited-user.component.sass']
+})
+export class InvitedUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
