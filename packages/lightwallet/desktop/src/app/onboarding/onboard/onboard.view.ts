58fd4f516fceeeaaea296a9d7021bdc155ee51b6
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-17 01:51:39 +0300

    Back NewUser guide to the create wallet view

--- packages/lightwallet/desktop/src/app/onboarding/onboard/onboard.view.ts
+++ packages/lightwallet/desktop/src/app/onboarding/onboard/onboard.view.ts
@@ -1,25 +1,8 @@
 import { Component } from '@angular/core';
-import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
   selector: 'view-onboard',
   templateUrl: './onboard.view.html',
   styleUrls: ['./onboard.view.sass']
 })
-export class OnboardView {
-
-  carouselOne: NgxCarousel = {
-    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
-    slide: 1,
-    speed: 400,
-    interval: 4000,
-    point: {
-      visible: true
-    },
-    load: 2,
-    touch: true,
-    loop: false
-  };
-
-
-}
+export class OnboardView {}
