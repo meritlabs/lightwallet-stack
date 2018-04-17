828007288799666b3667dd2f0c43c5e40460fe46
Ibby Hadeed <ibby@zyra.ca>
2018-04-17 18:59:07 -0400

    Use cleanAddress util

--- packages/lightwallet/common/validators/address.validator.ts
+++ packages/lightwallet/common/validators/address.validator.ts
@@ -1,11 +1,13 @@
 import { AbstractControl } from '@angular/forms';
 import { MWCService } from '@merit/common/services/mwc.service';
-import { couldBeAlias, getAddressInfo, isAddress } from '@merit/common/utils/addresses';
+import { cleanAddress, couldBeAlias, getAddressInfo, isAddress } from '@merit/common/utils/addresses';
 
 export class AddressValidator {
   static validateAddress(mwcService: MWCService, allowUnconfirmed?: boolean) {
     return async (abstractCtrl: AbstractControl) => {
-      const { value } = abstractCtrl;
+      let { value } = abstractCtrl;
+
+      value = cleanAddress(value);
 
       if (!value)
         return { required: true };
@@ -27,7 +29,7 @@ export class AddressValidator {
     return async (abstractCtrl: AbstractControl) => {
       let { value } = abstractCtrl;
 
-      value = value.replace(/[@|\s]/g, '');
+      value = cleanAddress(value);
 
       if (!couldBeAlias(value))
         return { InvalidFormat: true };
