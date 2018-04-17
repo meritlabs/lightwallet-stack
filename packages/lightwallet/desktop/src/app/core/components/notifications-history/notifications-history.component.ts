64642030f7ca0d6c3c141b596dcc8bec9a50aff5
Ibby Hadeed <ibby@zyra.ca>
2018-04-17 17:19:59 -0400

    Mark notifications as read + make clear button functional

--- packages/lightwallet/desktop/src/app/core/components/notifications-history/notifications-history.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/notifications-history/notifications-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { INotification } from '@merit/common/reducers/notifications.reducer';
 
 @Component({
@@ -7,13 +7,15 @@ import { INotification } from '@merit/common/reducers/notifications.reducer';
   styleUrls: ['./notifications-history.component.sass'],
   host: {
     '[class.show]': 'showHistory'
-  }
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotificationsHistoryComponent {
   @Input() notifications: INotification[];
   @Input() showHistory: boolean;
+  @Output() onClear: EventEmitter<void> = new EventEmitter<void>();
 
   clearAll() {
-    
+    this.onClear.emit();
   }
 }
