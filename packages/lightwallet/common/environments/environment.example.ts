b6b6ff3a47880e93c2eec4faf809a3ca427dfff4
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-04-18 21:30:24 +0300

    basic pipeline

--- packages/lightwallet/common/environments/environment.example.ts
+++ packages/lightwallet/common/environments/environment.example.ts
@@ -1,7 +1,7 @@
 export const ENV = {
   production: false,
   network: 'testnet',
-  mwsUrl: 'http://localhost:3232/bws/api',
-  rateUrl: 'https://mws.merit.me/bws/api/v1/rates',
+  mwsUrl: 'https://testnet.mws.merit.me/bws/api',
+  rateUrl: 'https://testnet.mws.merit.me/bws/api/v1/rates',
   feeLevel: 'superEconomy'
 };
