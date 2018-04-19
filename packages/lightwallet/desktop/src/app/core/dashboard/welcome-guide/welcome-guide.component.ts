cd4f17bb73d10ab9b64ded4f2dd296d2f3601566
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-20 00:03:57 +0300

    Fix mnemonic loading on first wallet creation

--- packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
+++ packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
@@ -1,11 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { map, switchMap } from 'rxjs/operators';
-import { selectWalletById } from '@merit/common/reducers/wallets.reducer';
+import { Component, Input} from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
-import { Observable } from 'rxjs/Observable';
-import { ActivatedRoute } from '@angular/router';
-import { IRootAppState } from '@merit/common/reducers';
-import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-welcome-guide',
@@ -16,8 +10,4 @@ import { Store } from '@ngrx/store';
 export class WelcomeGuideComponent {
   @Input() wallets: DisplayWallet[];
   @Input() loading: boolean;
-  mnemonic: any;
-  ngOnInit() {
-    this.mnemonic = this.wallets[0].client.getMnemonic();
-  }
 }
