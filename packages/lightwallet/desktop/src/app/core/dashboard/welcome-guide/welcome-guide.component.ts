02d0fb60338b606d6d8be1ff8d14e399eae69858
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-19 23:25:25 +0300

    Get mnemonic for welcome window

--- packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
+++ packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { map, switchMap } from 'rxjs/operators';
+import { selectWalletById } from '@merit/common/reducers/wallets.reducer';
+import { DisplayWallet } from '@merit/common/models/display-wallet';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { IRootAppState } from '@merit/common/reducers';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-welcome-guide',
   templateUrl: './welcome-guide.component.html',
   styleUrls: ['./welcome-guide.component.sass']
 })
-export class WelcomeGuideComponent implements OnInit {
 
-  constructor() { }
-
+export class WelcomeGuideComponent {
+  @Input() wallets: DisplayWallet[];
+  @Input() loading: boolean;
+  mnemonic: any;
   ngOnInit() {
+    this.mnemonic = this.wallets[0].client.getMnemonic();
   }
-
 }
