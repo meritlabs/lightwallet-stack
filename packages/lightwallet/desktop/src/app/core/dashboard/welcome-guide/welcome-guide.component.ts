9964fe86c0c00f64301b5e3fb98de20a88c78b97
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-19 22:57:21 +0300

    Init component for welcome guide

---
+++ packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-guide',
+  templateUrl: './welcome-guide.component.html',
+  styleUrls: ['./welcome-guide.component.sass']
+})
+export class WelcomeGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
