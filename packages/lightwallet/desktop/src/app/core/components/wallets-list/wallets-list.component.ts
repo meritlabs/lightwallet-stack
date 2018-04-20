c771dcf9cd6c68eda7e959e29f2c8842b9c3c027
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-20 23:21:21 +0300

    Update wallet redirect

--- packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'wallets-list',
@@ -10,8 +11,17 @@ export class WalletsListComponent {
   @Input() showButton: boolean = true;
   @Input() wallets: DisplayWallet[];
   @Input() loading: boolean;
+  @Input() redirect: boolean = false;
 
   trackByFn(wallet: DisplayWallet) {
     return wallet.id;
+  }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {}
+  ngOnInit() {
+    let wallet = this.wallets[0];
+    if(this.wallets.length === 1 && this.redirect === true) {
+      this.router.navigate([`/wallets/${wallet.id}`]);
+    }
   }
 }
