786b0fcd00a2a5ed6c71255cf7fdca8c87336ed1
Adil Wali <adil@merit.me>
2018-04-20 08:09:56 -0700

    Add staging deployment configuration

---
+++ packages/lightwallet/common/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const ENV = {
+  production: true,
+  network: 'testnet',
+  mwsUrl: 'https://testnet.mws.merit.me/bws/api',
+  rateUrl: 'https://testnet.mws.merit.me/bws/api/v1/rates',
+  feeLevel: 'superEconomy',
+  easyUrl: 'http://merit.test-app.link/'
+};
