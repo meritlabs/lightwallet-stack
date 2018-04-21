3aacde2849c59a3c5fc93f5ac42f5f8924561f42
Vladimir Gutorov <vlgutv@gmail.com>
2018-04-21 21:36:07 +0300

    Wallets list refactoring

--- packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
 import { Router, ActivatedRoute } from '@angular/router';
+import { PersistenceService2 } from '@merit/common/services/persistence2.service';
 
 @Component({
   selector: 'wallets-list',
@@ -12,19 +13,28 @@ export class WalletsListComponent {
   @Input() wallets: DisplayWallet[];
   @Input() loading: boolean;
   @Input() redirect: boolean = false;
+  singleWallet$: any = this.persistenceService.getViewSettings('singleWallet');
+
 
   trackByFn(wallet: DisplayWallet) {
     return wallet.id;
   }
   constructor(private route: ActivatedRoute,
-              private router: Router) {}
+              private router: Router,
+              private persistenceService: PersistenceService2) {}
+              
   ngOnInit() {
-    let wallet = this.wallets[0];
-    if(this.wallets.length === 1 && this.redirect === true) {
-      this.router.navigate(['/wallets/', wallet.id]);
-      localStorage.setItem('singleWallet', 'true');
+    let wallet = this.wallets[0],
+      redirectStatus = this.redirect || false;
+    if(this.wallets.length === 1) {
+      this.persistenceService.setViewSettings('singleWallet', true);
     }else {
-      localStorage.removeItem('singleWallet');
+      this.persistenceService.setViewSettings('singleWallet', false);
     }
+    this.singleWallet$.then(res => {
+      if(res && redirectStatus) {
+        this.router.navigate(['/wallets/', wallet.id]);
+      }
+    });
   }
 }
