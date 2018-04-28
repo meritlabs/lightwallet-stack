29aad663375224108bc78d93349b811fa152864c
tony.pizzicato <tony@merit.me>
2018-04-28 14:43:53 +0300

    auth user by key for market

--- packages/lightwallet/common/environments/environment.example.ts
+++ packages/lightwallet/common/environments/environment.example.ts
@@ -2,6 +2,8 @@ export const ENV = {
   production: false,
   network: 'testnet',
   mwsUrl: 'https://testnet.mws.merit.me/bws/api',
+  marketApi: 'https://testnet.market.merit.me/api',
+  marketUrl: 'https://testnet.market.merit.me',
   rateUrl: 'https://testnet.mws.merit.me/bws/api/v1/rates',
   feeLevel: 'superEconomy',
   easyUrl: 'https://merit.test-app.link/'
--- packages/lightwallet/common/environments/environment.staging.ts
+++ packages/lightwallet/common/environments/environment.staging.ts
@@ -2,6 +2,8 @@ export const ENV = {
   production: true,
   network: 'testnet',
   mwsUrl: 'https://testnet.mws.merit.me/bws/api',
+  marketApi: 'https://testnet.market.merit.me/api',
+  marketUrl: 'https://testnet.market.merit.me',
   rateUrl: 'https://testnet.mws.merit.me/bws/api/v1/rates',
   feeLevel: 'superEconomy',
   easyUrl: 'http://merit.test-app.link/'
