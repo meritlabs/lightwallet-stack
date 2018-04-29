e2ce9edf42138e80bf5ae824b5c27dad5360b2d3
Ibby Hadeed <ibby93@gmail.com>
2018-04-29 18:46:12 -0400

    Add more mobile e2e tests

---
+++ packages/lightwallet/mobile/e2e/receive.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { Transact } from './transact.po';
+import { browser, by, ElementFinder } from 'protractor';
+import { EC, TEST_WALLET_ALIAS } from './app.e2e-spec';
+
+describe('[Mobile] Receive', () => {
+  let transact: Transact,
+    receiveViewEl: ElementFinder,
+    qrCodeEl: ElementFinder;
+
+  beforeAll(async () => {
+    transact = new Transact();
+    receiveViewEl = transact.getReceiveView();
+    await transact.selectTab(1);
+    browser.wait(EC.visibilityOf(receiveViewEl));
+  });
+
+  it('should go to receive view', () => {
+    expect(browser.getCurrentUrl()).toContain('receive');
+    expect(receiveViewEl.isDisplayed()).toBeTruthy();
+  });
+
+  it('should display a QR code', () => {
+    qrCodeEl = receiveViewEl.element(by.css('qr-code img'));
+    expect(qrCodeEl.isDisplayed()).toBeTruthy();
+  });
+
+  it('should change QR code when changing requested amount', async () => {
+    const initialAmount = await qrCodeEl.getAttribute('src');
+    const input = receiveViewEl.element(by.css('ion-footer input[type=number]'));
+    expect(input.isDisplayed());
+    input.sendKeys(1);
+    expect(qrCodeEl.getAttribute('src')).not.toBe(initialAmount);
+  });
+
+  it('should display wallet alias', () => {
+    expect(receiveViewEl.element(by.css('ion-footer')).getText()).toContain(TEST_WALLET_ALIAS);
+  });
+});
---
+++ packages/lightwallet/mobile/e2e/wallets.e2e-spec.ts
@@ -0,0 +1,78 @@
+import { browser, by, element } from 'protractor';
+import { TEST_WALLET_ALIAS, TEST_WALLET_NAME } from './app.e2e-spec';
+import { Transact } from './transact.po';
+
+describe('[Mobile] Wallets view', () => {
+  let rootEl;
+  let headerEl;
+  let transact: Transact;
+
+  beforeAll(() => {
+    transact = new Transact();
+    transact.selectTab(0);
+    rootEl = element(by.css('view-wallets'));
+  });
+
+  describe('> Header', () => {
+    it('should be visible', () => {
+      headerEl = rootEl.element(by.css('.big-header'));
+      expect(headerEl.isDisplayed()).toBeTruthy();
+    });
+
+    it('should have a button to go to settings', () => {
+      const el = headerEl.element(by.css('button[navpush=SettingsView]'));
+      expect(el.isDisplayed()).toBeTruthy();
+      expect(el.isEnabled()).toBeTruthy();
+    });
+
+    it('should show merit balance', () => {
+      const el = headerEl.element(by.csss('h2.amount'));
+      expect(el.isDisplayed()).toBeTruthy();
+      expect(el.getText()).toContain('MRT');
+    });
+
+    it('should show invites balance', () => {
+      const el = headerEl.element(by.css('.detail-fiat:last-child'));
+      expect(el.isDisplayed()).toBeTruthy();
+      expect(el.getText()).toContain('Invites');
+    });
+  });
+
+  describe('> Wallets list', () => {
+    let walletsListEl, walletItemEl, walletItemLabelEl;
+
+    beforeAll(() => {
+      walletsListEl = rootEl.element(by.css('ion-list.wallets'));
+      walletItemEl = walletsListEl.element(by.css('button[ion-item]'));
+      walletItemLabelEl = walletItemEl.element(by.css('ion-label'));
+    });
+
+    it('should be visible', () => {
+      expect(walletsListEl.isDisplayed()).toBeTruthy();
+    });
+
+    it('should show one wallet', () => {
+      expect(walletItemEl.isDisplayed()).toBeTruthy();
+    });
+
+    it('should show wallet name', () => {
+      expect(walletItemLabelEl.getText()).toContain(TEST_WALLET_NAME);
+    });
+
+    it('should show wallet balance', () => {
+      expect(walletItemLabelEl.getText()).toContain('MRT');
+    });
+
+    it('should show wallet alias', () => {
+      expect(walletItemLabelEl.getText()).toContain(TEST_WALLET_ALIAS);
+    });
+  });
+
+  // describe('> Vaults list', () => {
+  //   let vaultsListEl;
+  //
+  //   beforeAll(() => {
+  //     vaultsListEl = element(by.css(''))
+  //   });
+  // });
+});
