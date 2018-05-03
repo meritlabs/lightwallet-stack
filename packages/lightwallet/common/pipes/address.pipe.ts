f19016f1952e10f70a644bad99a5b6f203e6498e
Ibby Hadeed <ibby@zyra.ca>
2018-05-02 22:20:27 -0400

    Add address pipe

---
+++ packages/lightwallet/common/pipes/address.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe } from '@angular/core';
+import { cleanAddress, isAddress, isAlias } from '@merit/common/utils/addresses';
+
+@Pipe({
+  name: 'address'
+})
+export class AddressPipe {
+  transform(value: string, maxAddressLength?: number, showEllipsis?: boolean) {
+    value = cleanAddress(value);
+
+    // isAlias call is cheap, so we can try that first
+    if (isAlias(value) || !isAddress(value)) {
+      return '@' + value;
+    } else {
+      if (maxAddressLength) {
+        value = value.slice(0, maxAddressLength);
+
+        if (showEllipsis) {
+          value += '...';
+        }
+      }
+    }
+
+    return value;
+  }
+}
