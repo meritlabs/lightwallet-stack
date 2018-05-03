853869ba98eac686b3cd72db8f829e2bffe6b033
fedor <f.avetisov@gmail.com>
2018-05-03 11:22:11 +0300

    removed verify step

--- packages/lightwallet/mobile/src/app/onboard/backup/backup.ts
+++ packages/lightwallet/mobile/src/app/onboard/backup/backup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-
+import { ToastControllerService, IMeritToastConfig } from '@merit/common/services/toast-controller.service';
 @IonicPage()
 @Component({
   selector: 'view-backup',
@@ -8,10 +8,13 @@ import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angu
 })
 export class BackupView {
   public mnemonic: string;
+  public copied: boolean;
 
   constructor(public alertController: AlertController,
               private navCtrl: NavController,
-              private navParams: NavParams) {
+              private navParams: NavParams,
+              private toastCtrl: ToastControllerService
+  ) {
     this.mnemonic = this.navParams.get('mnemonic');
   }
 
@@ -22,14 +25,24 @@ export class BackupView {
     return this.navCtrl.popToRoot();
   }
 
-  public skipBackupPrompt(): void {
-    this.alertController.create({
-      title: 'No backup, no Merit',
-      message: 'If you lose your wallet without a backup, it is lost for good!',
-      buttons: [
-        { text: 'Cancel', role: 'cancel' },
-        { text: 'Ok', handler: this.toTransactView.bind(this) }
-      ]
-    }).present();
+  public skipBackupPrompt(){
+    if (!this.copied) {
+      this.alertController.create({
+        title: 'No backup, no Merit',
+        message: "Have you written your phrase down? You can do it later on wallet export page, but we strongly recommend to do it now so you won't loose access to your Merit",
+        buttons: [
+          { text: 'Cancel', role: 'cancel' },
+          { text: 'Ok', handler: this.toTransactView.bind(this) }
+        ]
+      }).present();
+    } else {
+     this.toTransactView();
+    }
+
+  }
+
+  public markCopied() {
+    this.copied = true;
+    this.toastCtrl.success('Copied to clipboard');
   }
 }
