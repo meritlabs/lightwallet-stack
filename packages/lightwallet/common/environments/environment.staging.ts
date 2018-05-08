8fd8db31a6e52308a73acc11ed1dacace79091a5
tony.pizzicato <tony@merit.me>
2018-05-08 17:15:31 +0300

    fix market env settings

--- packages/lightwallet/common/environments/environment.staging.ts
+++ packages/lightwallet/common/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const ENV = {
   production: true,
   network: 'testnet',
   mwsUrl: 'https://testnet.mws.merit.me/bws/api',
-  marketApi: 'https://testnet.market.merit.me/api',
+  marketApi: 'https://testnet.market-api.merit.me',
   marketUrl: 'https://testnet.market.merit.me',
   rateUrl: 'https://testnet.mws.merit.me/bws/api/v1/rates',
   feeLevel: 'superEconomy',
