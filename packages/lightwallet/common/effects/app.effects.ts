98179a7cd07871f8fe2f8864a679ffece722fcb3
tony.pizzicato <tony@merit.me>
2018-05-09 02:43:01 +0300

    add user query parameter to unlock wallet with provided user code

--- packages/lightwallet/common/effects/app.effects.ts
+++ packages/lightwallet/common/effects/app.effects.ts
@@ -10,10 +10,26 @@ export class AppEffects {
   @Effect({ dispatch: false }) update$ = this.actions$.pipe(
     ofType(AppReducerActionType.UPDATE),
     filter((action: UpdateAppAction) => !action.payload.authorized),
-    tap(() => this.router.navigateByUrl('/onboarding', { queryParams: {} }))
+    tap(() => {
+      // working with window.location as router may not be initialized here
+      // lookup user=aliasoraddress in querystring
+      const q = window.location.search;
+      const params = q
+        .substring(Math.max(0, q.indexOf('?') + 1))
+        .split('&')
+        .reduce((res, pair) => {
+          const q = pair.split('=');
+          return { ...res, [q[0]]: q[1]};
+        }, {});
+
+      if (params['user']) {
+        this.router.navigateByUrl(`/onboarding/unlock?user=${params['user']}`);
+      } else if (window.location.pathname.indexOf('/onboarding') !== 0) {
+        this.router.navigateByUrl('/onboarding');
+      }
+    })
   );
 
-  constructor(private actions$: Actions,
-              private router: Router) {
+  constructor(private actions$: Actions, private router: Router) {
   }
 }
