0e3c87a46ed595c1fce473897906462997f42c90
tony.pizzicato <tony@merit.me>
2018-05-09 05:24:29 +0300

    add share link copy button

--- packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/wallets-list/wallets-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ToastControllerService } from '@merit/common/services/toast-controller.service';
 
 @Component({
   selector: 'wallets-list',
@@ -15,7 +16,20 @@ export class WalletsListComponent {
   trackByFn(wallet: DisplayWallet) {
     return wallet.id;
   }
-  constructor(private route: ActivatedRoute,
-              private router: Router) {}
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private toastCtrl: ToastControllerService
+            ) {}
+
+  shareLink(wallet: DisplayWallet) {
+    const code = wallet.alias || wallet.referrerAddress;
+
+    return `${window.location.origin}?user=${code}`;
+  }
+
+  onCopy($event: MouseEvent, walletId: number) {
+    $event.stopPropagation();
+    this.toastCtrl.success('Share link copied to clipboard!');
+  }
 }
