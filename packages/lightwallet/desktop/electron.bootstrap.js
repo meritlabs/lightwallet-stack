302ba08fb23058920edf236374e6c449d66b3023
Ibby Hadeed <ibby93@gmail.com>
2018-05-13 11:20:35 -0400

    Set notification icon

--- packages/lightwallet/desktop/electron.bootstrap.js
+++ packages/lightwallet/desktop/electron.bootstrap.js
@@ -1,12 +1,16 @@
 const nn = require('node-notifier');
 const { ipcRenderer } = require('electron');
+const { join } = require('path');
+
+const icon = join(__dirname, 'build/1024x1024.png')
 
 const showNotification = (title, message = ' ') => {
   nn.notify({
     title,
     message,
     wait: true,
-    sound: true
+    sound: true,
+    icon
   });
 };
 
