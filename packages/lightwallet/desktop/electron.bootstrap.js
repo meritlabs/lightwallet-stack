c5b25d0b1fe47dcee637d8fa6fc4d6a6af27503e
Ibby Hadeed <ibby93@gmail.com>
2018-05-14 12:35:52 -0400

    Send notifications from main thread

--- packages/lightwallet/desktop/electron.bootstrap.js
+++ packages/lightwallet/desktop/electron.bootstrap.js
@@ -1,22 +1,13 @@
-const nn = require('node-notifier');
 const { ipcRenderer } = require('electron');
-const { join } = require('path');
-
-const icon = join(__dirname, 'build/1024x1024.png')
 
 const showNotification = (title, message = ' ') => {
-  nn.notify({
+  ipcRenderer.send('showNotification', {
     title,
     message,
     wait: true,
-    sound: true,
-    icon
+    sound: true
   });
 };
-
-nn.on('click', () => {
-  ipcRenderer.send('notificationClick', true);
-});
 
 window['electron'] = {
   showNotification
