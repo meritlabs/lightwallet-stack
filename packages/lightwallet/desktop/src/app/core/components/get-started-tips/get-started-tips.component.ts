5899d377a582d46f5d40422841963548fdce33bc
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-17 00:53:19 +0300

    Save WIP

--- packages/lightwallet/desktop/src/app/core/components/get-started-tips/get-started-tips.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/get-started-tips/get-started-tips.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-get-started-tips',
   templateUrl: './get-started-tips.component.html',
-  styleUrls: ['./get-started-tips.component.sass']
+  styleUrls: ['./get-started-tips.component.sass'],
+  animations: [
+    trigger('slideInUp', [
+      state('true', style({})),
+      state('false', style({ maxHeight: 0 })),
+      // transition
+      transition('* => *', animate('150ms ease-out')),
+    ]),
+  ],
 })
-export class GetStartedTipsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class GetStartedTipsComponent {
+  constructor() {}
+  active: boolean = true;
+  showHide() {
+    if (this.active) this.active = false;
+    else this.active = true;
   }
-
 }
