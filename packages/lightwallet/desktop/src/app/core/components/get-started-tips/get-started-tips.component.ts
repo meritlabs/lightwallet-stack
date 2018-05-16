066c1d1595d846a33d1911fbe07bd3a64da6206b
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-16 20:41:28 +0300

    Init component for Dashboard starter tips

---
+++ packages/lightwallet/desktop/src/app/core/components/get-started-tips/get-started-tips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-started-tips',
+  templateUrl: './get-started-tips.component.html',
+  styleUrls: ['./get-started-tips.component.sass']
+})
+export class GetStartedTipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
