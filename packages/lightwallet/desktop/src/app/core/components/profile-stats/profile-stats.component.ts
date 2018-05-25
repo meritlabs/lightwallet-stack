24c04b7e3cacadc6b23042fec7b64f9b9c310f0b
Ibby Hadeed <ibby@zyra.ca>
2018-05-24 23:05:05 -0400

    Fix get started tips toggle

--- packages/lightwallet/desktop/src/app/core/components/profile-stats/profile-stats.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/profile-stats/profile-stats.component.ts
@@ -1,21 +1,13 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'profile-stats',
   templateUrl: './profile-stats.component.html',
   styleUrls: ['./profile-stats.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileStatsComponent {
   @Input() totals: any;
   @Input() loading: boolean;
   @Input() wallets: Object;
-  tipType: string = 'all';
-
-  showTips(value) {
-    this.tipType = value;
-    setTimeout(() => {
-      this.tipType = 'all';
-    }, 500);
-  }
 }
