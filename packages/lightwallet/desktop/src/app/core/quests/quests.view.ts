9e44f28f9a7bfb67edad708dcc99c2facb8eb74a
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-28 23:32:10 +0300

    Move quests list intoown route

--- packages/lightwallet/desktop/src/app/core/quests/quests.view.ts
+++ packages/lightwallet/desktop/src/app/core/quests/quests.view.ts
@@ -9,25 +9,4 @@ export class QuestsView implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
-  quests: Object = [
-    {
-      name: 'Who I`m',
-      shortDescription: `Find your wallet @alias.`,
-      status: 'compleate',
-      icon: '/assets/v1/icons/award.svg',
-    },
-    {
-      name: 'BackUp rookie',
-      shortDescription: `BackUp your wallet.`,
-      status: 'in progress',
-      icon: '/assets/v1/icons/award.svg',
-    },
-    {
-      name: 'Setup expret',
-      shortDescription: `Setup your LightWallet flow`,
-      status: 'new',
-      icon: '/assets/v1/icons/award.svg',
-    },
-  ];
 }
