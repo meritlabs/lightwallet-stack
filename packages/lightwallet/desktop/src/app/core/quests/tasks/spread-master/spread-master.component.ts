b44556851c1d045d0d7f6b3214b4e61f70a82a90
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-29 13:20:12 +0300

    Add style for inner achivement

--- packages/lightwallet/desktop/src/app/core/quests/tasks/spread-master/spread-master.component.ts
+++ packages/lightwallet/desktop/src/app/core/quests/tasks/spread-master/spread-master.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-spread-master',
   templateUrl: './spread-master.component.html',
-  styleUrls: ['./spread-master.component.sass']
+  styleUrls: ['./spread-master.component.sass'],
 })
 export class SpreadMasterComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  status: string = 'progress';
+  validate(link) {
+    this.http.get(`https://cors.now.sh/${link}`).subscribe(res => {
+      console.log(res);
+    });
   }
-
 }
