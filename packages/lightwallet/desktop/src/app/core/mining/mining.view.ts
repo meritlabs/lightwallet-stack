21cb03490ee4d1c76ce1d377a5c8ef9eb3b8fd6e
Maxim Khailo <max@merit.me>
2018-05-28 18:00:38 -0700

    Adding wallet selection in the miner.

--- packages/lightwallet/desktop/src/app/core/mining/mining.view.ts
+++ packages/lightwallet/desktop/src/app/core/mining/mining.view.ts
@@ -1,11 +1,14 @@
 import { Component, ViewEncapsulation} from '@angular/core';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
+import { WalletService } from '@merit/common/services/wallet.service';
+import { AddressService } from '@merit/common/services/address.service';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import {
   selectWallets, selectWalletsLoading,
 } from '@merit/common/reducers/wallets.reducer';
 import { IRootAppState } from '@merit/common/reducers';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'view-mining',
@@ -22,7 +25,10 @@ export class MiningView {
   address: string;
   alias: string;
 
-  constructor(private store: Store<IRootAppState>) {
+  constructor(
+    private store: Store<IRootAppState>,
+    private walletService: WalletService,
+    private addressService: AddressService) {
   }
 
   async ngOnInit() {
@@ -30,18 +36,20 @@ export class MiningView {
       const wallets = await this.wallets$.pipe(
             filter(w => w.length > 0), take(1)).toPromise();
 
-      this.selectedWallet = wallets[0];
-      this.address = this.selectedWallet.client.getRootAddress().toString();
-      let info = await this.addressService.getAddressInfo(this.address);
-      this.alias = info.alias;
-      this.formatAddress();
+      this.selectWallet(wallets[0]);
     } catch (err) {
       if (err.text)
         console.log('Could not initialize: ', err.text);
     }
   }
 
-  selectWallet(wallet: DisplayWallet) {
+  async selectWallet(wallet: DisplayWallet) {
+    if (!wallet) return;
+
     this.selectedWallet = wallet;
+    this.address = this.selectedWallet.client.getRootAddress().toString();
+    let info = await this.addressService.getAddressInfo(this.address);
+    this.alias = info.alias;
   }
+
 }
