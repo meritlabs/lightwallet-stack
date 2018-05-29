55e77c3b198a82a0323ef78f35a30ec274c23596
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-29 16:39:56 +0300

    Add quests reducer

---
+++ packages/lightwallet/common/reducers/quests.reducer.ts
@@ -0,0 +1,22 @@
+import { Quest, Quests } from '@merit/common/models/quest';
+import { Action, createFeatureSelector, createSelector } from '@ngrx/store';
+
+export interface IQuestsState {
+  quests: Quests[];
+}
+
+const DEFAULT_STATE: IQuestsState = {
+  quests: [],
+};
+
+export enum QuestsActionType {
+  Update = '[Quets] Update',
+}
+
+export class UpdateQuestsAction implements Action {
+  type = QuestsActionType.Update;
+}
+
+export type QuestsAction = UpdateQuestsAction;
+
+export function QuestsReducer(state: IQuestsState, action: QuestsAction) {}
