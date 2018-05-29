b1959a8d5af2b8b23e1632ad9bcf2ee884593c3a
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-29 16:39:56 +0300

    Add quests reducer

--- packages/lightwallet/common/reducers/index.ts
+++ packages/lightwallet/common/reducers/index.ts
@@ -2,18 +2,20 @@ import { appReducer, IAppState } from '@merit/common/reducers/app.reducer';
 import { walletsReducer, IWalletsState } from '@merit/common/reducers/wallets.reducer';
 import { ITransactionsState, transactionsReducer } from '@merit/common/reducers/transactions.reducer';
 import { INotificationsState, notificationsReducer } from '@merit/common/reducers/notifications.reducer';
+import { IQuestsState, QuestsReducer } from '@merit/common/reducers/quests.reducer';
 
 export interface IRootAppState {
   app: IAppState;
   wallets: IWalletsState;
   transactions: ITransactionsState;
   notifications: INotificationsState;
+  quests: IQuestsState;
 }
 
 export const reducer = {
   wallets: walletsReducer,
   app: appReducer,
   transactions: transactionsReducer,
-  notifications: notificationsReducer
+  notifications: notificationsReducer,
+  quests: QuestsReducer,
 };
-
