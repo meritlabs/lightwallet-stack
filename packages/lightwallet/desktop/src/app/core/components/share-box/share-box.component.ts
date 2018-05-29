fd50587143d5ab38326d13963b4ce1e7d5118c3a
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-29 22:39:16 +0300

    Init share box component

---
+++ packages/lightwallet/desktop/src/app/core/components/share-box/share-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share-box',
+  templateUrl: './share-box.component.html',
+  styleUrls: ['./share-box.component.sass']
+})
+export class ShareBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
