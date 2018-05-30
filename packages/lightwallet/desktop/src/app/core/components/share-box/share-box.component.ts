faacf487c803a4edb369bd78359e840efd0922e3
Vladimir Gutorov <vlgutv@gmail.com>
2018-05-30 12:19:44 +0300

    Update AddThis configs

--- packages/lightwallet/desktop/src/app/core/components/share-box/share-box.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/share-box/share-box.component.ts
@@ -26,6 +26,10 @@ export class ShareBoxComponent implements OnInit {
   wallets$: Observable<DisplayWallet[]> = this.store.select(selectWallets);
   selectedWallet: DisplayWallet;
 
+  shareTitle: string = 'Merit - digital currency for humans.';
+  shareUrl: string = 'wallet.merit.me';
+  shareText: string = `Merit aims to be the world’s friendliest digital currency, making it dead simple to pay friends, buy goods, and manage your wealth.\n Get wallet now, your activation code: @`;
+
   @Output() dismiss: EventEmitter<void> = new EventEmitter<void>();
 
   async ngOnInit() {
@@ -44,8 +48,9 @@ export class ShareBoxComponent implements OnInit {
     // move created shareThis into right container
     const newParent = document.getElementById('shareThis'),
       oldParent = document.getElementById('pasteShareThis');
+    console.log(oldParent);
 
-    while (oldParent.childNodes.length > 0) {
+    if (oldParent.childNodes.length > 0) {
       newParent.appendChild(oldParent.childNodes[0]);
     }
   }
@@ -58,6 +63,28 @@ export class ShareBoxComponent implements OnInit {
 
       while (oldParent.childNodes.length > 0) {
         newParent.appendChild(oldParent.childNodes[0]);
+      }
+      if (window.addthis_config && window.addthis_share) {
+        let alias = this.selectedWallet.alias;
+
+        window.addthis_config.ui_email_title = this.shareTitle;
+        window.addthis_config.ui_email_note = this.shareText + alias;
+        window.addthis_share = {
+          passthrough: {
+            twitter: {
+              text: `${this.shareTitle}\n ${this.shareText}${alias}`,
+            },
+            linkedin: {
+              title: this.shareTitle,
+              text: `${this.shareTitle}\n ${this.shareText}${alias}`,
+              description: `${this.shareTitle}\n ${this.shareText}${alias}`,
+            },
+            facebook: {
+              title: this.shareTitle,
+              text: `${this.shareTitle}\n ${this.shareText}${alias}`,
+            },
+          },
+        };
       }
     }
   }
