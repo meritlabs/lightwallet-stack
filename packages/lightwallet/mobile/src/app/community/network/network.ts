49350d7a6bfca15e81d6f4b0e49d33b8365278fe
fedor <f.avetisov@gmail.com>
2018-05-30 13:40:11 +0300

    added network data caching

--- packages/lightwallet/mobile/src/app/community/network/network.ts
+++ packages/lightwallet/mobile/src/app/community/network/network.ts
@@ -68,18 +68,38 @@ export class NetworkView {
       this.refreshData();
     });
   }
+
   async ionViewWillEnter() {
-    this.refreshData();
+    if (!this.loading) {
+      this.refreshing = true;
+      this.refreshData();
+      this.refreshing = false;
+    }
   }
 
   async doRefresh(refresher) {
+    this.refreshing = true;
     await this.refreshData();
+    this.refreshing = false;
     refresher.complete();
   }
 
   private async refreshData() {
     this.refreshing = true;
-    await Promise.all([this.loadCommunityInfo(), this.loadRequestsInfo()]);
+    const refreshCommunity = async () => await this.profileService.refreshCommunityInfo();
+    const refreshTotalInfo = async () => {
+      await this.profileService.refreshData();
+      this.wallets = await this.profileService.getWallets();
+
+      this.availableInvites = this.wallets.reduce((number, w) => {
+        return number + w.availableInvites;
+      }, 0);
+
+      this.pendingInvites = this.wallets.reduce((number, w) => {
+        return number + w.pendingInvites;
+      }, 0);
+    };
+    await Promise.all([refreshTotalInfo(), refreshCommunity(), this.loadRequestsInfo()]);
     this.refreshing = false;
   }
 
@@ -96,23 +116,6 @@ export class NetworkView {
     try {
       this.wallets = await this.profileService.getWallets();
 
-      let network = {
-        communitySize: 0,
-        networkValue: 0,
-        miningRewards: 0,
-        growthRewards: 0,
-        wallets: this.wallets.map(w => { return {
-          name: w.name,
-          alias: w.rootAlias,
-          referralAddress: w.rootAddress.toString(),
-          confirmed: w.confirmed,
-          communitySize: 0,
-          miningRewards: 0,
-          growthRewards: 0,
-          color: w.color
-        }})
-      };
-
       this.availableInvites = this.wallets.reduce((number, w) => {
         return number + w.availableInvites;
       }, 0);
@@ -120,37 +123,7 @@ export class NetworkView {
       this.pendingInvites = this.wallets.reduce((number, w) => {
         return number + w.pendingInvites;
       }, 0);
-
-      const addresses = network.wallets.map(w => w.referralAddress);
-
-      if (addresses.length) {
-
-        const getCommunitySizes = () => addresses.map(async (a) => {
-          const { referralcount } = await this.wallets[0].getCommunityInfo(a);
-          let w = network.wallets.find(w => w.referralAddress == a);
-          w.communitySize = referralcount;
-          network.communitySize += referralcount;
-        });
-
-        const getStatuses = () => this.wallets.map((w) => {
-          return w.getStatus();
-        });
-
-        const getRewards = async () => {
-          const rewards = await this.wallets[0].getRewards(addresses);
-          rewards.forEach(r => {
-            let w = network.wallets.find(w => w.referralAddress == r.address);
-            w.miningRewards = r.rewards.mining;
-            w.growthRewards = r.rewards.ambassador;
-            network.miningRewards += w.miningRewards;
-            network.growthRewards += w.growthRewards;
-          });
-        };
-
-        await Promise.all([getRewards()].concat(getCommunitySizes()).concat(getStatuses()));
-      }
-
-      this.network = network;
+      this.network = await this.profileService.getCommunityInfo();
     } catch (err) {
       this.logger.warn(err);
       this.toastCtrl.error(err.text || 'Unknown error');
