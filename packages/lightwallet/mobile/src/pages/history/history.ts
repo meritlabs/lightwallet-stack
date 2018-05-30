659313a6b99164fe811cf6b86ebb3891e3220281
fedor <f.avetisov@gmail.com>
2018-05-30 14:36:35 +0300

    added wallet selection to history page

--- packages/lightwallet/mobile/src/pages/history/history.ts
+++ packages/lightwallet/mobile/src/pages/history/history.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, Events } from 'ionic-angular';
+import { IonicPage, Events, ModalController } from 'ionic-angular';
 import { flatten, sortBy } from 'lodash';
 import { WalletService } from '@merit/common/services/wallet.service';
 import { AddressService } from '@merit/common/services/address.service';
@@ -9,6 +9,7 @@ import { MeritWalletClient } from '@merit/common/merit-wallet-client';
 import { createDisplayWallet, DisplayWallet } from '@merit/common/models/display-wallet';
 import { formatWalletHistory } from '@merit/common/utils/transactions';
 import { PersistenceService2 } from '../../../../common/services/persistence2.service';
+import { MERIT_MODAL_OPTS } from '@merit/common/utils/constants';
 
 @IonicPage()
 @Component({
@@ -16,22 +17,32 @@ import { PersistenceService2 } from '../../../../common/services/persistence2.se
   templateUrl: 'history.html',
 })
 export class HistoryView {
-  transactions: any[];
+  transactions:  Array<any> = [];
+  txs: Array<any> = [];
+
   loading: boolean;
   refreshing: boolean;
+  wallets: Array<MeritWalletClient>;
+  wallet: MeritWalletClient;
+
+  offset: number = 0;
+  limit: number = 10;
 
   constructor(private walletService: WalletService,
               private profileService: ProfileService,
               private addressService: AddressService,
               private contactsService: ContactsService,
               private events: Events,
-              private persistenceService: PersistenceService2
+              private persistenceService: PersistenceService2,
+              private modalCtrl: ModalController
   ) {
   }
 
   async ionViewDidLoad() {
     this.loading = true;
-    await this.loadData();
+    this.wallets = await this.profileService.getWallets();
+    if (this.wallets.length) this.wallet = this.wallets[0];
+    await this.loadHistory();
     this.loading = false;
   }
 
@@ -46,16 +57,45 @@ export class HistoryView {
 
   private async refreashData() {
     this.refreshing = true;
-    await this.loadData();
+    this.wallets = await this.profileService.getWallets();
+    await this.loadHistory();
     this.refreshing = false;
   }
 
-  async loadData(force?: boolean) {
-    const wallets = await this.profileService.getWallets();
-    const walletHistories = await Promise.all(wallets.map(async (wallet: MeritWalletClient) => {
-      const walletHistory = await this.walletService.getTxHistory(wallet, { force });
-      return formatWalletHistory(walletHistory, wallet, await this.persistenceService.getEasySends(), this.contactsService);
-    }));
-    this.transactions = sortBy(flatten(walletHistories), 'time').reverse();
+  async loadHistory() {
+    if (!this.wallet) return;
+    this.txs = await this.wallet.getTxHistory({ skip: 0, limit: this.limit, includeExtendedInfo: true });
+    await this.formatHistory();
   }
+
+  async loadMoreHistory(infiniter) {
+
+    this.offset += this.limit;
+    try {
+      const txs = await this.wallet.getTxHistory({ skip: this.offset, limit: this.limit, includeExtendedInfo: true });
+      this.txs = this.txs.concat(txs);
+      await this.formatHistory();
+    } catch (e) {
+      console.log(e);
+    }
+    infiniter.complete();
+  }
+
+  private async formatHistory() {
+    this.transactions = await formatWalletHistory(this.txs, this.wallet, await this.persistenceService.getEasySends(), this.contactsService);
+  }
+
+  selectWallet() {
+    const modal = this.modalCtrl.create('SelectWalletModal', {
+      selectedWallet: this.wallet,
+      availableWallets: this.wallets
+    }, MERIT_MODAL_OPTS);
+    modal.onDidDismiss((wallet) => {
+      if (wallet) {
+        this.wallet = wallet;
+      }
+    });
+    return modal.present();
+  }
+
 }
