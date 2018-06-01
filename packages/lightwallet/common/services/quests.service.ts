3f5ccd77983a890b67e5cf0064d797d9f14f6962
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-01 11:26:42 +0300

    Moved achievement login into quests list

--- packages/lightwallet/common/services/quests.service.ts
+++ packages/lightwallet/common/services/quests.service.ts
@@ -7,36 +7,18 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 import { LoadQuestsAction } from '@merit/common/reducers/quests.reducer';
 
-import { PersistenceService } from '@merit/common/services/persistence.service';
-import { MeritAchivementClient } from '@merit/common/achievements-client/api';
-import { MeritMarketClient } from '@merit/common/merit-market-client/api';
-import { ENV } from '@app/env';
-
 @Injectable()
 export class QuestsService {
   static BASE_URL: string = 'https://testnet.mws.merit.me/achivement-engine/api/v1/goals/';
 
-  constructor(
-    private http: HttpClient,
-    private store: Store<IRootAppState>,
-    private persistenceService: PersistenceService
-  ) {}
+  constructor(private http: HttpClient, private store: Store<IRootAppState>) {}
 
-  async loadQuests() {
-    let profile = await this.persistenceService.getProfile();
-
-    try {
-      const authData = await MeritAchivementClient.fromObj(profile).login();
-
-      console.log(authData);
-    } catch (e) {
-      console.log(e);
-    }
-    // this.http
-    //   .get(QuestsService.BASE_URL)
-    //   .toPromise()
-    //   .then((quests: Quest[]) => {
-    //     this.store.dispatch(new LoadQuestsAction(quests));
-    //   });
+  loadQuests() {
+    this.http
+      .get(QuestsService.BASE_URL)
+      .toPromise()
+      .then((quests: Quest[]) => {
+        this.store.dispatch(new LoadQuestsAction(quests));
+      });
   }
 }
