0ea7250d6f93ff0fac545222b035b1d735eef195
Ibby Hadeed <ibby@zyra.ca>
2018-06-01 22:25:01 -0400

    Fix GlobalSend destination validation

--- packages/lightwallet/common/utils/destination.ts
+++ packages/lightwallet/common/utils/destination.ts
@@ -3,10 +3,12 @@ import { SendMethodDestination } from '@merit/common/models/send-method';
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export function validateEmail(email: string) {
+  email = email || '';
   return EMAIL_REGEX.test(String(email).toLowerCase());
 }
 
 export function validatePhoneNumber(phoneNumber: string) {
+  phoneNumber = phoneNumber || '';
   return phoneNumber.replace(/\D+/g, '').length >= 10;
 }
 
