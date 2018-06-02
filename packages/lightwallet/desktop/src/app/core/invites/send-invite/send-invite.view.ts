30f884572f2dec7de9a1942f48260749608b6314
Ibby Hadeed <ibby@zyra.ca>
2018-06-02 16:35:21 -0400

    Hardcode wallet URL

--- packages/lightwallet/desktop/src/app/core/invites/send-invite/send-invite.view.ts
+++ packages/lightwallet/desktop/src/app/core/invites/send-invite/send-invite.view.ts
@@ -15,6 +15,7 @@ import { EasySendService } from '@merit/common/services/easy-send.service';
 import { MWCService } from '@merit/common/services/mwc.service';
 import { WalletService } from '@merit/common/services/wallet.service';
 import { cleanAddress } from '@merit/common/utils/addresses';
+import { getShareLink } from '@merit/common/utils/url';
 import { SendValidator } from '@merit/common/validators/send.validator';
 import { ToastControllerService } from '@merit/desktop/app/components/toast-notification/toast-controller.service';
 import { Store } from '@ngrx/store';
@@ -22,12 +23,6 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import 'rxjs/add/operator/toPromise';
 import { Observable } from 'rxjs/Observable';
 import { filter, take, tap } from 'rxjs/operators';
-
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(email).trim().toLowerCase());
-}
 
 @Component({
   selector: 'view-send-invite',
@@ -72,9 +67,8 @@ export class SendInviteView {
     if (this.hasAWalletWithInvites = wallets.length > 0) {
       this.selectedWallet = wallets[0];
       const code = this.selectedWallet.alias || this.selectedWallet.referrerAddress;
-      const shareCode = this.selectedWallet.shareCode;
-      this.emailSubject = `Merit invite from ${shareCode}`;
-      this.emailBody = `${shareCode} invites you to Merit Community. Create your wallet now - ${window.location.origin}?invite=${code}`;
+      this.emailSubject = `Merit invite from ${code}`;
+      this.emailBody = `${ code } invites you to Merit Community. Create your wallet now - ${ getShareLink(code) }`;
     }
 
     // this.formData.valueChanges.subscribe(val => (this.showEmailMessage = validateEmail(val.address)));
