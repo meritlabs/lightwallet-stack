3c91d0511d5753003459a00855523d20bd966e87
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-04 20:33:13 +0300

    Removed unused component

--- packages/lightwallet/desktop/src/app/core/dashboard/welcome-guide/welcome-guide.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-welcome-guide',
-  templateUrl: './welcome-guide.component.html',
-  styleUrls: ['./welcome-guide.component.sass'],
-})
-export class WelcomeGuideComponent {}
