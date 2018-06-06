fe0abd85b401171d82708fe456b62ea6270aa737
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-06 15:57:54 +0300

    Init Setup tracker dialog

---
+++ packages/lightwallet/desktop/src/app/core/dialog/welcome-to-setup-tracker/welcome-to-setup-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-to-setup-tracker',
+  templateUrl: './welcome-to-setup-tracker.component.html',
+  styleUrls: ['./welcome-to-setup-tracker.component.sass']
+})
+export class WelcomeToSetupTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
