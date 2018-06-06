799445e7464c7f4e12d4536881aafd93ec08609f
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-06 13:25:37 +0300

    Update setup list view

--- packages/lightwallet/desktop/src/app/core/wallet-setup/wallet-setup-list/wallet-setup-list.view.ts
+++ packages/lightwallet/desktop/src/app/core/wallet-setup/wallet-setup-list/wallet-setup-list.view.ts
@@ -7,15 +7,21 @@ import { Observable } from 'rxjs/Observable';
 import { Quests, Quest } from '@merit/common/models/quest';
 import { QuestsService } from '@merit/common/services/quests.service';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-wallet-setup-list',
   templateUrl: './wallet-setup-list.view.html',
   styleUrls: ['./wallet-setup-list.view.sass'],
 })
 export class WalletSetupListView implements OnInit {
-  constructor(private store: Store<IRootAppState>, private service: QuestsService) {}
+  constructor(private store: Store<IRootAppState>, private service: QuestsService, private formBuilder: FormBuilder) {}
 
   questState$: Observable<Quests> = this.store.select('quests');
+
+  formData: FormGroup = this.formBuilder.group({
+    trackerStatus: true,
+  });
 
   ngOnInit() {
     this.service.loadQuests();
