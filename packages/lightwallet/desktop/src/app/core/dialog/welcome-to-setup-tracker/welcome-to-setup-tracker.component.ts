648bd6a196295709f6af24ec037b60689c9ad68f
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-07 17:16:08 +0300

    Added logic layer for Tracker Welcome dialog

--- packages/lightwallet/desktop/src/app/core/dialog/welcome-to-setup-tracker/welcome-to-setup-tracker.component.ts
+++ packages/lightwallet/desktop/src/app/core/dialog/welcome-to-setup-tracker/welcome-to-setup-tracker.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AchievementsService } from '@merit/common/services/achievements.service';
+import { IRootAppState } from '@merit/common/reducers';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-welcome-to-setup-tracker',
@@ -7,9 +10,33 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./welcome-to-setup-tracker.component.sass'],
 })
 export class WelcomeToSetupTrackerComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<IRootAppState>,
+    private AchievementsService: AchievementsService
+  ) {}
+
+  trackerSettings: any;
 
   formData: FormGroup = this.formBuilder.group({
-    trackerStatus: true,
+    isSetupTrackerEnabled: false,
   });
+
+  async ngOnInit() {
+    await this.store.select('achievements').subscribe(res => {
+      this.trackerSettings = res.settings;
+      this.formData.patchValue({
+        isSetupTrackerEnabled: res.settings.isSetupTrackerEnabled,
+      });
+    });
+  }
+
+  changeTrackerState() {
+    this.trackerSettings.isSetupTrackerEnabled = !this.trackerSettings.isSetupTrackerEnabled;
+  }
+
+  closeAndSave() {
+    this.trackerSettings.isWelcomeDialogEnabled = !this.trackerSettings.isWelcomeDialogEnabled;
+    this.AchievementsService.setSettings(this.trackerSettings);
+  }
 }
