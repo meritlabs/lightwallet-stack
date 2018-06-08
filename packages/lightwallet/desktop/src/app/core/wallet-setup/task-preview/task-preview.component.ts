40af228b7b8ea6b64becbd5e5f77357de047ab8c
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-08 15:10:44 +0300

    Add action for define goal route / action

--- packages/lightwallet/desktop/src/app/core/wallet-setup/task-preview/task-preview.component.ts
+++ packages/lightwallet/desktop/src/app/core/wallet-setup/task-preview/task-preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Achievement } from '@merit/common/models/achievement';
 import { AchievementsService } from '@merit/common/services/achievements.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-preview',
@@ -8,11 +9,13 @@ import { AchievementsService } from '@merit/common/services/achievements.service
   styleUrls: ['./task-preview.component.sass'],
 })
 export class TaskPreviewComponent implements OnInit {
-  constructor(private AchievementsService: AchievementsService) {}
+  constructor(private AchievementsService: AchievementsService, private router: Router) {}
 
   @Input() goal;
   @Input() wallet;
   @Input() isComplete;
+
+  route: string;
 
   async ngOnInit() {
     this._completeWalletConfirmationGoal();
@@ -20,10 +23,23 @@ export class TaskPreviewComponent implements OnInit {
 
   _completeWalletConfirmationGoal() {
     let isConfirmed: boolean = this.wallet.confirmed,
-      goal: Achievement = this.goal;
+      goal: any = this.goal;
     if (isConfirmed && goal.name === 'Create Wallet' && goal.status !== 1) {
       console.log(goal);
       this.AchievementsService.updateGoal(goal.id, 0);
+    }
+  }
+
+  action(val) {
+    console.log(val);
+
+    switch (val) {
+      case 'Share your invite code':
+        console.log(val);
+
+        return;
+      default:
+        return this.router.navigate(['/wallets']);
     }
   }
 }
