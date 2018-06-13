20ed199f02d23732cc1b2e681e739dc4516fcc0e
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-13 22:35:48 +0300

    Init component for unlock wallet notification alert

---
+++ packages/lightwallet/desktop/src/app/components/wallet-unlock-alert/wallet-unlock-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-unlock-alert',
+  templateUrl: './wallet-unlock-alert.component.html',
+  styleUrls: ['./wallet-unlock-alert.component.sass']
+})
+export class WalletUnlockAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
