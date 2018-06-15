448c67ed295d69a7d036bb2bda87aa483fd793a8
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-15 13:44:40 +0300

    Remove unused declarations

--- packages/lightwallet/desktop/src/app/core/wallet-setup/wallet-setup.view.ts
+++ packages/lightwallet/desktop/src/app/core/wallet-setup/wallet-setup.view.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PersistenceService } from '@merit/common/services/persistence.service';
-import { MeritAchivementClient } from '@merit/common/achievements-client/api';
-import { ENV } from '@app/env';
 
 @Component({
   selector: 'app-wallet-setup',
   templateUrl: './wallet-setup.view.html',
   styleUrls: ['./wallet-setup.view.sass'],
 })
-export class WalletSetupView implements OnInit {
+export class WalletSetupView {
   constructor(private persistenceService: PersistenceService) {}
-
-  ngOnInit() {}
 }
