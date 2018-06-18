0cbe70e8bbedcde01ba855c4a3b95e3b86081caf
Ibby Hadeed <ibby93@gmail.com>
2018-06-18 15:59:26 -0400

    Add goals reducer

---
+++ packages/lightwallet/common/models/goals.ts
@@ -0,0 +1,68 @@
+export enum TaskSlug {
+  // Creator
+  CreateWallet = 'create-wallet',
+  UnlockWallet = 'unlock-wallet',
+
+  // FastStarter
+  InviteFriends = 'invite-friends',
+  ReceiveInviteRequest = 'receive-invite-request',
+  MineInvite = 'mine-invite',
+  ConfirmInviteRequest = 'confirm-invite-request',
+}
+
+export enum GoalSlug {
+  Creator = 'creator',
+  FastStarter = 'fast-starter',
+  MeritTycoon = 'merit-tycoon',
+  GrowthMaster = 'growth-master',
+}
+
+export interface ITask {
+  slug: TaskSlug;
+  name: string;
+  description: string;
+}
+
+export interface ITaskProgress {
+  slug: TaskSlug;
+  status: TaskStatus;
+}
+
+export type IFullTask = ITask & ITaskProgress;
+
+export interface IGoal {
+  slug: GoalSlug;
+  name: string;
+  description: string;
+  image: string;
+  tasks: ITask[];
+}
+
+export interface IFullGoal extends IGoal {
+  route: string;
+  title: string;
+  linkTitle: string;
+  tasks: IFullTask[];
+}
+
+export enum TaskStatus {
+  Incomplete = 0,
+  InProgress,
+  Complete
+}
+
+export interface IProgress {
+  userId?: string;
+  tasks?: ITaskProgress[];
+}
+
+export interface IFullProgress extends IProgress {
+  goals: IFullGoal[];
+}
+
+export interface IGoalSettings {
+  id: string;
+  userId: string;
+  isSetupTrackerEnabled: boolean;
+  isWelcomeDialogEnabled: boolean;
+}
---
+++ packages/lightwallet/common/reducers/goals.reducer.ts
@@ -0,0 +1,100 @@
+import { IFullProgress, IGoalSettings, TaskSlug, TaskStatus } from '@merit/common/models/goals';
+import { Action, createFeatureSelector, createSelector } from '@ngrx/store';
+
+export interface IGoalsState {
+  loading: boolean;
+  progress: IFullProgress;
+  settings: IGoalSettings;
+}
+
+const DEFAULT_STATE: IGoalsState = {
+  loading: true,
+  progress: {},
+  settings: {}
+};
+
+export enum GoalsActionType {
+  SetLoading = '[Goals] Set loading',
+  RefreshProgress = '[Goals] Refresh progress',
+  UpdateProgress = '[Goals] Update progress',
+  RefreshSettings = '[Goals] Refresh settings',
+  UpdateSettings = '[Goals] Update settings',
+  UpdateTaskStatus = '[Goals] Update task status'
+}
+
+export class SetGoalsLoadingAction extends Action {
+  type = GoalsActionType.SetLoading;
+
+  constructor(public loading: boolean) {}
+}
+
+export class RefreshGoalsProgressAction extends Action {
+  type = GoalsActionType.RefreshProgress;
+}
+
+export class UpdateGoalsProgressAction extends Action {
+  type = GoalsActionType.UpdateProgress;
+
+  constructor(public progress: IFullProgress) {}
+}
+
+export class RefreshGoalSettingsAction extends Action {
+  type = GoalsActionType.RefreshSettings;
+}
+
+export class UpdateGoalSettingsAction extends Action {
+  type = GoalsActionType.UpdateSettings;
+
+  constructor(public settings: IGoalSettings) {}
+}
+
+export class UpdateTaskStatus extends Action {
+  type = GoalsActionType.UpdateTaskStatus;
+
+  constructor(public taskSlug: TaskSlug, public status: TaskStatus)
+}
+
+export type GoalAction =
+  SetGoalsLoadingAction
+  & RefreshGoalsProgressAction
+  & UpdateGoalsProgressAction
+  & RefreshGoalSettingsAction
+  & UpdateGoalSettingsAction
+  & UpdateTaskStatus;
+
+export function goalsReducer(state: IGoalsState = DEFAULT_STATE, action: GoalAction) {
+  switch (action.type) {
+    case GoalsActionType.SetLoading:
+      return {
+        ...state,
+        loading: action.loading
+      };
+
+    case GoalsActionTyps.RefreshProgress:
+      return {
+        ...state,
+        loading: true
+      };
+
+    case GoalsActionType.UpdateProgress:
+      return {
+        ...state,
+        loading: false,
+        progress: action.progress
+      };
+
+    case GoalsActionType.UpdateSettings:
+      return {
+        ...state,
+        settings: action.settings
+      };
+
+    case GoalsActionType.UpdateTaskStatus:
+      state.
+  }
+}
+
+export const selectGoalsState = createFeatureSelector<IGoalsState>('goals');
+export const selectGoalsLoading = createSelector(selectGoalsState, state => state.loading);
+export const selectGoalsProgress = createSelector(selectGoalsState, state => state.progress);
+export const selectGoalSettings = createSelector(selectGoalsState, state => state.settings);
