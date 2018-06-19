9e253fd952114861d4a7f3bdcbc9265fc24b78f1
Ibby Hadeed <ibby@zyra.ca>
2018-06-19 19:08:44 -0400

    Add getLatestDefinedValue method

--- packages/lightwallet/common/utils/observables.ts
+++ packages/lightwallet/common/utils/observables.ts
@@ -1,15 +1,17 @@
 import { Observable } from 'rxjs/Observable';
-import { filter, take } from 'rxjs/operators';
+import { filter, take, tap } from 'rxjs/operators';
 import 'rxjs/add/operator/toPromise';
 
 export const getLatestValue = (obs: Observable<any>, filterFn?: (val: any) => boolean) => {
-  const args: any[] = [
-    take(1)
-  ];
+  const args: any[] = [];
 
   if (filterFn) {
     args.push(filter(filterFn));
   }
 
+  args.push(take(1));
+
   return obs.pipe.apply(obs, args).toPromise();
 };
+
+export const getLatestDefinedValue = (obs: Observable<any>) => getLatestValue(obs, val => !!val);
