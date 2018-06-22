47f451f57f619801f3e837a218bb20805fe4ed06
Aleksandar <ta4ka@hotmail.com>
2018-06-22 18:34:02 +0200

    - I added a button that shows and allows the user to skip the showing of passfrase;  [WIP]: I started with working on marking the objects in dashboard that have not been seen by user before;

--- packages/lightwallet/desktop/src/app/core/dashboard/dashboard.view.ts
+++ packages/lightwallet/desktop/src/app/core/dashboard/dashboard.view.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { DisplayWallet } from '@merit/common/models/display-wallet';
-import { IDisplayTransaction } from '@merit/common/models/transaction';
+import { IDisplayTransaction, Visited } from '@merit/common/models/transaction';
 import { IRootAppState } from '@merit/common/reducers';
 import { selectTransactions, selectTransactionsLoading } from '@merit/common/reducers/transactions.reducer';
 import { selectWallets, selectWalletsLoading, selectWalletTotals } from '@merit/common/reducers/wallets.reducer';
@@ -27,8 +27,9 @@ export class DashboardView {
     .select(selectTransactions)
     .pipe(map((transactions: IDisplayTransaction[]) => (isArray(transactions) ? transactions.slice(0, 5) : [])));
   transactionsLoading$: Observable<boolean> = this.store.select(selectTransactionsLoading);
+  visited: Visited[] = [];
 
-  constructor(private store: Store<IRootAppState>, private sanitizer: DomSanitizer) {}
+  constructor(private store: Store<IRootAppState>, private sanitizer: DomSanitizer) { }
 
   getHistoryStyle(length: number) {
     return this.sanitizer.bypassSecurityTrustStyle('height: ' + length * 110 + 'px');
@@ -39,5 +40,52 @@ export class DashboardView {
       let element: HTMLElement = document.getElementById('sendMrt') as HTMLElement;
       element.click();
     }
+  }
+
+  ngOnInit() {
+    this.transactions$ = this.syncTransactions(this.transactions$);
+  }
+
+  private syncTransactions(transactions: Observable<IDisplayTransaction[]>): Observable<IDisplayTransaction[]> {
+    // debugger
+
+    this.visited = JSON.parse(localStorage.getItem('lstVisited'));
+
+    if (this.visited == null) {
+      //save first time
+      transactions
+        .subscribe((lstTrans) => {
+          this.visited=[];
+          lstTrans.forEach(item => {
+            const vis = new Visited(item.time, false);
+            this.visited.push(vis);
+          });
+          
+          console.log('break');
+          lstTrans.forEach(item => {
+            localStorage.setItem('lstVisited', JSON.stringify(this.visited));
+          });
+        });
+    } else {
+      // compare the list find witch item is new 
+      transactions
+        .subscribe((lstTrans) => {
+          lstTrans.forEach(item => {
+            var isVis = true;
+            this.visited.forEach(itemVisited => {
+              if (item.time == itemVisited.time) {
+                console.log('Item already seen');
+                isVis=false;
+                return;
+              }
+            });
+            item.isNew = isVis;
+
+
+          });
+        });
+    }
+    
+    return transactions;
   }
 }
