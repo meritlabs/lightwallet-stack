f733c486765ebd66bed5e20ff048341098573525
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-24 20:04:52 +0300

    Finalize Invite application setup

--- packages/lightwallet/desktop/src/app/onboarding/invite-application/invite-application.component.ts
+++ packages/lightwallet/desktop/src/app/onboarding/invite-application/invite-application.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ENV } from '@app/env';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-invite-application',
   templateUrl: './invite-application.component.html',
-  styleUrls: ['./invite-application.component.sass']
+  styleUrls: ['./invite-application.component.sass'],
 })
-export class InviteApplicationComponent implements OnInit {
+export class InviteApplicationComponent {
+  applicationUrl: SafeResourceUrl;
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit() {
+    this.applicationUrl = this.sanitizer.bypassSecurityTrustResourceUrl(ENV.discordBotApplication);
   }
-
 }
