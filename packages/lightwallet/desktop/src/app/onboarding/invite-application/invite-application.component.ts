6bf4575e803506e93fa1cf5a6a9d5b924e736423
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-24 16:56:49 +0300

    Generate invite application componet

---
+++ packages/lightwallet/desktop/src/app/onboarding/invite-application/invite-application.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-application',
+  templateUrl: './invite-application.component.html',
+  styleUrls: ['./invite-application.component.sass']
+})
+export class InviteApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
