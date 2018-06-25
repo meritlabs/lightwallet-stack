e1734cd322b85a07c12b9e0b4a5bc82d638ed097
Ibby Hadeed <ibby@zyra.ca>
2018-06-24 23:34:45 -0400

    Link Electron update functions to the UI

--- packages/lightwallet/desktop/src/services/electron.service.ts
+++ packages/lightwallet/desktop/src/services/electron.service.ts
@@ -1,13 +1,69 @@
-declare const window: any;
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+export interface IUpdateInfo {
+  size: string;
+  version: string;
+  releaseNotes: string[];
+  releaseDate: string;
+}
+
+export interface IUpdateProgress {
+  total?: number;
+  delta?: number;
+  transferred?: number;
+  percent?: number;
+  bytesPerSecond?: number;
+  timeRemaining?: number;
+}
+
+declare class ElectronInterface {
+  showNotification(title: string, message: string);
+  checkForUpdates(): Promise<IUpdateInfo>;
+  downloadUpdate(progressCallback: (progress: IUpdateProgress) => void): Promise<any>;
+  installUpdate(): void;
+}
+
+declare global {
+  interface Window {
+    electron: ElectronInterface;
+  }
+}
 
 export class ElectronService {
   static get isElectronAvailable(): boolean {
     return typeof window.electron !== 'undefined';
   }
 
+  static get electron(): ElectronInterface {
+    return window.electron;
+  }
+
   static showNotification(title: string, message: string) {
-    if (ElectronService.isElectronAvailable) {
-      window.electron.showNotification(title, message);
+    if (this.isElectronAvailable) {
+      this.electron.showNotification(title, message);
     }
+  }
+
+  static checkForUpdates(): Promise<IUpdateInfo> {
+    if (this.isElectronAvailable) {
+      return this.electron.checkForUpdates();
+    }
+  }
+
+  static downloadUpdate(): Observable<IUpdateProgress> {
+    return new Observable<IUpdateProgress>((observer: Observer<IUpdateProgress>) => {
+      this.electron.downloadUpdate((progress: IUpdateProgress) => {
+        if (progress.percent === 100) {
+          observer.complete();
+        } else {
+          observer.next(progress);
+        }
+      });
+    });
+  }
+
+  static installUpdate() {
+    return this.electron.installUpdate();
   }
 }
