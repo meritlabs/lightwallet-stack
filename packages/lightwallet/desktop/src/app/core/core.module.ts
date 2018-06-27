6060fe5515e8cc9d9867421c3b86c916f17b0d2d
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-27 11:28:30 +0300

    Moved accept ignoge functional into peding invite item component

--- packages/lightwallet/desktop/src/app/core/core.module.ts
+++ packages/lightwallet/desktop/src/app/core/core.module.ts
@@ -41,6 +41,7 @@ import { TaskPreviewComponent } from './wallet-setup/task-preview/task-preview.c
 import { WalletSetupListView } from './wallet-setup/wallet-setup-list/wallet-setup-list.view';
 import { RecordPassphraseComponent } from './dialog/record-passphrase/record-passphrase.component';
 import { WelcomeToSetupTrackerComponent } from './dialog/welcome-to-setup-tracker/welcome-to-setup-tracker.component';
+import { PendingInviteItemComponent } from './invites/pending-invite-item/pending-invite-item.component';
 
 export function getPages() {
   return [
@@ -96,6 +97,7 @@ export function getPages() {
     RecordPassphraseComponent,
     TaskPreviewComponent,
     WelcomeToSetupTrackerComponent,
+    PendingInviteItemComponent,
   ],
   providers: [WalletPasswordGuard],
 })
