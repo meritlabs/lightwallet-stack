d2b4d991970728b155242494db572a37b12e9e52
fedor <f.avetisov@gmail.com>
2018-06-27 23:10:41 +0300

    added pin lock attempts

--- packages/lightwallet/mobile/src/pages/pin-lock/pin-lock.ts
+++ packages/lightwallet/mobile/src/pages/pin-lock/pin-lock.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform, ViewController } from 'ionic-angular';
 import { PersistenceService } from '@merit/common/services/persistence.service';
-
 @IonicPage()
 @Component({
   selector: 'view-pin-lock',
@@ -14,27 +13,71 @@ export class PinLockView {
   error:boolean;
   isTouchIdAvailable: boolean;
 
+  pinBlockedUntil: number;
+  blocked: boolean;
+  blockedText: string;
+  attempts: number = 0;
+
+  readonly MAX_ATTEMPTS = 10;
+  readonly BLOCK_FOR_MINUTES = 10;
+
+  timerId: number;
+
+
   constructor(
     private persistenceService: PersistenceService,
     private platform: Platform,
     private viewCtrl: ViewController
   ) {
     this.isTouchIdAvailable =  this.platform.is('cordova');
+
   }
 
-  async enter(number) {
-    if (this.pin.length < 4) this.pin += number;
+  ionViewWillEnter() {
+    this.timerId = setInterval(this.checkIsBlocked.bind(this), 1000);
+  }
 
-    if (this.pin.length == 4) {
-      setTimeout(this.checkPin.bind(this), 200);
+  ionViewWillLeave() {
+    clearInterval(this.timerId);
+  }
+
+  async checkIsBlocked() {
+    this.pinBlockedUntil = await this.persistenceService.getPinBlockedTimestamp();
+    const diff = this.pinBlockedUntil - Date.now();
+    if (!this.pinBlockedUntil || diff <= 0) {
+      this.blocked = false;
+      this.blockedText = null;
+    } else {
+      const minutes = Math.ceil(diff/60000);
+      this.blocked = true;
+      this.blockedText = `You can try again in ${minutes > 1 ? minutes+' minutes' : 'one minute'}`;
+    }
+  }
+
+
+  async enter(number) {
+    if (this.error || this.blocked) return;
+    if (this.pin.length < 4) {
+      this.pin += number;
+      if (this.pin.length == 4) {
+        setTimeout(this.checkPin.bind(this), 200);
+      }
     }
   }
 
   private async checkPin() {
+    this.attempts++;
     if (await this.persistenceService.checkPin(this.pin)) {
       this.viewCtrl.dismiss();
     } else {
       this.error = true;
+      if (this.attempts >= this.MAX_ATTEMPTS) {
+        this.blocked = true;
+        this.checkIsBlocked();
+        this.pinBlockedUntil = Date.now()+1000*60*this.BLOCK_FOR_MINUTES;
+        this.persistenceService.blockPin(this.pinBlockedUntil);
+        this.attempts = 0;
+      }
       setTimeout(() => {
         this.pin = '';
         this.error = false;
@@ -43,6 +86,7 @@ export class PinLockView {
   }
 
   backspace() {
+    if (this.error || this.blockedText || this.pin.length == 4) return;
     this.pin = this.pin.slice(0, -1);
   }
 
