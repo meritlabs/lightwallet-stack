95e6bf833f328876b944c4d3cd4d5ab5ecb9d699
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-28 10:47:36 +0300

    Component generated

---
+++ packages/lightwallet/desktop/src/app/core/components/profile-stats/community-rank/community-rank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-rank',
+  templateUrl: './community-rank.component.html',
+  styleUrls: ['./community-rank.component.sass']
+})
+export class CommunityRankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
