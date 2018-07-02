297e3d53da805caf0a2062158f129e48ba3a5b64
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-07-02 23:36:35 +0300

    [DLW] wire stats to header

--- packages/lightwallet/desktop/src/app/core/components/profile-stats/profile-stats.component.ts
+++ packages/lightwallet/desktop/src/app/core/components/profile-stats/profile-stats.component.ts
@@ -1,7 +1,7 @@
 import { OnInit, ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ProfileService } from '@merit/common/services/profile.service';
 import { MeritWalletClient } from '@merit/common/merit-wallet-client';
-import { log } from 'util';
+import { forEach } from 'lodash';
 
 @Component({
   selector: 'profile-stats',
@@ -9,7 +9,7 @@ import { log } from 'util';
   styleUrls: ['./profile-stats.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileStatsComponent {
+export class ProfileStatsComponent implements OnInit {
   @Input() totals: any;
   @Input() loading: boolean;
   @Input() wallets;
@@ -22,37 +22,72 @@ export class ProfileStatsComponent {
   displayLeaderboard: any;
   offset: number = 0;
   LIMIT: number = 100;
+
+  rankData: {
+    totalAnv: number,
+    bestRank: number,
+    bestPercentile: number,
+    percentileStr: string,
+  } = {
+    totalAnv: 0,
+    bestRank: 0,
+    bestPercentile: 0,
+    percentileStr: '',
+  };
+
   constructor(private profileService: ProfileService) {}
 
   async ngOnInit() {
     this._wallets = await this.profileService.getConfimedWallets();
     await Promise.all([this.getLeaderboard(), this.getRankInfo()]);
+    this.loadRankingInfo();
     this.loading = false;
   }
 
   async getLeaderboard() {
     this.leaderboard = (await this._wallets[0].getCommunityLeaderboard(this.LIMIT)).ranks;
     this.displayLeaderboard = this.leaderboard.slice(this.offset, this.LIMIT);
-    console.log(this.leaderboard);
+    console.log('getLeaderboard');
   }
 
   async getRankInfo() {
     let ranks = [];
     this._wallets.map(async w => {
-      console.log(w);
-
-      let rankInfo = (await w.getCommunityRank()).ranks[0];
-      console.log('!!!');
-
-      console.log(rankInfo);
-
+      const rankInfo = (await w.getCommunityRank()).ranks[0];
       rankInfo.walletName = w.rootAlias ? '@' + w.rootAlias : w.name;
       ranks.push(rankInfo);
     });
     this.ranks = ranks;
+    console.log('getRankInfo');
   }
 
   onRankClose($event) {
     this.rankActive = $event;
+  }
+
+  private loadRankingInfo() {
+    console.log('loadRankingInfo');
+    const ranks = this.ranks;
+    console.log(ranks);
+    let rankData = {
+      totalAnv: 0,
+      bestRank: 0,
+      bestPercentile: 0,
+      percentileStr: '',
+    };
+
+    console.log(ranks.length);
+    ranks.forEach(walletRank => {
+      console.log(walletRank);
+      rankData.totalAnv += walletRank.anv;
+      if (!rankData.bestRank || rankData.bestRank > walletRank.rank) rankData.bestRank = walletRank.rank;
+      if (!rankData.bestPercentile || rankData.bestPercentile < walletRank.percentile) rankData.bestPercentile = walletRank.percentile;
+    });
+
+    rankData.percentileStr = (rankData.bestPercentile > 20)
+      ? 'Top ' + Math.max(Math.round(100 - rankData.bestPercentile), 1) + '%'
+      : 'Bottom ' + Math.max(Math.round(rankData.bestPercentile), 1) + '%';
+
+    this.rankData = rankData;
   }
 }
