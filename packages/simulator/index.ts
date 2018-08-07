feba6a5ed459a6b11a385463c12af28d48b7a8c0
Adil Wali <adil@merit.me>
2018-08-07 08:07:42 -0700

    Adding simulation for wallet creation.

--- packages/simulator/index.ts
+++ packages/simulator/index.ts
@@ -1,5 +1,10 @@
 import { MeritWalletClient } from '../lightwallet/common/merit-wallet-client';
 
+// Parent Alias: webdemo
+// Parent Address: mRSLCXZrU76xkSGZVPY3pQC4i9ExASu2aP
+const NETWORK: string = 'testnet';
+const BWSURL: string = 'http//127.0.0.1/bws/api';
+
 
 export class MeritSimulator {
 
@@ -16,7 +21,29 @@ export class MeritSimulator {
     return mwc;
   }
 
-  public createWallet() {
+  public async createWallet(): Promise<MeritWalletClient> {
+    let walletClient: MeritWalletClient = this.getClient(null, {});
     
+    walletClient.seedFromRandomWithMnemonic({
+      network:  NETWORK,
+      passphrase: "",
+      account: 0
+    });
+    
+    await walletClient.createWallet("test1", "me", 1, 1, {network: NETWORK, singleAddress: true, walletPrivKey: null, parentAddress: "mRSLCXZrU76xkSGZVPY3pQC4i9ExASu2aP", alias: this.randomString()})
+
+    return walletClient;
   }
+
+
+  private randomString(length = 5) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return "test" + text;
+  }
+  
 }
