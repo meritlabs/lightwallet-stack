f9c2d9ebb3f1ef512caed37269ee253acd66eab7
Adil Wali <adil@merit.me>
2018-08-06 18:32:39 -0700

    Adding basic simulator to help model PoG behavior at scale on testnet.

---
+++ packages/simulator/index.ts
@@ -0,0 +1,19 @@
+import { MeritWalletClient } from '../lightwallet/common/merit-wallet-client';
+
+
+export class MeritSimulator {
+
+  public getClient(walletData?, opts?: { bwsurl?: string; verbose?: boolean; }): MeritWalletClient {
+    opts = opts || {bwsurl: "http//127.0.0.1/bws/api", verbose: true};
+  
+    const mwc = MeritWalletClient.getInstance({
+    baseUrl: opts.bwsurl,
+    verbose: opts.verbose || false,
+    timeout: 100000,
+    transports: ['polling'],
+  });
+
+  return mwc;
+}
+
+}
