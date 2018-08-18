bd92de95625139fdc5ec1596e14176e6ff5ab479
Ibby Hadeed <ibby@zyra.ca>
2018-08-18 09:16:31 -0400

    Add hideOnPageChange option to show method

--- packages/lightwallet/common/services/loading-controller.service.ts
+++ packages/lightwallet/common/services/loading-controller.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export abstract class LoadingControllerService {
-  abstract show(message?: string);
+  abstract show(message?: string, hideOnPageChange?: boolean);
   abstract hide();
 }
