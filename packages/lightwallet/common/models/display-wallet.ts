f31ea319591996acc9aae571b4964c9a67e18c3a
Ibby Hadeed <ibby@zyra.ca>
2018-08-19 11:01:38 -0400

    Better error handling

--- packages/lightwallet/common/models/display-wallet.ts
+++ packages/lightwallet/common/models/display-wallet.ts
@@ -1,6 +1,5 @@
 import { MeritWalletClient } from '@merit/common/merit-wallet-client';
 import { FiatAmount } from '@merit/common/models/fiat-amount';
-import { AddressService } from '@merit/common/services/address.service';
 import { PersistenceService2 } from '@merit/common/services/persistence2.service';
 import { TxFormatService } from '@merit/common/services/tx-format.service';
 import { WalletService } from '@merit/common/services/wallet.service';
@@ -8,6 +7,7 @@ import { isNil, sumBy } from 'lodash';
 import { DEFAULT_WALLET_COLOR } from '../utils/constants';
 import { InviteRequest, InviteRequestsService } from '@merit/common/services/invite-request.service';
 import { IRankInfo } from '@merit/common/models/rank';
+import { getAddressInfo } from '@merit/common/utils/addresses';
 
 export interface IDisplayWalletOptions {
   skipStatus?: boolean;
@@ -79,7 +79,6 @@ export class DisplayWallet {
 
   constructor(public client: MeritWalletClient,
               private walletService: WalletService,
-              private addressService: AddressService,
               private inviteRequestsService: InviteRequestsService,
               private txFormatService?: TxFormatService,
               private persistenceService2?: PersistenceService2) {
@@ -107,46 +106,90 @@ export class DisplayWallet {
   }
 
   async updateAlias() {
-    const { alias } = await this.addressService.getAddressInfo(this.referrerAddress);
-    if (alias) {
-      this.alias = alias;
+    try {
+      const { alias } = await getAddressInfo(this.referrerAddress);
+      if (alias) {
+        this.alias = alias;
+      }
+    } catch (err) {
+      console.log('Error updating alias', err);
     }
   }
 
   // Alias if you have one; otherwise address.
   async updateShareCode() {
-    const { alias } = await this.addressService.getAddressInfo(this.referrerAddress);
-    if (alias) {
-      this.shareCode = '@' + alias;
-    } else {
-      this.shareCode = this.referrerAddress;
+    try {
+      const { alias } = await getAddressInfo(this.referrerAddress);
+      if (alias) {
+        this.shareCode = '@' + alias;
+      } else {
+        this.shareCode = this.referrerAddress;
+      }
+    } catch (err) {
+      console.log('Error updating share code', err);
     }
   }
 
   async updateStatus() {
-    this.client.status = await this.client.getStatus();
-    const visitedInvites = await this.persistenceService2.getVisitedInvites() || [];
-    this.inviteRequests = (await this.inviteRequestsService.getInviteRequests(this.client))
-      .map(request => {
-        request.isNew = visitedInvites.findIndex(id => id === request.id) === -1;
-        return request;
-      });
+    try {
+      this.client.status = await this.client.getStatus();
+      const visitedInvites = await this.persistenceService2.getVisitedInvites() || [];
+      this.inviteRequests = (await this.inviteRequestsService.getInviteRequests(this.client))
+        .map(request => {
+          request.isNew = visitedInvites.findIndex(id => id === request.id) === -1;
+          return request;
+        });
+    } catch (err) {
+      console.log('Error updating status', err);
+    }
   }
 
   async updateRankInfo() {
-    this.rankInfo = await this.client.getRankInfo();
+    try {
+      if (!this.confirmed) {
+        throw 'Wallet not confirmed';
+      }
+
+      this.rankInfo = await this.client.getRankInfo();
+    } catch (err) {
+      console.log('Unable to get rank info', err);
+      this.rankInfo = {
+        address: this.address,
+        alias: this.alias,
+        lastUpdated: Date.now(),
+        anv: 0,
+        anvChangeDay: 0,
+        anvChangeWeek: 0,
+        communitySize: 0,
+        anvPercent: 0,
+        communitySizeChange: 0,
+        anvChange: 0,
+        communitySizeChangeDay: 0,
+        communitySizeChangeWeek: 0,
+        percentile: '0',
+        rank: null,
+        rankChange: 0,
+        rankChangeDay: 0,
+        rankChangeWeek: 0,
+      };
+    }
+
     this.communitySize = this.rankInfo.communitySize || 0;
   }
 
   async updateRewards() {
-    this.totalNetworkValueMicro = await this.client.getANV();
+    try {
+      this.totalNetworkValueMicro = await this.client.getANV();
 
-    const rewardsData = await this.client.getRewards();
-    // If we cannot properly fetch data, let's return wallets as-is.
-    if (rewardsData && rewardsData.length > 0) {
-      this.miningRewardsMicro = sumBy(rewardsData, 'rewards.mining');
-      this.growthRewardsMicro = sumBy(rewardsData, 'rewards.ambassador');
-      this.formatNetworkInfo();
+      const rewardsData = await this.client.getRewards();
+      // If we cannot properly fetch data, let's return wallets as-is.
+      if (rewardsData && rewardsData.length > 0) {
+        this.miningRewardsMicro = sumBy(rewardsData, 'rewards.mining');
+        this.growthRewardsMicro = sumBy(rewardsData, 'rewards.ambassador');
+        this.formatNetworkInfo();
+      }
+    } catch (err) {
+      console.log('Error updating rewards', err);
     }
   }
 
@@ -170,12 +213,11 @@ export class DisplayWallet {
 
 export async function createDisplayWallet(wallet: MeritWalletClient,
                                           walletService: WalletService,
-                                          addressService: AddressService,
                                           inviteRequestsService: InviteRequestsService,
                                           txFormatService?: TxFormatService,
                                           persistenceService2?: PersistenceService2,
                                           options: IDisplayWalletOptions = {}): Promise<DisplayWallet> {
-  const displayWallet = new DisplayWallet(wallet, walletService, addressService, inviteRequestsService, txFormatService, persistenceService2);
+  const displayWallet = new DisplayWallet(wallet, walletService, inviteRequestsService, txFormatService, persistenceService2);
   return updateDisplayWallet(displayWallet, options);
 }
 
