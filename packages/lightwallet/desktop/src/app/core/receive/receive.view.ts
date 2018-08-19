7792a79a78bfa684bddf1c69a516baa0fd20cc5c
Ibby Hadeed <ibby@zyra.ca>
2018-08-19 15:54:06 -0400

    Refactor

--- packages/lightwallet/desktop/src/app/core/receive/receive.view.ts
+++ packages/lightwallet/desktop/src/app/core/receive/receive.view.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { DisplayWallet } from '@merit/common/models/display-wallet';
 import { IRootAppState } from '@merit/common/reducers';
-import { selectConfirmedWallets, selectWalletsLoading } from '@merit/common/reducers/wallets.reducer';
-import { AddressService } from '@merit/common/services/address.service';
 import { ConfigService } from '@merit/common/services/config.service';
-import { RateService } from '@merit/common/services/rate.service';
 import { ToastControllerService } from '@merit/common/services/toast-controller.service';
-import { WalletService } from '@merit/common/services/wallet.service';
 import { Store } from '@ngrx/store';
 import 'rxjs/add/operator/take';
-import { Observable } from 'rxjs/Observable';
 import { ReceiveViewController } from '@merit/common/controllers/receive-view.controller';
 
 @Component({
   selector: 'view-receive',
   templateUrl: './receive.view.html',
   styleUrls: ['./receive.view.sass'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ReceiveView implements OnInit {
   ctrl: ReceiveViewController;
@@ -24,7 +18,7 @@ export class ReceiveView implements OnInit {
   constructor(configService: ConfigService,
               store: Store<IRootAppState>,
               toastCtrl: ToastControllerService) {
-    this.ctrl = new ReceiveViewController(configService, store, toastCtrl)
+    this.ctrl = new ReceiveViewController(configService, store, toastCtrl);
   }
 
   async ngOnInit() {
