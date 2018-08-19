6e213dcf304939dc309958ffc55b0eb10d689ed0
Ibby Hadeed <ibby@zyra.ca>
2018-08-19 11:09:29 -0400

    Refactor

--- packages/lightwallet/mobile/src/pages/pin-lock/pin-lock.ts
+++ packages/lightwallet/mobile/src/pages/pin-lock/pin-lock.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform, ViewController } from 'ionic-angular';
+import { IonicPage, NavParams, Platform, ViewController } from 'ionic-angular';
 import { PersistenceService } from '@merit/common/services/persistence.service';
 import { TouchIdService } from '@merit/mobile/services/touch-id.service';
 
@@ -12,7 +12,7 @@ export class PinLockView {
 
   pin: string = '';
 
-  error:boolean;
+  error: boolean;
   isTouchIdAvailable: boolean;
 
   pinBlockedUntil: number;
@@ -35,7 +35,7 @@ export class PinLockView {
     private platform: Platform,
     private viewCtrl: ViewController,
     private touchIdService: TouchIdService,
-    private navParams: NavParams
+    private navParams: NavParams,
   ) {
     if (this.navParams.get('newPinMode')) {
       this.mode = 'new';
@@ -70,9 +70,9 @@ export class PinLockView {
       this.blocked = false;
       this.blockedText = null;
     } else {
-      const minutes = Math.ceil(diff/60000);
+      const minutes = Math.ceil(diff / 60000);
       this.blocked = true;
-      this.blockedText = `You can try again in ${minutes > 1 ? minutes+' minutes' : 'one minute'}`;
+      this.blockedText = `You can try again in ${minutes > 1 ? minutes + ' minutes' : 'one minute'}`;
     }
   }
 
@@ -105,7 +105,7 @@ export class PinLockView {
       if (this.attempts >= this.MAX_ATTEMPTS) {
         this.blocked = true;
         this.checkIsBlocked();
-        this.pinBlockedUntil = Date.now()+1000*60*this.BLOCK_FOR_MINUTES;
+        this.pinBlockedUntil = Date.now() + 1000 * 60 * this.BLOCK_FOR_MINUTES;
         this.persistenceService.blockPin(this.pinBlockedUntil);
         this.attempts = 0;
       }
