c1e0b99868f25c9a76748f15bb6129b5a0d21246
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-22 15:55:52 +0300

    Init BackupNewWallet component

---
+++ packages/lightwallet/desktop/src/app/core/dialog/backup-new-wallet/backup-new-wallet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'backup-new-wallet',
+  templateUrl: './backup-new-wallet.component.html',
+  styleUrls: ['./backup-new-wallet.component.sass']
+})
+
+export class BackupNewWallet {
+
+}
