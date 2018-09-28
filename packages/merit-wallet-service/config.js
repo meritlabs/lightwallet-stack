af66a6d02392d45de43e424b154e7ad347d731d5
Eugene Sypachev (@Axblade) <eugene@merit.me>
2018-09-28 22:32:23 +0300

    BWS->MWS

--- packages/merit-wallet-service/config.js
+++ packages/merit-wallet-service/config.js
@@ -55,7 +55,7 @@ var config = {
     },
   },
   pushNotificationsOpts: {
-    templatePath: './packages/bitcore-wallet-service/lib/templates', // this must be relative to bitcore-node runner
+    templatePath: './packages/merit-wallet-service/lib/templates', // this must be relative to bitcore-node runner
     defaultLanguage: 'en',
     defaultUnit: 'mrt',
     subjectPrefix: '',
